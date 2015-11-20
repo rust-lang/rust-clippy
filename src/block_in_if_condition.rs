b8cd8d6a610014f1589004e065f3a380622ae8dd
John Quigley <jmquigs@gmail.com>
2015-11-20 18:58:38 -0500

    block in if condition: implement check for closures with blocks

--- src/block_in_if_condition.rs
+++ src/block_in_if_condition.rs
@@ -23,26 +23,65 @@ impl LintPass for BlockInIfCondition {
     }
 }
 
+fn find_bad_block(expr: &Expr_) -> bool {
+    match expr {
+        &ExprBinary(_, ref left, ref right) => find_bad_block(&left.node) || find_bad_block(&right.node),
+        &ExprUnary(_, ref exp) => find_bad_block(&exp.node),
+        //&ExprBlock(ref block) => is_block_offensive(block), // false positive alert! don't include this
+        &ExprClosure(_, _, ref block) => {
+            if !block.stmts.is_empty() {
+                true
+            } else {
+                if let Some(ref ex) = block.expr {
+                    let expr:&Expr_ = &ex.node;
+                    match expr {
+                        &ExprBlock(_) => true,
+                        _ => false
+                    }
+                } else {
+                    false
+                }
+            }
+        },
+        &ExprCall(_, ref args) => args.into_iter().find(|e| find_bad_block(&e.node)).is_some(),
+        &ExprMethodCall(_, _, ref args) => args.into_iter().find(|e| find_bad_block(&e.node)).is_some(),
+        _ => {
+            //println!("Dropping out for {:?}", expr);
+            false
+        },
+    }
+}
+
+const BRACED_EXPR_MESSAGE:&'static str = "omit braces around single expression condition";
+const COMPLEX_BLOCK_MESSAGE:&'static str = "in an 'if' condition, avoid complex blocks or closures with blocks; instead, move the block or closure higher and bind it with a 'let'";
+
 impl EarlyLintPass for BlockInIfCondition {
     fn check_expr(&mut self, cx: &EarlyContext, expr: &Expr) {
         if let ExprIf(ref check, ref then, _) = expr.node {
             if let ExprBlock(ref block) = check.node {
                 if block.stmts.is_empty() {
                     if let Some(ref ex) = block.expr {
-                        // zero statements, so remove block
+                        // don't dig into the expression here, just suggest that they remove
+                        // the block
 
                         span_help_and_lint(cx, BLOCK_IN_IF_CONDITION_EXPR, check.span,
-                            "omit braces around single expression condition",
+                            BRACED_EXPR_MESSAGE,
                             &format!("try\nif {} {} ... ", snippet_block(cx, ex.span, ".."),
                             snippet_block(cx, then.span, "..")));
                     }
                 } else {
                     // move block higher
                     span_help_and_lint(cx, BLOCK_IN_IF_CONDITION_STMT, check.span,
-                        "avoid complex blocks in an 'if' condition; instead, move the block higher and bind it with a 'let'",
+                        COMPLEX_BLOCK_MESSAGE,
                         &format!("try\nlet res = {};\nif res {} ... ",
                         snippet_block(cx, block.span, ".."),
                         snippet_block(cx, then.span, "..")));
+                }
+            } else {
+                // go spelunking into the expression, looking for blocks
+                if find_bad_block(&check.node) {
+                    span_help_and_lint(cx, BLOCK_IN_IF_CONDITION_STMT, check.span,
+                        COMPLEX_BLOCK_MESSAGE, "");
                 }
             }
         }
--- tests/compile-fail/block_in_if_condition.rs
+++ tests/compile-fail/block_in_if_condition.rs
@@ -4,6 +4,18 @@
 #![deny(block_in_if_condition_expr)]
 #![deny(block_in_if_condition_stmt)]
 
+fn condition_has_block() -> i32 {
+
+    if { //~ERROR in an 'if' condition, avoid complex blocks or closures with blocks; instead, move the block or closure higher and bind it with a 'let'
+        let x = 3;
+        x == 3
+    } {
+        6
+    } else {
+        10
+    }
+}
+
 fn condition_has_block_with_single_expression() -> i32 {
     if { true } { //~ERROR omit braces around single expression condition
         6
@@ -12,11 +24,28 @@ fn condition_has_block_with_single_expression() -> i32 {
     }
 }
 
-fn condition_has_block() -> i32 {
-    if { //~ERROR avoid complex blocks in an 'if' condition; instead, move the block higher and bind it with a 'let'
-        let x = 3;
-        x == 3
-    } {
+fn predicate<F: FnOnce(T) -> bool, T>(pfn: F, val:T) -> bool {
+    pfn(val)
+}
+
+fn pred_test() {
+    let v = 3;
+    let sky = "blue";
+    // this is a sneaky case, where the block isn't directly in the condition, but is actually
+    // inside a closure that the condition is using.  same principle applies.  add some extra
+    // expressions to make sure linter isn't confused by them.
+    if v == 3 && sky == "blue" && predicate(|x| { let target = 3; x == target }, v) { //~ERROR in an 'if' condition, avoid complex blocks or closures with blocks; instead, move the block or closure higher and bind it with a 'let'
+
+    }
+
+    if predicate(|x| { let target = 3; x == target }, v) { //~ERROR in an 'if' condition, avoid complex blocks or closures with blocks; instead, move the block or closure higher and bind it with a 'let'
+
+    }
+
+}
+
+fn unit_block() -> i32 {
+    if { true; } == { false; } {
         6
     } else {
         10
@@ -32,9 +61,18 @@ fn condition_is_normal() -> i32 {
     }
 }
 
+fn closure_without_block() {
+    if predicate(|x| x == 3, 6) {
+
+    }
+}
+
 // need a main anyway, use it get rid of unused warnings too
 fn main() {
     assert!(condition_has_block_with_single_expression() == 6);
     assert!(condition_has_block() == 6);
+    assert!(unit_block() == 6);
     assert!(condition_is_normal() == 6);
+    closure_without_block();
+    pred_test();
 }
