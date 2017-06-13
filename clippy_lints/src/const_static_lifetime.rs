0a4099c3c6f8a71511a94bafa98b3f921cc26937
Paul Florence <florencepaul@hotmail.fr>
2017-06-14 00:03:20 +0200

    Implementation of the new lint "const with a static lifetime".

--- clippy_lints/src/const_static_lifetime.rs
+++ clippy_lints/src/const_static_lifetime.rs
@@ -1,10 +1,29 @@
-// use syntax::ast::*;
-use rustc::lint::{LintPass, EarlyLintPass, LintArray};
+use syntax::ast::{Item, ItemKind, TyKind};
+use rustc::lint::{LintPass, EarlyLintPass, LintArray, EarlyContext};
+use utils::span_help_and_lint;
 
 
+
+/// **What it does:** Checks for constants with an explicit `'static` lifetime.
+///
+/// **Why is this bad?** Adding `'static` to every reference can create very complicated types.
+///
+/// **Known problems:** None.
+///
+/// **Example:**
+/// ```rust
+///  const GATED_CFGS: &'static [(&'static str, &'static str, fn(&Features) -> bool)] = &[..]
+/// ```
+/// This code can be rewritten as
+/// ```rust
+///  const GATED_CFGS: &[(&str, &str, fn(&Features) -> bool)] = &[...]
+/// ```
+///
+///
 declare_lint! {
-    pub CONST_STATIC_LIFETIME, Warn,
-    "Warn when specifying the lifetime of a constant."
+    pub CONST_STATIC_LIFETIME, 
+    Warn,
+    "Constants have by default a `'static` lifetime ."
 }
 
 
@@ -18,7 +37,20 @@ impl LintPass for StaticConst {
     }
 }
 
-impl EarlyLintPass for StaticConst {}
+impl EarlyLintPass for StaticConst {
+    fn check_item(&mut self, cx: &EarlyContext, item: &Item) {
 
+        if let ItemKind::Const(ref p_ty, _) = item.node {
 
-// impl<'a,'tcx> LintPass
+            if let TyKind::Rptr(ref optionnal_lifetime, _) = p_ty.node {
+                if let &Some(lifetime) = optionnal_lifetime {
+                    span_help_and_lint(cx,
+                                       CONST_STATIC_LIFETIME,
+                                       lifetime.span,
+                                       "Constants have by default a `'static` lifetime",
+                                       "consider removing the `'static`");
+                }
+            }
+        }
+    }
+}
