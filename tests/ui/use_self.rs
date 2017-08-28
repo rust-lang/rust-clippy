8cf01753c527a04fbdd1c2b2ec0d5288a2f5285d
Mason Remaley <MasonRemaley@users.noreply.github.com>
2017-08-28 10:19:24 -0400

    Updates `use_self` tests for generics/lifetimes
    These are gonna fail right now as I haven't actually implemented the fix
    yet.

--- tests/ui/use_self.rs
+++ tests/ui/use_self.rs
@@ -45,8 +45,6 @@ mod better {
     }
 }
 
-//todo the lint does not handle lifetimed struct
-//the following module should trigger the lint on the third method only
 mod lifetimes {
     struct Foo<'a>{foo_str: &'a str}
 
@@ -55,6 +53,7 @@ mod lifetimes {
         fn foo(s: &str) -> Foo {
             Foo { foo_str: s }
         }
+
         // cannot replace with `Self`, because that's `Foo<'a>`
         fn bar() -> Foo<'static> {
             Foo { foo_str: "foo"}
@@ -63,6 +62,24 @@ mod lifetimes {
         // `Self` is applicable here
         fn clone(&self) -> Foo<'a> {
             Foo {foo_str: self.foo_str}
+        }
+    }
+}
+
+mod generics {
+    struct Foo<T> {
+        value: T,
+    }
+
+    impl<T> Foo<T> {
+        // `Self` is applicable here
+        fn foo(value: T) -> Foo<T> {
+            Foo { value }
+        }
+
+        // `Cannot` use `Self` as a return type as the generic types are different
+        fn bar(value: i32) -> Foo<i32> {
+            Foo { value }
         }
     }
 }
