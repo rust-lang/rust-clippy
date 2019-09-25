1beb61c830ced8204cced04a531ddf270cdf5d50
Philipp Hansch <dev@phansch.net>
2019-09-25 07:48:18 +0200

    Add run-rustfix for result_map_unit_fn lint

--- tests/ui/result_map_unit_fn.rs
+++ tests/ui/result_map_unit_fn.rs
@@ -1,3 +1,6 @@
+// run-rustfix
+// rustfix-only-machine-applicable
+
 #![feature(never_type)]
 #![warn(clippy::result_map_unit_fn)]
 #![allow(unused)]
@@ -27,15 +30,6 @@ impl HasResult {
 #[rustfmt::skip]
 fn result_map_unit_fn() {
     let x = HasResult { field: Ok(10) };
-
-    x.field.map(plus_one);
-    let _: Result<(), usize> = x.field.map(do_nothing);
-
-    x.field.map(do_nothing);
-
-    x.field.map(do_nothing);
-
-    x.field.map(diverge);
 
     let captured = 10;
     if let Ok(value) = x.field { do_nothing(value + captured) };
@@ -74,29 +68,6 @@ fn result_map_unit_fn() {
 
 
     x.field.map(|ref value| { do_nothing(value + captured) });
-
-
-    x.field.map(|value| { do_nothing(value); do_nothing(value) });
-
-    x.field.map(|value| if value > 0 { do_nothing(value); do_nothing(value) });
-
-    // Suggestion for the let block should be `{ ... }` as it's too difficult to build a
-    // proper suggestion for these cases
-    x.field.map(|value| {
-        do_nothing(value);
-        do_nothing(value)
-    });
-    x.field.map(|value| { do_nothing(value); do_nothing(value); });
-
-    // The following should suggest `if let Ok(_X) ...` as it's difficult to generate a proper let variable name for them
-    let res: Result<!, usize> = Ok(42).map(diverge);
-    "12".parse::<i32>().map(diverge);
-
-    let res: Result<(), usize> = Ok(plus_one(1)).map(do_nothing);
-
-    // Should suggest `if let Ok(_y) ...` to not override the existing foo variable
-    let y: Result<usize, usize> = Ok(42);
-    y.map(do_nothing);
 }
 
 fn main() {}
