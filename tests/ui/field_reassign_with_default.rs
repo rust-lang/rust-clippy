22e9bc86312254d64f8ab1c3a5ad006d7329f649
Henri Lunnikivi <henri.lunnikivi@gmail.com>
2019-11-01 02:38:15 +0200

    Improve UI test cases

--- tests/ui/field_reassign_with_default.rs
+++ tests/ui/field_reassign_with_default.rs
@@ -11,11 +11,6 @@ struct B {
     j: i64,
 }
 
-#[derive(Default)]
-struct C {
-    i: i32,
-}
-
 fn main() {
     // wrong
     let mut a: A = Default::default();
@@ -31,8 +26,29 @@ fn main() {
     };
 
     // right
-    let b = B { i: 42, j: 24 };
+    let mut a: A = Default::default();
+    if a.i == 0 {
+        a.j = 12;
+    }
 
     // right
-    let c: C = Default::default();
+    let mut a: A = Default::default();
+    let b = 5;
+
+    // right
+    let mut b = 32;
+    let mut a: A = Default::default();
+    b = 2;
+
+    // right
+    let b: B = B { i: 42, j: 24 };
+
+    // right
+    let mut b: B = B { i: 42, j: 24 };
+    b.i = 52;
+
+    // right
+    let mut b = B { i: 15, j: 16 };
+    let mut a: A = Default::default();
+    b.i = 2;
 }
