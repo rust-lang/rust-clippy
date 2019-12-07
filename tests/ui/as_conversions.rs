9b3456d538e8b8fff1d74d91a330b678688bc501
Areredify <misha-babenko@yandex.ru>
2019-12-07 14:52:40 +0300

    add macro tests

--- tests/ui/as_conversions.rs
+++ tests/ui/as_conversions.rs
@@ -1,7 +1,23 @@
-#[warn(clippy::as_conversions)]
+#![warn(clippy::as_conversions)]
+
+macro_rules! mcr1 {
+    (0_u32 as u64) => {
+        ()
+    };
+}
+
+macro_rules! mcr2 {
+    () => {
+        0_u32 as u64
+    };
+}
 
 fn main() {
     let i = 0u32 as u64;
 
     let j = &i as *const u64 as *mut u64;
+
+    let k = mcr1!(0_u32 as u64);
+
+    let p = mcr2!();
 }
