13502bf5018823d850301896408f29579708c3d0
Marcin Serwin <toxyxer@gmail.com>
2020-05-06 14:01:27 +0200

    Add no_to_string_in_display test

--- tests/ui/no_to_string_in_display.rs
+++ tests/ui/no_to_string_in_display.rs
@@ -1,5 +1,37 @@
 #![warn(clippy::no_to_string_in_display)]
 
+use std::fmt;
+
+struct A;
+impl A {
+    fn fmt(&self) {
+        self.to_string();
+    }
+}
+
+trait B {
+    fn fmt(&self) {}
+}
+
+impl B for A {
+    fn fmt(&self) {
+        self.to_string();
+    }
+}
+
+impl fmt::Display for A {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.to_string())
+    }
+}
+
+fn fmt(a: A) {
+    a.to_string();
+}
+
 fn main() {
-    // test code goes here
+    let a = A;
+    a.to_string();
+    a.fmt();
+    fmt(a);
 }
