481468e3dfb25052f33502d0ea43356712ad438b
Marcin Serwin <toxyxer@gmail.com>
2020-05-06 10:16:29 +0200

    Fill in lint description

--- clippy_lints/src/no_to_string_in_display.rs
+++ clippy_lints/src/no_to_string_in_display.rs
@@ -3,24 +3,36 @@ use rustc_session::{declare_lint_pass, declare_tool_lint};
 use rustc_hir::*;
 
 declare_clippy_lint! {
-    /// **What it does:**
+    /// **What it does:** Checks for uses of `to_string()` when implementing
+    /// `Display` traits.
     ///
-    /// **Why is this bad?**
+    /// **Why is this bad?** Usually `to_string` is implemented indirectly
+    /// via `Display`. Hence using it while implementing `Display` would
+    /// lead to infinite recursion.
     ///
     /// **Known problems:** None.
     ///
     /// **Example:**
     ///
     /// ```rust
-    /// // example code where clippy issues a warning
+    /// impl fmt::Display for Structure {
+    ///     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    ///         write!(f, "{}", self.to_string())
+    ///     }
+    /// }
+    ///
     /// ```
     /// Use instead:
     /// ```rust
-    /// // example code which does not raise clippy warning
+    /// impl fmt::Display for Structure {
+    ///     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    ///         write!(f, "{}", self.0)
+    ///     }
+    /// }
     /// ```
     pub NO_TO_STRING_IN_DISPLAY,
     correctness,
-    "default lint description"
+    "to_string method used while implementing Display trait"
 }
 
 declare_lint_pass!(NoToStringInDisplay => [NO_TO_STRING_IN_DISPLAY]);
