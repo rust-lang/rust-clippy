7ade51508a0a870b3fe9ea36be64f82bbe952c1a
Ryan1729 <Ryan1729@gmail.com>
2020-10-01 00:36:44 -0600

    add initial version of `TyIdentIter`

--- clippy_lints/src/utils/ast_utils/ident_iter.rs
+++ clippy_lints/src/utils/ast_utils/ident_iter.rs
@@ -1,5 +1,5 @@
 use core::iter::{self, FusedIterator};
-use rustc_ast::{Expr, ExprKind, MacCall};
+use rustc_ast::{Expr, ExprKind, MacCall, Ty, TyKind};
 use rustc_ast::ptr::P;
 use rustc_span::symbol::Ident;
 
@@ -7,6 +7,10 @@ pub type IdentIter<'a> = Box<dyn Iterator<Item = Ident> + 'a>;
 
 pub fn from_expr<'expr>(expr: &'expr Expr) -> IdentIter<'expr> {
     Box::new(ExprIdentIter::new(expr))
+}
+
+pub fn from_ty<'ty>(ty: &'ty Ty) -> IdentIter<'ty> {
+    Box::new(TyIdentIter::new(ty))
 }
 
 struct ExprIdentIter<'expr> {
@@ -108,6 +112,15 @@ impl <'expr> Iterator for ExprIdentIter<'expr> {
                         )
                 )
             },
+            ExprKind::Cast(ref expr, ref ty)
+            | ExprKind::Type(ref expr, ref ty) => {
+                set_and_call_next!(
+                    ExprIdentIter::new(expr)
+                        .chain(
+                            TyIdentIter::new(ty)
+                        )
+                )
+            },
             _ => todo!(),
         };
 
@@ -120,3 +133,73 @@ impl <'expr> Iterator for ExprIdentIter<'expr> {
 }
 
 impl <'expr> FusedIterator for ExprIdentIter<'expr> {}
+
+struct TyIdentIter<'ty> {
+    ty: &'ty Ty,
+    inner: Option<IdentIter<'ty>>,
+    done: bool,
+}
+
+impl <'ty> TyIdentIter<'ty> {
+    fn new(ty: &'ty Ty) -> Self {
+        Self {
+            ty,
+            inner: None,
+            done: false,
+        }
+    }
+}
+
+impl <'ty> Iterator for TyIdentIter<'ty> {
+    type Item = Ident;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.done {
+            return None;
+        }
+
+        let inner_opt = &mut self.inner;
+
+        if let Some(mut inner) = inner_opt.take() {
+            let output = inner.next();
+
+            if output.is_some() {
+                *inner_opt = Some(inner);
+                return output;
+            }
+        }
+
+        macro_rules! set_and_call_next {
+            ($iter: expr) => {{
+                let mut p_iter = $iter;
+
+                let next_item = p_iter.next();
+
+                *inner_opt = Some(Box::new(p_iter));
+
+                next_item
+            }}
+        }
+
+        let output = match self.ty.kind {
+            TyKind::Never
+            | TyKind::Infer
+            | TyKind::Err
+            | TyKind::CVarArgs => None,
+            TyKind::Slice(ref ty) => {
+                set_and_call_next!(
+                    TyIdentIter::new(ty)
+                )
+            },
+            _ => todo!(),
+        };
+
+        if output.is_none() {
+            self.done = true;
+        }
+
+        output
+    }
+}
+
+impl <'expr> FusedIterator for TyIdentIter<'expr> {}
