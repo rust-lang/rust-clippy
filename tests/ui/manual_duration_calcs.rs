ab770457556c3efb817d7c0a593d77ba7cdf336e
himanoa <matsunoappy@gmail.com>
2020-10-31 04:25:38 +0900

    Add test case

--- tests/ui/manual_duration_calcs.rs
+++ tests/ui/manual_duration_calcs.rs
@@ -28,9 +28,12 @@ fn main() {
     let _ = dur.subsec_nanos() / 699;
 
     // Manual implementation
-    let _nanos = dur.as_secs() * 1_000_000_000 + dur.subsec_nanos() as u64;
-    let _milis = dur.as_secs() * 1_000 + dur.subsec_millis() as u64;
-    let _secs_f64_1 = (dur.as_secs() as f64 * 1_000.0 + dur.subsec_millis() as f64) / 100.0;
+    // let _nanos = dur.as_secs() * 1_000_000_000 + dur.subsec_nanos() as u64;
+    // let _milis = dur.as_secs() * 1_000 + dur.subsec_millis() as u64;
+    // let _secs_f64_1 = (dur.as_secs() as f64 * 1_000.0 + dur.subsec_millis() as f64) / 100.0;
     let _secs_f64_2 = dur.as_secs() as f64 + dur.subsec_millis() as f64 / 1_000.0;
     let _secs_f64_3 = dur.as_secs() as f64 + dur.subsec_nanos() as f64 / 1_000_000_000.0;
+
+    let _secs_f32_2 = dur.as_secs() as f32 + dur.subsec_millis() as f32 / 1_000.0;
+    let _secs_f32_3 = dur.as_secs() as f32 + dur.subsec_nanos() as f32 / 1_000_000_000.0;
 }
