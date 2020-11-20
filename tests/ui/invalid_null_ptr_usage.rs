d76f8e587082bb91e0dee8f5206e5c8f919430f8
boxdot <d@zerovolt.org>
2020-11-20 17:57:14 +0100

    Add test for ptr::null_mut.

--- tests/ui/invalid_null_ptr_usage.rs
+++ tests/ui/invalid_null_ptr_usage.rs
@@ -3,4 +3,6 @@
 fn main() {
     let _slice: &[usize] = unsafe { std::slice::from_raw_parts(std::ptr::null(), 0) };
     let _slice: &[usize] = unsafe { std::slice::from_raw_parts(core::ptr::null(), 0) };
+    let _slice: &[usize] = unsafe { std::slice::from_raw_parts(std::ptr::null_mut(), 0) };
+    let _slice: &[usize] = unsafe { std::slice::from_raw_parts(core::ptr::null_mut(), 0) };
 }
