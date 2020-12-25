ed25378753b3f993e76863a0fba8c6885ae61a2a
himanoa <matsunoappy@gmail.com>
2020-12-25 20:10:50 +0900

    Refactor manual_re_implementation_upper_the_unit implementation
    - Use utility functions

--- clippy_lints/src/manual_duration_calcs.rs
+++ clippy_lints/src/manual_duration_calcs.rs
@@ -303,107 +303,40 @@ impl<'tcx> ManualDurationCalcs {
 
     pub fn manual_re_implementation_upper_the_unit(cx: &LateContext<'tcx>, expr: &'tcx Expr<'_>) {
         if_chain! {
-            // Extraction necessary expression(left)
-            // let secs_f64 = diff.as_secs() as f64 + diff.subsec_nanos() as f64 / 1_000_000_000.0;
-            //                ^^^^^^^^^^^^^^^^^^^^^
             if let ExprKind::Binary(
-                Spanned { node: BinOpKind::Add, ..  },
-                ref plus_cast_left_expr,
+                Spanned {
+                    node: BinOpKind::Add, ..
+                },
+                ref add_left_expr,
                 ref rest,
             ) = expr.kind;
-
-            if let ExprKind::Cast(
-                Expr {
-                    kind: ExprKind::MethodCall(PathSegment { ident: left_ident, ..  }, _, [left_receiver], _,),
-                     ..
-                 },
-                Ty { kind: TyKind::Path(QPath::Resolved( None, Path { res: Res::PrimTy(left_type), ..  },)), ..  },
-            ) = plus_cast_left_expr.kind;
-
-            // Extraction necessary expression(right)
-            // let secs_f64 = diff.as_secs() as f64 + diff.subsec_nanos() as f64 / 1_000_000_000.0;
-            //                                        ^^^^^^^^^^^^^^^^^^^^^^^^^^
-            if let ExprKind::Binary(
-                Spanned { node: BinOpKind::Div, ..  },
-                ref plus_cast_right_expr,
-                ref divisor_expr,
-            ) = rest.kind;
-
-            if let ExprKind::Cast(
-                Expr {
-                    kind: ExprKind::MethodCall( PathSegment { ident: right_ident, ..  }, _, [right_receiver], _),
-                    ..
-                },
-                Ty { kind: TyKind::Path( QPath::Resolved( None, Path { res: Res::PrimTy(right_type), ..  })), ..  }
-            ) = plus_cast_right_expr.kind;
-
-            // Extraction necessary expression(divisor literal)
-            // let secs_f64 = diff.as_secs() as f64 + diff.subsec_nanos() as f64 / 1_000_000_000.0;
-            //                                                                     ^^^^^^^^^^^^^^^
-            if let ExprKind::Lit(Spanned { node: LitKind::Float(ref divisor_lit, _), .. }) = divisor_expr.kind;
-
-
-            // Extraction necessary expression(method receiver)
-            // let secs_f64 = diff.as_secs() as f64 + diff.subsec_nanos() as f64 / 1_000_000_000.0;
-            //                ^^^^                    ^^^^
-            if let Expr {
-                kind: ExprKind::Path(
-                          QPath::Resolved(
-                              None,
-                              Path {
-                                  segments: [
-                                      PathSegment {
-                                          ident: left_receiver_ident,
-                                          ..
-                                      }
-                                  ],
-                              ..
-                              }
-                          )
-                      ),
-                ..
-            } = left_receiver;
-            if let Expr {
-                kind: ExprKind::Path(
-                          QPath::Resolved(
-                              None,
-                              Path {
-                                  segments: [
-                                      PathSegment {
-                                          ident: right_receiver_ident,
-                                          ..
-                                      }
-                                  ],
-                                  ..
-                              }
-                              )
-                          ),
-                          ..
-            } = right_receiver;
-
-            // same receiver guard
-            if left_receiver_ident == right_receiver_ident;
-
+            if let Some(parsed_div) = parse_division_expr(cx, &rest);
+            if let Some(parsed_add) = parse_method_call_expr(cx, &add_left_expr, None);
             then {
-                let divisor: f64 = divisor_lit.as_str().parse().unwrap();
-
-                let left_method_name: &str = &left_ident.as_str().to_string();
-                let right_method_name: &str = &right_ident.as_str().to_string();
-
-                let suggested_fn = match (left_method_name, left_type, right_method_name, right_type, divisor) {
-                    ("as_secs", PrimTy::Float(FloatTy::F64), "subsec_nanos", PrimTy::Float(FloatTy::F64), div) if div == 1_000_000_000.0 => "as_secs_f64",
-                    ("as_secs", PrimTy::Float(FloatTy::F32), "subsec_nanos", PrimTy::Float(FloatTy::F32), div) if div == 1_000_000_000.0 => "as_secs_f32",
-                    ("subsec_nanos", PrimTy::Float(FloatTy::F64), "as_secs", PrimTy::Float(FloatTy::F64), div) if div == 1_000_000_000.0 => "as_secs_f64",
-                    ("subsec_nanos", PrimTy::Float(FloatTy::F32), "as_secs", PrimTy::Float(FloatTy::F32), div) if div == 1_000_000_000.0 => "as_secs_f32",
-                    ("as_secs", PrimTy::Float(FloatTy::F64), "subsec_millis", PrimTy::Float(FloatTy::F64), div ) if div == 1_000.0 => "as_secs_f64",
-                    ("as_secs", PrimTy::Float(FloatTy::F32), "subsec_millis", PrimTy::Float(FloatTy::F32), div) if div == 1_000.0 => "as_secs_f32",
-                    ("subsec_millis", PrimTy::Float(FloatTy::F64), "as_secs", PrimTy::Float(FloatTy::F64), div) if div == 1_000.0 => "as_secs_f64",
-                    ("subsec_millis", PrimTy::Float(FloatTy::F32), "as_secs", PrimTy::Float(FloatTy::F32), div) if div == 1_000.0 => "as_secs_f32",
-                    _ => return,
+                let suggested_fn = match (
+                    parsed_add.method_name.to_string().as_str(),
+                    parsed_add.cast_type,
+                    parsed_div.method_name.to_string().as_str(),
+                    parsed_div.cast_type,
+                    parsed_div.divisor
+                ) {
+                    ("as_secs", Some(PrimTy::Float(FloatTy::F64)), "subsec_millis", Some(PrimTy::Float(FloatTy::F64)), Divisor::F64(divisor)) if (divisor - 1000.0).abs() < f64::EPSILON => {
+                        "as_secs_f64"
+                    },
+                    ("as_secs", Some(PrimTy::Float(FloatTy::F64)), "subsec_nanos", Some(PrimTy::Float(FloatTy::F64)), Divisor::F64(divisor)) if (divisor - 1_000_000_000.0).abs() < f64::EPSILON => {
+                        "as_secs_f64"
+                    },
+                    ("as_secs", Some(PrimTy::Float(FloatTy::F32)), "subsec_millis", Some(PrimTy::Float(FloatTy::F32)), Divisor::F64(divisor)) if (divisor - 1000.0).abs() < f64::EPSILON => {
+                        "as_secs_f32"
+                    },
+                    ("as_secs", Some(PrimTy::Float(FloatTy::F32)), "subsec_nanos", Some(PrimTy::Float(FloatTy::F32)), Divisor::F64(divisor)) if (divisor - 1_000_000_000.0).abs() < f64::EPSILON => {
+                        "as_secs_f32"
+                    },
+                    _ => { return;  }
                 };
 
-
                 let mut applicability = Applicability::MachineApplicable;
+
                 span_lint_and_sugg(
                     cx,
                     MANUAL_DURATION_CALCS,
@@ -412,13 +345,13 @@ impl<'tcx> ManualDurationCalcs {
                     "try",
                     format!(
                         "{}.{}()",
-                        snippet_with_applicability(cx, left_receiver.span, "_", &mut applicability),
+                        snippet_with_applicability(cx, parsed_add.receiver, "_", &mut applicability),
                         suggested_fn
-                        ),
-                        applicability,
-                        );
+                    ),
+                    applicability,
+                );
             }
-        };
+        }
     }
 
     pub fn manual_re_implementation_as_secs_f64_for_div_and_mul(cx: &LateContext<'tcx>, expr: &'tcx Expr<'_>) {
