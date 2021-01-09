ef88eacde92fbefbee14fe828272b4cba2dae195
xiongmao86 <xiongmao86dev@sina.com>
2021-01-09 20:24:55 +0800

    Add test.

--- tests/ui/into_and_try_into_instead_of_from_and_try_from.rs
+++ tests/ui/into_and_try_into_instead_of_from_and_try_from.rs
@@ -1,4 +1,9 @@
+// run-rustfix
 #![warn(clippy::into_and_try_into_instead_of_from_and_try_from)]
+use std::convert::TryFrom;
+
+fn foo<T>(a: T) where u32: From<T> {}
+fn bar<T>(a: T) where u32: TryFrom<T> {}
 
 fn main() {
     // test code goes here
