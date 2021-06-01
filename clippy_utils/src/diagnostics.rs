f3720a8234c1801b737d00f90e8c9086cec5dff8
Cameron Steffen <cam.steffen94@gmail.com>
2021-05-31 21:49:56 -0500

    Use span_clippy_lint in helper functions

--- clippy_utils/src/diagnostics.rs
+++ clippy_utils/src/diagnostics.rs
@@ -58,14 +58,28 @@ impl Drop for ClippyDiagnosticBuilder<'_> {
 
 /// Same as [`LintContext::struct_span_lint`] but automatically adds additional Clippy lint info to
 /// the diagnostic via [`docs_lint`].
-pub fn span_clippy_lint<C, S, F>(cx: &C, lint: &'static Lint, sp: S, f: F)
-where
-    C: LintContext,
-    S: Into<MultiSpan>,
-    F: FnOnce(ClippyLintDiagnosticBuilder<'_>),
-{
+pub fn span_clippy_lint(
+    cx: &impl LintContext,
+    lint: &'static Lint,
+    sp: impl Into<MultiSpan>,
+    decorate: impl FnOnce(ClippyLintDiagnosticBuilder<'_>),
+) {
     cx.struct_span_lint(lint, sp, |diag| {
-        f(ClippyLintDiagnosticBuilder { lint, diag });
+        decorate(ClippyLintDiagnosticBuilder { lint, diag });
+    });
+}
+
+/// Same as [`TyCtxt::struct_span_lint_hir`] but automatically adds additional Clippy lint info to
+/// the diagnostic via [`docs_lint`].
+pub fn span_clippy_lint_hir(
+    cx: &LateContext<'_>,
+    lint: &'static Lint,
+    hir_id: HirId,
+    sp: impl Into<MultiSpan>,
+    decorate: impl FnOnce(ClippyLintDiagnosticBuilder<'_>),
+) {
+    cx.tcx.struct_span_lint_hir(lint, hir_id, sp, |diag| {
+        decorate(ClippyLintDiagnosticBuilder { lint, diag });
     });
 }
 
@@ -102,10 +116,8 @@ fn docs_link(diag: &mut DiagnosticBuilder<'_>, lint: &'static Lint) {
 ///    |     ^^^^^^^^^^^^^^^^^^^^^^^
 /// ```
 pub fn span_lint<T: LintContext>(cx: &T, lint: &'static Lint, sp: impl Into<MultiSpan>, msg: &str) {
-    cx.struct_span_lint(lint, sp, |diag| {
-        let mut diag = diag.build(msg);
-        docs_link(&mut diag, lint);
-        diag.emit();
+    span_clippy_lint(cx, lint, sp, |diag| {
+        diag.build(msg);
     });
 }
 
@@ -137,15 +149,13 @@ pub fn span_lint_and_help<'a, T: LintContext>(
     help_span: Option<Span>,
     help: &str,
 ) {
-    cx.struct_span_lint(lint, span, |diag| {
+    span_clippy_lint(cx, lint, span, |diag| {
         let mut diag = diag.build(msg);
         if let Some(help_span) = help_span {
             diag.span_help(help_span, help);
         } else {
             diag.help(help);
         }
-        docs_link(&mut diag, lint);
-        diag.emit();
     });
 }
 
@@ -180,15 +190,13 @@ pub fn span_lint_and_note<'a, T: LintContext>(
     note_span: Option<Span>,
     note: &str,
 ) {
-    cx.struct_span_lint(lint, span, |diag| {
+    span_clippy_lint(cx, lint, span, |diag| {
         let mut diag = diag.build(msg);
         if let Some(note_span) = note_span {
             diag.span_note(note_span, note);
         } else {
             diag.note(note);
         }
-        docs_link(&mut diag, lint);
-        diag.emit();
     });
 }
 
@@ -202,19 +210,14 @@ where
     S: Into<MultiSpan>,
     F: FnOnce(&mut DiagnosticBuilder<'_>),
 {
-    cx.struct_span_lint(lint, sp, |diag| {
-        let mut diag = diag.build(msg);
-        f(&mut diag);
-        docs_link(&mut diag, lint);
-        diag.emit();
+    span_clippy_lint(cx, lint, sp, |diag| {
+        f(&mut diag.build(msg));
     });
 }
 
 pub fn span_lint_hir(cx: &LateContext<'_>, lint: &'static Lint, hir_id: HirId, sp: Span, msg: &str) {
-    cx.tcx.struct_span_lint_hir(lint, hir_id, sp, |diag| {
-        let mut diag = diag.build(msg);
-        docs_link(&mut diag, lint);
-        diag.emit();
+    span_clippy_lint_hir(cx, lint, hir_id, sp, |diag| {
+        diag.build(msg);
     });
 }
 
@@ -226,11 +229,8 @@ pub fn span_lint_hir_and_then(
     msg: &str,
     f: impl FnOnce(&mut DiagnosticBuilder<'_>),
 ) {
-    cx.tcx.struct_span_lint_hir(lint, hir_id, sp, |diag| {
-        let mut diag = diag.build(msg);
-        f(&mut diag);
-        docs_link(&mut diag, lint);
-        diag.emit();
+    span_clippy_lint_hir(cx, lint, hir_id, sp, |diag| {
+        f(&mut diag.build(msg));
     });
 }
 
@@ -263,8 +263,8 @@ pub fn span_lint_and_sugg<'a, T: LintContext>(
     sugg: String,
     applicability: Applicability,
 ) {
-    span_lint_and_then(cx, lint, sp, msg, |diag| {
-        diag.span_suggestion(sp, help, sugg, applicability);
+    span_clippy_lint(cx, lint, sp, |diag| {
+        diag.build(msg).span_suggestion(sp, help, sugg, applicability);
     });
 }
 
