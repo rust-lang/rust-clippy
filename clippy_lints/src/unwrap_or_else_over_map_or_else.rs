f5d4aab6a2698584fd46a3b3db803ed14bf5ec74
valentine-mario <valentine13400@gmail.com>
2021-06-19 22:12:18 +0100

    added lint for map_or_else

--- clippy_lints/src/unwrap_or_else_over_map_or_else.rs
+++ clippy_lints/src/unwrap_or_else_over_map_or_else.rs
@@ -1,11 +1,13 @@
-use rustc_lint::{LateLintPass, LateContext};
-use rustc_session::{declare_lint_pass, declare_tool_lint};
+use clippy_utils::diagnostics::span_lint_and_help;
+use if_chain::if_chain;
 use rustc_hir::*;
+use rustc_lint::{LateContext, LateLintPass};
+use rustc_session::{declare_lint_pass, declare_tool_lint};
 
 declare_clippy_lint! {
-    /// **What it does:**
+    /// **What it does:** Suggest the use of Result::unwrap_or_else over Result::map_or_else if map_or_else is just used to unpack a successful result while handling an error
     ///
-    /// **Why is this bad?**
+    /// **Why is this bad?** The unwrap_or_else is shorter and more descriptive
     ///
     /// **Known problems:** None.
     ///
@@ -14,9 +16,22 @@ declare_clippy_lint! {
     /// ```rust
     /// // example code where clippy issues a warning
     /// ```
+    /// func_result(in_num: u8) -> Result<&'static str, &'static str> {
+    ///    if in_num % 2 != 0 {
+    ///        return Err("Can't do this because input is odd...");
+    ///    }
+    ///    Ok("An even number :)")
+    /// }
+    ///
+    /// func_result(2).map_or_else(|e| println!("{:?}", e), |n| println!("{}", n))
     /// Use instead:
     /// ```rust
     /// // example code which does not raise clippy warning
+    ///  /// let c= func_result(3).unwrap_or_else(|e| {
+    ///     println!("Error: {:?}", e);
+    ///    process::exit(1);
+    /// });
+    ///
     /// ```
     pub UNWRAP_OR_ELSE_OVER_MAP_OR_ELSE,
     pedantic,
@@ -25,4 +40,26 @@ declare_clippy_lint! {
 
 declare_lint_pass!(UnwrapOrElseOverMapOrElse => [UNWRAP_OR_ELSE_OVER_MAP_OR_ELSE]);
 
-impl LateLintPass<'_> for UnwrapOrElseOverMapOrElse {}
+impl LateLintPass<'_> for UnwrapOrElseOverMapOrElse {
+    fn check_expr(&mut self, cx: &LateContext<'_>, expr: &Expr<'_>) {
+        if_chain! {
+            //check if this is a method call eg func.map_or_else()
+            if let ExprKind::MethodCall(method, _t_span, args, _) = expr.kind;
+            //check if the function name is map_or_else
+            if method.ident.as_str() == "map_or_else";
+            //check if the first arg is a closure
+            if let ExprKind::Closure(_, _, _, _, _) = args[1].kind ;
+            then{
+                println!("{:?}",  method.ident.as_str());
+                span_lint_and_help(
+                    cx,
+                    UNWRAP_OR_ELSE_OVER_MAP_OR_ELSE,
+                    _t_span,
+                    "Result::unwrap_or_else is shorter and more succinet",
+                    None,
+                    "consider unwrap_or_else(|e| handle_the_error(e)) to unpack result",
+                );
+            }
+        }
+    }
+}
--- tests/ui/unwrap_or_else_over_map_or_else.rs
+++ tests/ui/unwrap_or_else_over_map_or_else.rs
@@ -1,5 +1,17 @@
 #![warn(clippy::unwrap_or_else_over_map_or_else)]
+use std::process;
 
 fn main() {
-    // test code goes here
+    let c = func_result(3).unwrap_or_else(|e| {
+        println!("Error: {:?}", e);
+        process::exit(1);
+    });
+    func_result(2).map_or_else(|e| println!("{:?}", e), |n| println!("{}", n))
+}
+
+fn func_result(in_num: u8) -> Result<&'static str, &'static str> {
+    if in_num % 2 != 0 {
+        return Err("Can't do this because input is odd...");
+    }
+    Ok("An even number :)")
 }
