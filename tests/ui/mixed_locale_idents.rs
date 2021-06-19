b474a0826b0495865570db0fde8646d73a708b62
Igor Aleksanov <popzxc@yandex.ru>
2021-06-19 21:35:31 +0300

    Add german example to UI test

--- tests/ui/mixed_locale_idents.rs
+++ tests/ui/mixed_locale_idents.rs
@@ -6,4 +6,7 @@ pub struct Blоck;
 
 fn main() {
     let black_чёрный_黒い_काला = "good luck hand-writing it";
+
+    // Should not spawn the lint, as it represents valid sequence in a single locale.
+    let nutzer_zähler = "user counter";
 }
