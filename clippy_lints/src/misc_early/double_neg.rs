3efd15529ec1c35c7c6cd95b13f833c0d99cc081
J-ZhengLi <lizhengghengj@gmail.com>
2021-12-30 16:02:59 +0800

    update lint help message for double_neg To make it more beginner friendly

--- clippy_lints/src/misc_early/double_neg.rs
+++ clippy_lints/src/misc_early/double_neg.rs
@@ -1,4 +1,4 @@
-use clippy_utils::diagnostics::span_lint;
+use clippy_utils::diagnostics::span_lint_and_help;
 use rustc_ast::ast::{Expr, ExprKind, UnOp};
 use rustc_lint::EarlyContext;
 
@@ -7,11 +7,15 @@ use super::DOUBLE_NEG;
 pub(super) fn check(cx: &EarlyContext<'_>, expr: &Expr) {
     if let ExprKind::Unary(UnOp::Neg, ref inner) = expr.kind {
         if let ExprKind::Unary(UnOp::Neg, _) = inner.kind {
-            span_lint(
+            span_lint_and_help(
                 cx,
                 DOUBLE_NEG,
                 expr.span,
-                "`--x` could be misinterpreted as pre-decrement by C programmers, is usually a no-op",
+                "`--x`, which is a double negation of `x` and not a pre-decrement operator \
+                as in C/C++. Rust does not have unary increment (++) or decrement (--) operators \
+                at the moment",
+                None,
+                "consider using `x -= 1` if you intended to decrease the value of `x`"
             );
         }
     }
