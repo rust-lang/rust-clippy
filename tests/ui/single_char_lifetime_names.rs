e0cec6bff4cd8202fae3d3b94e0d550025cef969
Ruby Lazuli <general@patchmixolydic.com>
2022-01-09 01:38:37 -0600

    test that `'_` is not linted by `single_char_lifetime_names`

--- tests/ui/single_char_lifetime_names.rs
+++ tests/ui/single_char_lifetime_names.rs
@@ -1,5 +1,6 @@
 #![warn(clippy::single_char_lifetime_names)]
 
+// Lifetimes should only be linted when they're introduced
 struct DiagnosticCtx<'a, 'b>
 where
     'a: 'b,
@@ -8,6 +9,7 @@ where
     _unit: &'b (),
 }
 
+// Only the lifetimes on the `impl`'s generics should be linted
 impl<'a, 'b> DiagnosticCtx<'a, 'b> {
     fn new(source: &'a str, unit: &'b ()) -> DiagnosticCtx<'a, 'b> {
         Self {
@@ -17,6 +19,7 @@ impl<'a, 'b> DiagnosticCtx<'a, 'b> {
     }
 }
 
+// No lifetimes should be linted here
 impl<'src, 'unit> DiagnosticCtx<'src, 'unit> {
     fn new_pass(source: &'src str, unit: &'unit ()) -> DiagnosticCtx<'src, 'unit> {
         Self {
@@ -24,6 +27,13 @@ impl<'src, 'unit> DiagnosticCtx<'src, 'unit> {
             _unit: unit,
         }
     }
+}
+
+// Only 'a should be linted here
+fn split_once<'a>(base: &'a str, other: &'_ str) -> (&'a str, Option<&'a str>) {
+    base.split_once(other)
+        .map(|(left, right)| (left, Some(right)))
+        .unwrap_or((base, None))
 }
 
 fn main() {
