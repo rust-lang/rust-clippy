62ad62ba79e8341548bc19baff6d8901c150f97a
xFrednet <xFrednet@gmail.com>
2022-02-17 23:12:38 +0100

    Make `is_nightly_run` const

--- clippy_utils/src/nightly.rs
+++ clippy_utils/src/nightly.rs
@@ -1,28 +1,14 @@
 //! This module is intended to hold most implementations related to Clippy's
 //! nightly lints.
 
-use std::lazy::SyncOnceCell;
-
-use rustc_session::Session;
-
-static IS_NIGHTLY_RUN: SyncOnceCell<bool> = SyncOnceCell::new();
-
-/// This function is used to determine if nightly lints should be enabled or disabled
-/// in this Clippy run.
-///
-/// It's only allowed to call this once. This is done by [`clippy_lints::lib`]
-pub fn eval_is_nightly_run(sess: &Session) {
-    // This allows users to disable nightly lints on nightly
-    let disable_nightly = std::env::var("CLIPPY_NIGHTLY").map(|s| s == "0").unwrap_or(false);
-    // This allows users to enable nightly lints on stable
-    let enable_nightly = std::env::var("CLIPPY_NIGHTLY").map(|s| s == "1").unwrap_or(false);
-
-    let is_nightly_run = enable_nightly || (sess.is_nightly_build() && !disable_nightly);
-
-    IS_NIGHTLY_RUN
-        .set(is_nightly_run)
-        .expect("`ENABLE_NIGHTLY_LINTS` should only be set once.");
-}
+/// This determines if Clippy is being compiled for nightly or stable. Bootstrap which is
+/// used to compile Clippy in the rust repo will set the `CFG_DISABLE_UNSTABLE_FEATURES`
+/// environment value if Clippy is being compiled for nightly (Or another edge case see
+/// [`rustc_feature::UnstableFeatures`]). In the rust-clippy repo, Clippy is being compiled
+/// with cargo, therefore we can just check if a cargo environment value is set to determine
+/// if this is a nightly run or not
+const IS_NIGHTLY: bool =
+    std::option_env!("CFG_DISABLE_UNSTABLE_FEATURES").is_some() || std::option_env!("CARGO_MANIFEST_DIR").is_some();
 
 /// This function checks if the current run is a nightly run with Clippy's nightly lints. This is
 /// destinct from rustc's as a nightly build can disable Clippy's nightly features.
@@ -30,6 +16,6 @@ pub fn eval_is_nightly_run(sess: &Session) {
 /// See [`Session::is_nightly_build(&self)`] if you want to check if the current build is a nightly
 /// build.
 #[inline]
-pub fn is_nightly_run() -> bool {
-    *IS_NIGHTLY_RUN.get().unwrap_or(&false)
+pub const fn is_nightly_run() -> bool {
+    IS_NIGHTLY
 }
