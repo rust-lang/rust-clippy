509031f44c7a0950bb60229c14196ab3c55e4c27
Yoav Lavi <yoavlavi122@gmail.com>
2022-03-23 10:58:58 +0100

    normalize argument names

--- clippy_lints/src/methods/unnecessary_join.rs
+++ clippy_lints/src/methods/unnecessary_join.rs
@@ -10,26 +10,26 @@ use rustc_span::{sym, Span};
 use super::UNNECESSARY_JOIN;
 
 pub(super) fn check<'tcx>(
-    context: &LateContext<'tcx>,
+    cx: &LateContext<'tcx>,
     expr: &'tcx Expr<'tcx>,
     join_self_arg: &'tcx Expr<'tcx>,
     join_arg: &'tcx Expr<'tcx>,
     span: Span,
 ) {
     let applicability = Applicability::MachineApplicable;
-    let collect_output_adjusted_type = context.typeck_results().expr_ty_adjusted(join_self_arg);
+    let collect_output_adjusted_type = cx.typeck_results().expr_ty_adjusted(join_self_arg);
     if_chain! {
         // the turbofish for collect is ::<Vec<String>>
         if let Ref(_, ref_type, _) = collect_output_adjusted_type.kind();
         if let Slice(slice) = ref_type.kind();
-        if is_type_diagnostic_item(context, *slice, sym::String);
+        if is_type_diagnostic_item(cx, *slice, sym::String);
         // the argument for join is ""
         if let ExprKind::Lit(spanned) = &join_arg.kind;
         if let LitKind::Str(symbol, _) = spanned.node;
         if symbol.is_empty();
         then {
             span_lint_and_sugg(
-                context,
+                cx,
                 UNNECESSARY_JOIN,
                 span.with_hi(expr.span.hi()),
                 r#"called `.collect<Vec<String>>().join("")` on an iterator"#,
