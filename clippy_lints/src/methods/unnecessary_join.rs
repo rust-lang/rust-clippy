95a7aa22c0e678e9f61c5fb34fb9d023c962e85a
Yoav Lavi <yoavlavi122@gmail.com>
2022-03-23 10:33:59 +0100

    remove unneeded ref

--- clippy_lints/src/methods/unnecessary_join.rs
+++ clippy_lints/src/methods/unnecessary_join.rs
@@ -17,7 +17,7 @@ pub(super) fn check<'tcx>(
     span: Span,
 ) {
     let applicability = Applicability::MachineApplicable;
-    let collect_output_adjusted_type = &context.typeck_results().expr_ty_adjusted(join_self_arg);
+    let collect_output_adjusted_type = context.typeck_results().expr_ty_adjusted(join_self_arg);
     if_chain! {
         // the turbofish for collect is ::<Vec<String>>
         if let Ref(_, ref_type, _) = collect_output_adjusted_type.kind();
