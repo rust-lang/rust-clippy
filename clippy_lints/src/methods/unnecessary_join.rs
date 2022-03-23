ecfffb13f05e78931c374db6afb19e813512b1f9
Yoav Lavi <yoavlavi122@gmail.com>
2022-03-23 11:01:40 +0100

    cleanup

--- clippy_lints/src/methods/unnecessary_join.rs
+++ clippy_lints/src/methods/unnecessary_join.rs
@@ -1,5 +1,4 @@
-use clippy_utils::diagnostics::span_lint_and_sugg;
-use clippy_utils::ty::is_type_diagnostic_item;
+use clippy_utils::{diagnostics::span_lint_and_sugg, ty::is_type_diagnostic_item};
 use rustc_ast::ast::LitKind;
 use rustc_errors::Applicability;
 use rustc_hir::{Expr, ExprKind};
