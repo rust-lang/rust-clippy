243e671b7297639833093417fc0f9769d96ff30d
Dmitrii Lavrov <d.lavrov@proscom.ru>
2022-06-30 20:02:52 +0300

    it detects errors

--- clippy_lints/src/bool_to_int_with_if.rs
+++ clippy_lints/src/bool_to_int_with_if.rs
@@ -1,19 +1,30 @@
+use rustc_ast::LitKind;
 use rustc_hir::*;
 use rustc_lint::{LateContext, LateLintPass};
 use rustc_session::{declare_lint_pass, declare_tool_lint};
 
 declare_clippy_lint! {
     /// ### What it does
-    ///
+    /// Instead of using an if statement to convert a bool to an int, this lint suggests using a `as` coercion or a `from()` function.
     /// ### Why is this bad?
+    /// Coersion or `from()` is idiomatic way to convert bool to a number.
+    /// Both methods are guaranteed to return 1 for true, and 0 for false. See https://doc.rust-lang.org/std/primitive.bool.html#impl-From%3Cbool%3E
     ///
     /// ### Example
     /// ```rust
-    /// // example code where clippy issues a warning
+    /// if condition {
+    ///     1_i64
+    /// } else {
+    ///     0
+    /// }
     /// ```
     /// Use instead:
     /// ```rust
-    /// // example code which does not raise clippy warning
+    /// i64::from(condition)
+    /// ```
+    /// or
+    /// ```rust
+    /// condition as i64
     /// ```
     #[clippy::version = "1.64.0"]
     pub BOOL_TO_INT_WITH_IF,
@@ -22,4 +33,64 @@ declare_clippy_lint! {
 }
 declare_lint_pass!(BoolToIntWithIf => [BOOL_TO_INT_WITH_IF]);
 
-impl LateLintPass<'_> for BoolToIntWithIf {}
+impl<'tcx> LateLintPass<'tcx> for BoolToIntWithIf {
+    fn check_expr(&mut self, ctx: &LateContext<'tcx>, expr: &'tcx rustc_hir::Expr<'tcx>) {
+        if !expr.span.from_expansion() {
+            check_if_else(ctx, expr);
+        }
+    }
+}
+
+fn check_if_else<'tcx>(ctx: &LateContext<'tcx>, expr: &'tcx rustc_hir::Expr<'tcx>) {
+    if_chain!(
+        if let ExprKind::If(check, then, Some(else_)) = &expr.kind;
+
+        if let Some(then_lit) = int_literal(then);
+        if let Some(else_lit) = int_literal(else_);
+
+        if check_int_literal_equals_val(then_lit, 1);
+        if check_int_literal_equals_val(else_lit, 0);
+
+        then {
+            dbg!(expr.span);
+        }
+    );
+}
+
+// If block contains only a int literal expression, return literal expression
+fn int_literal<'tcx>(expr: &'tcx rustc_hir::Expr<'tcx>) -> Option<&'tcx rustc_hir::Expr<'tcx>> {
+    if_chain!(
+        if let ExprKind::Block(block, _) = expr.kind;
+        if let Block {
+            stmts: [],       // Shouldn't lint if statements with side effects
+            expr: Some(expr),
+            hir_id: _,
+            rules: _,
+            span: _,
+            targeted_by_break: _,
+        } = block;
+        if let ExprKind::Lit(lit) = &expr.kind;
+
+        if let LitKind::Int(val, _) = lit.node;
+
+        then {
+            return Some(expr)
+        }
+    );
+
+    None
+}
+
+fn check_int_literal_equals_val<'tcx>(expr: &'tcx rustc_hir::Expr<'tcx>, expected_value: u128) -> bool {
+    if_chain!(
+        if let ExprKind::Lit(lit) = &expr.kind;
+        if let LitKind::Int(val, _) = lit.node;
+        if val == expected_value;
+
+        then {
+            return true;
+        }
+    );
+
+    return false;
+}
--- tests/ui/bool_to_int_with_if.rs
+++ tests/ui/bool_to_int_with_if.rs
@@ -1,5 +1,25 @@
 #![warn(clippy::bool_to_int_with_if)]
 
 fn main() {
-    // test code goes here
+    // should warn
+    let cond = true;
+    let x = if (cond) { 1 } else { 0 };
+
+    // shouldn't
+    let x = if (cond) { Some(1) } else { None };
+
+    let x = if (cond) {
+        side_effect();
+        1
+    } else {
+        0
+    };
+
+    let x = if (cond) { 2 } else { 0 };
+
+    // questionable
+    // maybe `!cond as usize` is reasonable
+    let x = if (cond) { 0 } else { 1 };
 }
+
+fn side_effect() {}
