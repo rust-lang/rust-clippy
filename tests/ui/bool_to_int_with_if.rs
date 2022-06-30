033e8f160c766a3edc1b5a56cc10e2acaf8b22e7
Dmitrii Lavrov <d.lavrov@proscom.ru>
2022-06-30 20:50:18 +0300

    fromat and add some tests

--- tests/ui/bool_to_int_with_if.rs
+++ tests/ui/bool_to_int_with_if.rs
@@ -1,25 +1,29 @@
 #![warn(clippy::bool_to_int_with_if)]
 
 fn main() {
-    // should warn
     let cond = true;
-    let x = if (cond) { 1 } else { 0 };
+
+    // should warn
+    let x = if cond { 1 } else { 0 };
+
+    let x = if cond ^ true { 1 } else { 0 };
 
     // shouldn't
-    let x = if (cond) { Some(1) } else { None };
 
-    let x = if (cond) {
+    let x = if cond { Some(1) } else { None };
+
+    let x = if cond {
         side_effect();
         1
     } else {
         0
     };
 
-    let x = if (cond) { 2 } else { 0 };
+    let x = if cond { 2 } else { 0 };
 
     // questionable
     // maybe `!cond as usize` is reasonable
-    let x = if (cond) { 0 } else { 1 };
+    let x = if cond { 0 } else { 1 };
 }
 
 fn side_effect() {}
