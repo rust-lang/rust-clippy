fe0172f2156299fa40e0a768c690fae2f085f138
Dmitrii Lavrov <d.lavrov@proscom.ru>
2022-06-30 21:44:39 +0300

    fix tests

--- tests/ui/bool_to_int_with_if.rs
+++ tests/ui/bool_to_int_with_if.rs
@@ -6,30 +6,30 @@ fn main() {
     let cond = true;
 
     // should warn
-    let x = if cond { 1 } else { 0 };
+    let _x = if cond { 1 } else { 0 };
 
-    let x = if cond ^ true { 1 } else { 0 };
+    let _x = if cond ^ true { 1 } else { 0 };
 
     // it should be type aware
-    let x: u8 = if cond { 1 } else { 0 };
-    let x = if cond { 1i16 } else { 0i16 };
+    let _x: u8 = if cond { 1 } else { 0 };
+
+    let _x = if cond { 1i16 } else { 0i16 };
 
     // shouldn't
+    let _x = if cond { Some(1) } else { None };
 
-    let x = if cond { Some(1) } else { None };
-
-    let x = if cond {
+    let _x = if cond {
         side_effect();
         1
     } else {
         0
     };
 
-    let x = if cond { 2 } else { 0 };
+    let _x = if cond { 2 } else { 0 };
 
     // questionable
     // maybe `!cond as usize` is reasonable
-    let x = if cond { 0 } else { 1 };
+    let _x = if cond { 0 } else { 1 };
 }
 
 fn side_effect() {}
