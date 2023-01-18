989ee2d97ed46a48156e8fc70a47acaf70165f35
Niki4tap <rombiklol2@gmail.com>
2023-01-18 23:55:39 +0300

    add const test to `cmp_nan` and a fixme

--- tests/ui/cmp_nan.rs
+++ tests/ui/cmp_nan.rs
@@ -1,8 +1,14 @@
+#![warn(clippy::cmp_nan)]
+#![allow(clippy::float_cmp, clippy::no_effect, clippy::unnecessary_operation)]
+
 const NAN_F32: f32 = f32::NAN;
 const NAN_F64: f64 = f64::NAN;
 
-#[warn(clippy::cmp_nan)]
-#[allow(clippy::float_cmp, clippy::no_effect, clippy::unnecessary_operation)]
+// don't lint these, since `{f32,f64}::is_nan` is not yet const stable
+// FIXME: make this lint when `{f32,f64}::is_nan` becomes const stable
+const IS_F32_NAN: bool = NAN_F32 == f32::NAN;
+const IS_F64_NAN: bool = NAN_F64 == f64::NAN;
+
 fn main() {
     let x = 5f32;
     x == f32::NAN;
