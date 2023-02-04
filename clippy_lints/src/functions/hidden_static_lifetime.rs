edb006f41828793034d901e8a47e64248aacd4a9
blyxyas <blyxyas@gmail.com>
2023-02-04 23:16:31 +0100

    Preview of current code (Not working, revert later)

--- clippy_lints/src/functions/hidden_static_lifetime.rs
+++ clippy_lints/src/functions/hidden_static_lifetime.rs
@@ -1,9 +1,9 @@
 use crate::rustc_lint::LintContext;
-use clippy_utils::diagnostics::span_lint_and_help;
+use clippy_utils::diagnostics::{span_lint, span_lint_and_help};
 use rustc_hir::{
-    intravisit::FnKind, BareFnTy, FnDecl, FnRetTy, GenericArg, GenericArgs, GenericBound, GenericParam,
-    GenericParamKind, Generics, Lifetime, LifetimeParamKind, MutTy, ParamName, PolyTraitRef, QPath, Ty, TyKind,
-    TypeBindingKind, WherePredicate,
+    intravisit::{FnKind, Visitor},
+    BareFnTy, FnDecl, FnRetTy, GenericArg, GenericArgs, GenericBound, GenericParam, GenericParamKind, Generics,
+    Lifetime, LifetimeParamKind, MutTy, ParamName, PolyTraitRef, QPath, Ty, TyKind, TypeBindingKind, WherePredicate,
 };
 use rustc_lint::LateContext;
 use rustc_middle::lint::in_external_macro;
@@ -21,115 +21,90 @@ use super::HIDDEN_STATIC_LIFETIME;
 // NOTE: Struct's fields follow the same rules as types
 
 pub(super) fn check_fn<'tcx>(cx: &LateContext<'_>, kind: FnKind<'tcx>, decl: &'tcx FnDecl<'_>, span: Span) {
-    if !in_external_macro(cx.sess(), span) && let FnKind::ItemFn(_, generics, _) =
-kind {         let mut lifetime_is_used;
-        for generic in generics.params.iter() {
-            if let GenericParamKind::Lifetime { kind } = generic.kind &&
-			kind != LifetimeParamKind::Elided {
-				lifetime_is_used = false;
-				// Check that inputs don't use this lifetime
-				for input in decl.inputs {
-					if lifetime_is_used {
-						break;
-					}
-					// If input is reference
-					if let TyKind::Ref(lifetime, mut_ty) = &input.kind {
-						if !lifetime.is_anonymous() && lifetime.ident == generic.name.ident() {
-								lifetime_is_used = true;
-						} else {
-							lifetime_is_used = check_if_uses_lifetime(mut_ty.ty, &generic.name);
-						}
-					} else {
-						lifetime_is_used = check_if_uses_lifetime(input, &generic.name);
-					};
-				};
+    if !in_external_macro(cx.sess(), span) &&
+            let FnKind::ItemFn(_, generics, _) = kind {
 
-				if !lifetime_is_used {
-					// Check that lifetime is used in return type.
-					if let FnRetTy::Return(ret_ty) = decl.output {
-						// Check that it isn't used in `where` predicate
-						for predicate in generics.predicates {
-							// Check for generic types: `where A: 'a`
-							if let WherePredicate::BoundPredicate(bound_predicate) = predicate {
-								for bound in bound_predicate.bounds {
-									if let GenericBound::Outlives(lifetime) = bound {
-										if lifetime.ident.name == rustc_span::symbol::kw::StaticLifetime {
-											continue;
-										}
-										lifetime_is_used = true;
-									} else {
-										// Check that generic isn't X<A = B>.
-										if let GenericBound::Trait(poly_trait_ref, _) = bound {
-											for segment in poly_trait_ref.trait_ref.path.segments {
-												if let Some(gen_args) = segment.args {
-													for ty_binding in gen_args.bindings {
-														if let TypeBindingKind::Equality { .. } = ty_binding.kind {
-															lifetime_is_used = true;
-														};
-													};
-												};
-											};
-										} else {
-											span_lint_and_help(cx,
-												HIDDEN_STATIC_LIFETIME,
-												bound.span(),
-												"this lifetime can be changed to `'static`",None,
-								&format!("try removing the lifetime parameter `{}` and changing references to `'static`", generic.name.ident().as_str()));
-										};
-									};
-								};
-							} else {
-								// Check for other lifetimes
-								if let WherePredicate::RegionPredicate(region_predicate) = predicate {
-									if region_predicate.lifetime.hir_id.owner == generic.hir_id.owner {
-										lifetime_is_used = true;
-									} else {
-										span_lint_and_help(cx,
-											HIDDEN_STATIC_LIFETIME,
-											region_predicate.span,
-											"this lifetime can be changed to `'static`",
-											None,
-								&format!("try removing the lifetime parameter `{}` and changing references to `'static`", generic.name.ident().as_str()));
-									};
-								};
-							};
-						};
-
-						// Check again.
-						if !lifetime_is_used &&
-							check_validness(ret_ty, generic, generics) &&
-							lifetime_not_used_in_ty(ret_ty, generic) {
-								span_lint_and_help(cx,
-								HIDDEN_STATIC_LIFETIME,
-								generic.span,
-								"this lifetime can be changed to `'static`",
-								None,
-								&format!(
-									"try removing the lifetime parameter `{}` and changing references to `'static`",
-									generic.name.ident().as_str()),
-							);
-						};
-					};
-				};
-			};
+        let mut v = FuncVisitor {
+            ret_ty: &decl.output,
+            inputs: decl.inputs,
+			predicates: &generics.predicates,
+            lifetime_is_used: false
         };
-    };
+
+        for param in generics.params {
+            if let GenericParamKind::Lifetime { kind } = param.kind && kind != LifetimeParamKind::Elided {
+                v.visit_generic_param(param);
+                if !v.lifetime_is_used {
+                    span_lint(cx, HIDDEN_STATIC_LIFETIME, param.span, "hi");
+                };
+            }
+        };
+	}
 }
 
-fn check_if_uses_lifetime(input: &Ty<'_>, generic_name: &ParamName) -> bool {
-    if let TyKind::Path(QPath::Resolved(_, path)) = &input.kind {
+struct FuncVisitor<'a> {
+    ret_ty: &'a FnRetTy<'a>,
+    inputs: &'a [Ty<'a>],
+    predicates: &'a [WherePredicate<'a>],
+    lifetime_is_used: bool,
+}
+
+impl<'v> Visitor<'_> for FuncVisitor<'v> {
+    fn visit_generic_param(&mut self, param: &GenericParam<'_>) {
+        // Check inputs
+		for input in self.inputs {
+            if ref_uses_lifetime(input, param) || check_path(input, param) {
+				dbg!("@@@@@@@@@@@@@@@@@@@@@@@@");
+                self.lifetime_is_used = true;
+                return;
+            };
+        }
+
+        // Check return
+        if let FnRetTy::Return(ret_ty) = self.ret_ty {
+            if ref_uses_lifetime(ret_ty, param) || check_path(ret_ty, param) {
+				dbg!("============================");
+                self.lifetime_is_used = true;
+                return;
+            };
+        };
+
+        // Check predicates
+
+        for predicate in self.predicates {
+            for bound in predicate.bounds() {
+                if let GenericBound::Outlives(lifetime) = bound &&
+				lifetime.ident.name == param.name.ident().name {
+					self.lifetime_is_used = true;
+					return;
+                }
+            }
+        }
+    }
+}
+
+fn ref_uses_lifetime(mut ty: &Ty<'_>, lifetime: &GenericParam<'_>) -> bool {
+    while let TyKind::Ref(lt_ref, mut_ty) = &ty.kind {
+		if lt_ref.ident.name == lifetime.name.ident().name && mut_ty.mutbl.is_not() {
+			return true;
+        } else {
+            ty = mut_ty.ty;
+        }
+    }
+    false
+}
+
+fn check_path(ty: &Ty<'_>, lifetime: &GenericParam<'_>) -> bool {
+    if let TyKind::Path(QPath::Resolved(_, path)) = ty.peel_refs().kind {
         for segment in path.segments {
             for arg in segment.args().args {
-                // If input's lifetime and function's are the same.
-                if let GenericArg::Lifetime(lifetime) = arg {
-                    if lifetime.is_anonymous() {
+                if let GenericArg::Lifetime(lt_arg) = arg {
+                    if lt_arg.ident.name == lifetime.name.ident().name {
                         return true;
-                    }
-                    if let ParamName::Plain(ident) = generic_name {
-                        if lifetime.ident.name == ident.name {
-                            return true;
-                        };
                     };
+                } else if let &GenericArg::Type(ty) = arg {
+					dbg!("!!!!!!!!!!!!!!!!!!!!!!!!!");
+                    return check_all_types(ty, lifetime);
                 };
             }
         }
@@ -137,55 +112,16 @@ fn check_if_uses_lifetime(input: &Ty<'_>, generic_name: &ParamName) -> bool {
     false
 }
 
-fn check_validness(ret_ty: &Ty<'_>, generic: &GenericParam<'_>, generics: &Generics<'_>) -> bool {
-    // (#10123) Comment by @Jarcho explains what "valid" means.
-    // The lint doesn't check invalid return types, because not every lifetime can be changed to 'static
-    // without problems.
-    if let TyKind::BareFn(barefn) = ret_ty.peel_refs().kind {
-        for input in barefn.decl.inputs {
-            if let TyKind::Ref(lifetime, _) = input.kind {
-                if lifetime.ident.name == generic.name.ident().name {
-                    return false;
-                }
-            }
-        }
-    }
-
-    if let TyKind::Ref(_, mut_ty) = &ret_ty.kind {
-        // Check for &'a mut &'b T
-        if mut_ty.mutbl.is_mut() {
-            // This path diverges:
-            // * &'a mut &'b T : Not valid
-            // * &'a mut T : Only if T: 'static (Checked before)
-            if let TyKind::Ref(_, _) = &mut_ty.ty.kind {
-                return false;
-            };
-
-            for predicate in generics.predicates {
-                for bound in predicate.bounds() {
-                    if let GenericBound::Outlives(lifetime) = bound {
-                        if !lifetime.is_static() {
-                            return false;
-                        }
-                    }
-                }
-            }
-        }
-    };
-    true
-}
-
-// true = lifetime isn't used (success)
-fn lifetime_not_used_in_ty(ret_ty: &Ty<'_>, generic: &GenericParam<'_>) -> bool {
+fn check_all_types(ty: &Ty<'_>, lifetime: &GenericParam<'_>) -> bool {
     fn ty_uses_lifetime(ty: &Ty<'_>, generic: &GenericParam<'_>) -> bool {
         if let TyKind::Path(QPath::Resolved(_, path)) = ty.kind {
             for segment in path.segments {
                 if let Some(GenericArgs { args, .. }) = segment.args {
                     for arg in args.iter() {
                         if let GenericArg::Lifetime(lifetime) = arg {
-                            if lifetime.ident.name == generic.name.ident().name {
+							if lifetime.ident.name == generic.name.ident().name {
                                 // generic is used
-                                return false;
+                                return true;
                             }
                         } else if let GenericArg::Type(ty) = arg {
                             return classify_ty(ty, generic);
@@ -194,16 +130,24 @@ fn lifetime_not_used_in_ty(ret_ty: &Ty<'_>, generic: &GenericParam<'_>) -> bool 
                 }
             }
         }
-        true
+        false
     }
 
     #[inline]
     fn barefn_uses_lifetime(barefn: &BareFnTy<'_>, generic: &GenericParam<'_>) -> bool {
-        for param in barefn.generic_params {
-            if param.def_id == generic.def_id {
+        // Check inputs
+        for input in barefn.decl.inputs {
+            if ref_uses_lifetime(input, generic) || check_path(input, generic) {
                 return false;
-            }
+            };
         }
+
+        // Check return
+        if let FnRetTy::Return(ret_ty) = barefn.decl.output {
+            if check_path(ret_ty, generic) {
+                return false;
+            };
+        };
         true
     }
 
@@ -217,13 +161,13 @@ fn lifetime_not_used_in_ty(ret_ty: &Ty<'_>, generic: &GenericParam<'_>) -> bool 
             if let GenericArg::Lifetime(lifetime) = arg {
                 if lifetime.ident.name == generic.name.ident().name {
                     // generic is used
-                    return false;
+                    return true;
                 }
             } else if let GenericArg::Type(ty) = arg {
                 return classify_ty(ty, generic);
             }
         }
-        true
+        false
     }
 
     #[inline]
@@ -236,10 +180,10 @@ fn lifetime_not_used_in_ty(ret_ty: &Ty<'_>, generic: &GenericParam<'_>) -> bool 
         } in traits
         {
             if bound_generic_params.iter().any(|param| param.def_id == generic.def_id) {
-                return false;
+                return true;
             };
         }
-        true
+        false
     }
 
     #[inline]
@@ -255,26 +199,14 @@ fn lifetime_not_used_in_ty(ret_ty: &Ty<'_>, generic: &GenericParam<'_>) -> bool 
             	traitobject_uses_lifetime(lifetime, poly_trait_ref, generic),
             TyKind::Typeof(_) // This is unused for now, this needs revising when Typeof is used.
             | TyKind::Err
-            | TyKind::Ref(_, _) /* This will never be the case*/
-            | TyKind::Never => true,
-            TyKind::Infer => false,
+            | TyKind::Never => false,
+			TyKind::Ref(_, MutTy { ty, ..}) => ref_uses_lifetime(ty, generic),
+            TyKind::Infer => true,
         }
     }
 
     // Separate refs from ty.
-    let mut can_change = false;
-    let mut final_ty = ret_ty;
-    while let TyKind::Ref(lifetime, MutTy { ty, .. }) = &final_ty.kind {
-        if lifetime.ident.name == generic.name.ident().name {
-            can_change = true;
-        };
-        final_ty = ty;
-    }
 
-    // Now final_ty is equivalent to ret_ty.peel_refs
-
-    if can_change {
-        return classify_ty(final_ty, generic);
-    }
-    false
+    // Now final_ty is equivalent to ty.peel_refs
+    return classify_ty(ty.peel_refs(), lifetime);
 }
