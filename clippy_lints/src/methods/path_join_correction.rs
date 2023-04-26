412dfa6508bced40091e69c7f817ad3d684e3bc6
Mason Ray Hanna III <mhanna0000@gmail.com>
2023-04-26 11:52:55 -0400

    Build fix attempt #2.

--- clippy_lints/src/methods/path_join_correction.rs
+++ clippy_lints/src/methods/path_join_correction.rs
@@ -9,17 +9,17 @@ use super::PATH_JOIN_CORRECTION;
 
 pub(super) fn check<'tcx>(cx: &LateContext<'tcx>, expr: &'tcx Expr<'tcx>, join_arg: &'tcx Expr<'tcx>, span: Span) {
     let ty = cx.typeck_results().expr_ty(expr);
-    if is_type_diagnostic_item(cx, ty, Path) {
-        let applicability = Applicability::MachineApplicable;
-        if_chain!(
-         if let ExprKind::Lit(spanned) = &join_arg.kind;
-         if let LitKind::Str(symbol, _) = spanned.node;
-         if symbol.as_str().starts_with('/') || symbol.as_str().starts_with('\\');
-          then {
-           span_lint_and_sugg(
-           cx,
-           PATH_JOIN_CORRECTION,
-           span.with_hi(expr.span.hi()),
+    if_chain!(
+    if is_type_diagnostic_item(cx, ty, Path);
+    let applicability = Applicability::MachineApplicable;
+    if let ExprKind::Lit(spanned) = &join_arg.kind;
+    if let LitKind::Str(symbol, _) = spanned.node;
+    if symbol.as_str().starts_with('/') || symbol.as_str().starts_with('\\');
+     then {
+      span_lint_and_sugg(
+      cx,
+      PATH_JOIN_CORRECTION,
+      span.with_hi(expr.span.hi()),
            r#"argument in join called on path contains a starting '/'"#,
            "try removing first '/' or '\\'",
            "join(\"your/path/here\")".to_owned(),
@@ -27,5 +27,4 @@ pub(super) fn check<'tcx>(cx: &LateContext<'tcx>, expr: &'tcx Expr<'tcx>, join_a
          );
         }
         );
-    }
 }
