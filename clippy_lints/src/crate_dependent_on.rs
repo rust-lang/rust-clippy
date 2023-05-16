5004d7eae36b4d653abaa88bac6575d7058cc3d8
Centri3 <114838443+Centri3@users.noreply.github.com>
2023-05-16 11:46:57 -0500

    betterish error message?

--- clippy_lints/src/crate_dependent_on.rs
+++ clippy_lints/src/crate_dependent_on.rs
@@ -1,5 +1,7 @@
+use clippy_utils::diagnostics::span_lint;
 use clippy_utils::{diagnostics::span_lint_and_help, is_no_core_crate, is_no_std_crate};
 use rustc_lint::{LateContext, LateLintPass, Lint};
+use rustc_session::cstore::ExternCrateSource;
 use rustc_session::{declare_lint_pass, declare_tool_lint};
 use rustc_span::DUMMY_SP;
 use rustc_span::{sym, Span, Symbol};
@@ -66,9 +68,8 @@ impl LateLintPass<'_> for CrateDependentOn {
         // it cannot find multiple dependencies at once and cannot find the true name of the
         // dependency if it's not depended on by the crate directly
 
-        let (mut std, mut core) = (vec![], vec![]);
-        let mut std_present = false;
-        let mut core_present = false;
+        let mut std = vec![];
+        let mut core = vec![];
 
         let tcx = cx.tcx;
         for krate in tcx.crates(()) {
@@ -78,41 +79,27 @@ impl LateLintPass<'_> for CrateDependentOn {
                 let dep_name = tcx.crate_name(ext.dependency_of);
 
                 if krate_name == sym::std {
-                    std_present = true;
-                    std.push((dep_name, ext.span));
+                    std.push((ext, ext.span));
                 } else if krate_name == sym::core {
-                    core_present = true;
-                    core.push((dep_name, ext.span));
+                    core.push((ext, ext.span));
                 }
             }
         }
 
-        // TODO: There's quite a bit of code duplication below here
-
-        if is_no_std_crate(cx) {
-            if std.is_empty() && std_present {
-                report_crate(cx, NO_STD_CRATE_STD_DEPENDENT, "#![no_std]");
-            }
-
-            for (dep_name, dep_span) in std {
-                report_dependent_crate(cx, Some(dep_span), NO_STD_CRATE_STD_DEPENDENT, "#![no_std]", dep_name);
-            }
-        }
-
-        if is_no_core_crate(cx) {
-            if core.is_empty() && core_present {
-                report_crate(cx, NO_CORE_CRATE_CORE_DEPENDENT, "#![no_core]");
-            }
-
-            for (dep_name, dep_span) in core {
-                report_dependent_crate(
-                    cx,
-                    Some(dep_span),
-                    NO_CORE_CRATE_CORE_DEPENDENT,
-                    "#![no_core]",
-                    dep_name,
-                );
-            }
+        for (krate, span) in std {
+            span_lint(
+                cx,
+                NO_STD_CRATE_STD_DEPENDENT,
+                span,
+                &format!(
+                    "lol {krate:#?} {}",
+                    if let ExternCrateSource::Extern(def_id) = krate.src {
+                        format!("{:?}", tcx.crate_name(def_id.krate))
+                    } else {
+                        format!("no")
+                    }
+                ),
+            );
         }
     }
 }
