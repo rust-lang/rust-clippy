74e79b33c4c76de241b184bb22a0c6dfa316cbc5
Centri3 <114838443+Centri3@users.noreply.github.com>
2023-05-16 12:49:14 -0500

    update tests

--- tests/ui/auxiliary/fake_no_std.rs
+++
@@ -1,8 +0,0 @@
-//@aux-build:std_dependent_crate2.rs
-#![allow(unused)]
-#![no_std]
-#![no_main]
-
-extern crate std_dependent_crate2;
-
-use core::panic::PanicInfo;
--- tests/ui/no_std_crate_std_dependent.rs
+++ tests/ui/no_std_crate_std_dependent.rs
@@ -1,10 +1,7 @@
-//@aux-build:fake_no_std.rs
-//@aux-build:std_dependent_crate1.rs
+//@aux-build:std_dependent_crate2.rs
 #![allow(unused)]
 #![warn(clippy::no_std_crate_std_dependent)]
 #![no_std]
 #![no_main]
 
-// not actually no_std
-extern crate fake_no_std;
-extern crate std_dependent_crate1;
+extern crate std_dependent_crate2;
