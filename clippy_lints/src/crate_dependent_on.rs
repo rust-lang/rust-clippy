1e24da38ea6bc98b6bbba688168fc76dc5deb2c0
Centri3 <114838443+Centri3@users.noreply.github.com>
2023-06-01 16:22:53 -0500

    finish implementation

--- clippy_lints/src/crate_dependent_on.rs
+++ clippy_lints/src/crate_dependent_on.rs
@@ -1,17 +1,16 @@
-use clippy_utils::diagnostics::span_lint_and_then;
+use clippy_utils::diagnostics::span_lint;
 use clippy_utils::{is_no_core_crate, is_no_std_crate};
 use rustc_lint::{LateContext, LateLintPass, Lint};
 use rustc_session::cstore::ExternCrate;
 use rustc_session::{declare_lint_pass, declare_tool_lint};
 use rustc_span::sym;
 
-// TODO: The example
 declare_clippy_lint! {
     /// ### What it does
-    /// Checks for dependencies which depend on `std` in `#![no_std]` crates.
+    /// Checks if a `#![no_std]` crate depends on `std`.
     ///
     /// ### Why is this bad?
-    /// The dependency will pull in `std` regardless, defeating the whole point of `#![no_std]`.
+    /// It will pull in `std` regardless, defeating the whole point of `#![no_std]`.
     ///
     /// ### Example
     /// ```rust,ignore
@@ -20,12 +19,6 @@ declare_clippy_lint! {
     /// // this is unlikely to be what you want!
     /// use i_depend_on_std::*;
     /// ```
-    /// Use instead:
-    /// ```rust,ignore
-    /// #![no_std]
-    ///
-    /// // same functionality but without std
-    /// use i_dont_depend_on_std::*;
     /// ```
     #[clippy::version = "1.71.0"]
     pub NO_STD_CRATE_STD_DEPENDENT,
@@ -33,13 +26,12 @@ declare_clippy_lint! {
     "disallow depending on std in `#![no_std]` crates"
 }
 
-// TODO: Tests and the example
 declare_clippy_lint! {
     /// ### What it does
-    /// Checks for dependencies which pull in `core` in `#![no_core]` crates.
+    /// Checks if a `#![no_core]` crate depends on `core`.
     ///
     /// ### Why is this bad?
-    /// The dependency will pull in core regardless, defeating the whole point of `#![no_core]`.
+    /// It will pull in `std` regardless, defeating the whole point of `#![no_core]`.
     ///
     /// ### Example
     /// ```rust,ignore
@@ -47,13 +39,6 @@ declare_clippy_lint! {
     ///
     /// // this is unlikely to be what you want!
     /// use i_depend_on_core::*;
-    /// ```
-    /// Use instead:
-    /// ```rust,ignore
-    /// #![no_core]
-    ///
-    /// // same functionality but without core
-    /// use i_dont_depend_on_core::*;
     /// ```
     #[clippy::version = "1.71.0"]
     pub NO_CORE_CRATE_CORE_DEPENDENT,
@@ -66,9 +51,12 @@ declare_lint_pass!(CrateDependentOn => [NO_STD_CRATE_STD_DEPENDENT, NO_CORE_CRAT
 impl LateLintPass<'_> for CrateDependentOn {
     fn check_crate(&mut self, cx: &LateContext<'_>) {
         let (no_std, no_core) = (is_no_std_crate(cx), is_no_core_crate(cx));
-        // they will only be present once
-        let mut std = Vec::with_capacity(1);
-        let mut core = Vec::with_capacity(1);
+        // afaik, an extern crate will only appear once in `tcx.crates` so we don't need to handle a case
+        // where there are multiple of them. This unfortunately also means we cannot reliably trace it back
+        // to find the dependency that the original crate depends on, that inadvertently depends on
+        // std and/or core.
+        let mut std = None;
+        let mut core = None;
 
         if !no_std && !no_core {
             return;
@@ -79,33 +67,31 @@ impl LateLintPass<'_> for CrateDependentOn {
             if let Some(ext) = tcx.extern_crate(krate.as_def_id()) {
                 let krate_name = tcx.crate_name(*krate);
                 if krate_name == sym::std {
-                    std.push(*ext);
+                    std.get_or_insert(ext);
                 } else if krate_name == sym::core {
-                    core.push(*ext);
+                    core.get_or_insert(ext);
                 }
             }
         }
 
-        if no_std {
-            check_dependent(cx, NO_STD_CRATE_STD_DEPENDENT, &std, "#![no_std]");
-        }
-        if no_core {
-            check_dependent(cx, NO_CORE_CRATE_CORE_DEPENDENT, &core, "#![no_core]");
-        }
+        check_dependent(cx, NO_STD_CRATE_STD_DEPENDENT, std, "#![no_std]", no_std);
+        check_dependent(cx, NO_CORE_CRATE_CORE_DEPENDENT, core, "#![no_core]", no_core);
     }
 }
 
-fn check_dependent(cx: &LateContext<'_>, lint: &'static Lint, exts: &Vec<ExternCrate>, attr: &'static str) {
-    for ext in exts {
-        span_lint_and_then(
+fn check_dependent(
+    cx: &LateContext<'_>,
+    lint: &'static Lint,
+    ext: Option<&ExternCrate>,
+    attr: &'static str,
+    attr_set: bool,
+) {
+    if attr_set && let Some(ext) = ext {
+        span_lint(
             cx,
             lint,
             ext.span,
             &format!("`{attr}` crate depends on a crate that isn't `{attr}`"),
-            |diag| {
-                diag.help("find and remove the offending dependency");
-                diag.span_help(ext.span, "try removing this dependency");
-            },
         );
     }
 }
