aa4fab52310599e8c833e74789c751cb0e6f9c24
Guillaume Gomez <guillaume1.gomez@gmail.com>
2023-06-05 14:49:20 +0200

    Extend `borrow_as_ptr` test

--- tests/ui/borrow_as_ptr.rs
+++ tests/ui/borrow_as_ptr.rs
@@ -4,7 +4,9 @@
 fn main() {
     let val = 1;
     let _p = &val as *const i32;
+    let _p = (&val as *const i32) as *const u32;
 
     let mut val_mut = 1;
     let _p_mut = &mut val_mut as *mut i32;
+    let _p_mut = (&mut val_mut as *mut i32) as *mut u32;
 }
