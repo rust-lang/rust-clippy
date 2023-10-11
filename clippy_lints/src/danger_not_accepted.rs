a01a03a46fdb2ae50933cd3412580f251b67353b
Radbuglet <10578060+Radbuglet@users.noreply.github.com>
2023-10-11 08:37:57 -0700

    allow `#[dangerous]` to specify a `reason`

--- clippy_lints/src/danger_not_accepted.rs
+++ clippy_lints/src/danger_not_accepted.rs
@@ -11,12 +11,14 @@ use rustc_span::{Span, Symbol};
 
 // Future improvements:
 //
-// - Allow users to override modules as *not* having a specific danger.
+// - Allow users to override modules as *not* posing a specific danger.
 // - Allow users to specify additional dangerous items in the clippy config.
 // - Devise a scheme (maybe path compression?) to reduce the amount of ancestry tracing we have to
 //   do to determine the dangers posed by a method.
-// - Implement a way to forbid `accept_danger` in a given module.
-// - Allow `accept_danger` and `dangerous` to be used as inner attributes on stable Rust.
+// - Implement a way to forbid additional `accept_danger` calls in a given module.
+// - Allow `accept_danger` and `dangerous` to be used as inner attributes on stable Rust. As
+//   discussed, this would likely involve granting the clippy attribute namespace special status,
+//   similar to `#[allow(...)]`.
 //
 
 declare_clippy_lint! {
@@ -124,7 +126,10 @@ impl LateLintPass<'_> for DangerNotAccepted {
         // Both `accept_danger` and `dangerous` contribute to the accepted danger map.
         for attr_name in ["accept_danger", "dangerous"] {
             for attr in get_attr(cx.sess(), attrs, attr_name) {
-                let dangers = parse_dangers_attr(cx, attr);
+                // We don't really care about why a user might have accepted a danger or marked a
+                // section as dangerous.
+                let (dangers, _ignored_reason) = parse_dangers_attr(cx, attr);
+
                 for (_span, danger) in dangers {
                     *self.accepted_dangers.entry(danger).or_default() += 1;
                 }
@@ -136,7 +141,7 @@ impl LateLintPass<'_> for DangerNotAccepted {
         // Both `accept_danger` and `dangerous` contribute to the accepted danger map.
         for attr_name in ["accept_danger", "dangerous"] {
             for attr in get_attr(cx.sess(), attrs, attr_name) {
-                let dangers = parse_dangers_attr(cx, attr);
+                let (dangers, _ignored_reason) = parse_dangers_attr(cx, attr);
                 for (_span, danger) in dangers {
                     match self.accepted_dangers.entry(danger) {
                         StdEntry::Occupied(mut entry) => {
@@ -153,27 +158,38 @@ impl LateLintPass<'_> for DangerNotAccepted {
     }
 }
 
+struct UnacceptedDanger {
+    span: Span,
+    id: Symbol,
+    reason: Option<token::Lit>,
+}
+
 impl DangerNotAccepted {
-    fn get_unaccepted_dangers(&self, cx: &LateContext<'_>, item_id: def_id::DefId) -> Option<Vec<(Span, Symbol)>> {
+    fn get_unaccepted_dangers(&self, cx: &LateContext<'_>, item_id: def_id::DefId) -> Option<Vec<UnacceptedDanger>> {
         let mut unaccepted_dangers = Vec::new();
         let mut item_iter = Some(item_id);
 
         while let Some(item_id) = item_iter {
             item_iter = cx.tcx.opt_parent(item_id);
 
-            // HACK: Ensure that this is not an intrinsic because calling `get_attrs_unchecked` on
-            //  a foreign module breaks everything.
+            // HACK: Ensure that this is not a foreign module because calling `get_attrs_unchecked` on
+            //  an intrinsic foreign module breaks everything.
             if cx.tcx.def_kind(item_id) == def::DefKind::ForeignMod {
                 continue;
             }
 
             for attr in get_attr(cx.sess(), cx.tcx.get_attrs_unchecked(item_id), "dangerous") {
-                for danger in parse_dangers_attr(cx, attr) {
-                    if self.accepted_dangers.contains_key(&danger.1) {
+                let (dangers, danger_reason) = parse_dangers_attr(cx, attr);
+                for (danger_span, danger_id) in dangers {
+                    if self.accepted_dangers.contains_key(&danger_id) {
                         continue;
                     }
 
-                    unaccepted_dangers.push(danger);
+                    unaccepted_dangers.push(UnacceptedDanger {
+                        span: danger_span,
+                        id: danger_id,
+                        reason: danger_reason,
+                    });
                 }
             }
         }
@@ -182,64 +198,146 @@ impl DangerNotAccepted {
     }
 }
 
-fn parse_dangers_attr(cx: &LateContext<'_>, attr: &ast::Attribute) -> Vec<(Span, Symbol)> {
-    const EXPECTATION: &str = "Expected a delimited attribute with a list of danger identifiers.";
+fn parse_dangers_attr(cx: &LateContext<'_>, attr: &ast::Attribute) -> (Vec<(Span, Symbol)>, Option<token::Lit>) {
+    const EXPECTATION: &str = "expected a delimited attribute with a list of danger identifiers";
+    const NOTHING_AFTER_ERR: &str = "nothing should come after the reason attribute besides an optional comma";
 
     let span = attr.span;
+    let reason_sym = sym!(reason);
 
+    // Expect a normal non doc-comment attribute.
     let rustc_ast::AttrKind::Normal(attr) = &attr.kind else {
         cx.sess().span_err(span, EXPECTATION);
-        return Vec::new();
+        return (Vec::new(), None);
     };
 
+    // Expect it to be a delimited attribute of the form #[attr(...)] and not #[attr {...}]
     let ast::AttrArgs::Delimited(attr) = &attr.item.args else {
         cx.sess().span_err(span, EXPECTATION);
-        return Vec::new();
+        return (Vec::new(), None);
     };
 
     if attr.delim != token::Delimiter::Parenthesis {
         cx.sess().span_err(span, EXPECTATION);
-        return Vec::new();
+        return (Vec::new(), None);
     }
 
+    // Parse the attribute arguments
     let mut stream = attr.tokens.trees();
     let mut dangers = Vec::new();
+    let mut specified_reason = None;
 
     loop {
+        // Expect an identifier
         let sym = match stream.next() {
-            Some(tokenstream::TokenTree::Token(sym, _)) => sym,
+            Some(tokenstream::TokenTree::Token(sym, _)) if let Some((sym, _)) = sym.ident() => sym,
+            // An EOS is also valid here.
             None => break,
-            _ => {
-                cx.sess().span_err(span, EXPECTATION);
-                return Vec::new();
-            },
-        };
 
-        let Some((sym, _)) = sym.ident() else {
-            cx.sess().span_err(span, EXPECTATION);
-            return Vec::new();
-        };
+            // Otherwise, raise an error.
+            Some(tokenstream::TokenTree::Token(sym, _)) => {
+                cx.sess().span_err(sym.span, format!("{EXPECTATION}; this was not an identifier"));
 
-        dangers.push((sym.span, sym.name));
-
-        match stream.next() {
-            Some(tokenstream::TokenTree::Token(sym, _)) if sym.kind == token::TokenKind::Comma => {
-                continue;
-            },
-            None => {
-                break;
+                return (Vec::new(), None);
             },
             _ => {
                 cx.sess().span_err(span, EXPECTATION);
-                return Vec::new();
+                return (Vec::new(), None);
             },
+        };
+
+        // If the identifier is not "reason", add it as a danger
+        #[allow(clippy::if_not_else, reason = "it is clearer to put the common path first")]
+        if sym.name != reason_sym {
+            // Push it to the danger list
+            dangers.push((sym.span, sym.name));
+
+            // If we find a comma, continue. If we find an EOS for the inner stream, break.
+            match stream.next() {
+                // If we find an equality sign, continue.
+                Some(tokenstream::TokenTree::Token(sym, _)) if sym.kind == token::TokenKind::Comma => {
+                    continue;
+                },
+                None => break,
+                // Otherwise, raise an error.
+                Some(tokenstream::TokenTree::Token(sym, _)) => {
+                    cx.sess()
+                        .span_err(sym.span, format!("{EXPECTATION}; this was not a comma delimiter"));
+                },
+                _ => {
+                    cx.sess().span_err(span, EXPECTATION);
+                    return (Vec::new(), None);
+                },
+            }
+        } else {
+            // If the identifier was "reason", expect an equality sign.
+            let eq_tok = match stream.next() {
+                // If we find a comma, continue.
+                Some(tokenstream::TokenTree::Token(tok, _)) if tok.kind == token::TokenKind::Eq => tok,
+                // Otherwise, raise an error.
+                Some(tokenstream::TokenTree::Token(tok, _)) => {
+                    cx.sess().span_err(tok.span, "expected = after a reason attribute");
+                    return (Vec::new(), None);
+                },
+                _ => {
+                    cx.sess().span_err(sym.span, "expected = after a reason attribute");
+                    return (Vec::new(), None);
+                },
+            };
+
+            // ...then, expect a string literal.
+            let (reason_tok, reason_lit) = match stream.next() {
+                // If we find a string literal, continue.
+                Some(tokenstream::TokenTree::Token(tok, _))
+                if
+                    let token::TokenKind::Literal(lit) = tok.kind &&
+                    // We don't admit byte string literals because they wouldn't be `&str`s in regular
+                    // scenarios so why treat them as strings here?
+                    let token::LitKind::Str | token::LitKind::StrRaw(_) |
+                        token::LitKind::CStr | token::LitKind::CStrRaw(_) = lit.kind
+                => (tok, lit),
+                // Otherwise, raise an error.
+                Some(tokenstream::TokenTree::Token(tok, _)) => {
+                    cx.sess().span_err(tok.span, "expected a string literal after a reason attribute");
+                    return (Vec::new(), None);
+                },
+                _ => {
+                    cx.sess().span_err(eq_tok.span, "expected a string literal after a reason attribute");
+                    return (Vec::new(), None);
+                },
+            };
+
+            specified_reason = Some(reason_lit);
+
+            // Finally, because the reason must be specified as the last attribute, expect either an
+            // optional comma or an EOS and break.
+            match stream.next() {
+                Some(tokenstream::TokenTree::Token(tok, _)) if tok.kind == token::TokenKind::Comma => {
+                    // The token after the comma must be an EOS
+                    if stream.next().is_some() {
+                        cx.sess().span_err(tok.span, NOTHING_AFTER_ERR);
+                    }
+                },
+                None => {},
+                // Otherwise, raise an error.
+                Some(tokenstream::TokenTree::Token(tok, _)) => {
+                    cx.sess().span_err(tok.span, NOTHING_AFTER_ERR);
+                    return (Vec::new(), None);
+                },
+                _ => {
+                    cx.sess().span_err(reason_tok.span, NOTHING_AFTER_ERR);
+                    return (Vec::new(), None);
+                },
+            }
+
+            break;
         }
     }
 
-    dangers
+    (dangers, specified_reason)
 }
 
-fn emit_dangerous_call_lint(cx: &LateContext<'_>, expr: &'_ Expr<'_>, unaccepted_dangers: &[(Span, Symbol)]) {
+fn emit_dangerous_call_lint(cx: &LateContext<'_>, expr: &'_ Expr<'_>, unaccepted_dangers: &[UnacceptedDanger]) {
     // Define formatting helpers
     struct FmtInline<F>(F);
 
@@ -260,7 +358,10 @@ fn emit_dangerous_call_lint(cx: &LateContext<'_>, expr: &'_ Expr<'_>, unaccepted
     }
 
     // Collect all unique dangers
-    let unique_dangers = unaccepted_dangers.iter().map(|(_, sym)| sym).collect::<FxHashSet<_>>();
+    let unique_dangers = unaccepted_dangers
+        .iter()
+        .map(|danger| danger.id)
+        .collect::<FxHashSet<_>>();
 
     // Create a lint
     span_lint_and_then(
@@ -283,8 +384,19 @@ fn emit_dangerous_call_lint(cx: &LateContext<'_>, expr: &'_ Expr<'_>, unaccepted
             }),
         ),
         |diag| {
-            for (danger_span, danger_name) in unaccepted_dangers {
-                diag.span_note(*danger_span, format!("danger `{danger_name}` declared here"));
+            for danger in unaccepted_dangers {
+                if let Some(reason) = danger.reason {
+                    diag.span_note(
+                        danger.span,
+                        format!(
+                            "danger `{}` declared here with the justification `{}`",
+                            danger.id,
+                            reason.symbol.as_str(),
+                        ),
+                    );
+                } else {
+                    diag.span_note(danger.span, format!("danger `{}` declared here", danger.id));
+                }
             }
         },
     );
--- tests/ui/danger_not_accepted.rs
+++ tests/ui/danger_not_accepted.rs
@@ -37,11 +37,14 @@ struct Maz;
 mod waz {
     pub fn woo() {}
 
-    #[clippy::dangerous(may_deadlock)]
+    #[clippy::dangerous(may_deadlock, reason = "your program may deadlock in calling this function")]
     pub fn woo2() {}
 
     impl super::Maz {
-        #[clippy::dangerous(may_delete_system)]
+        #[clippy::dangerous(
+            may_delete_system,
+            reason = "calling this has a very strong chance of just deleting your computer"
+        )]
         pub fn faz(&self) {}
     }
 
@@ -54,3 +57,22 @@ trait FazTrait {
     #[clippy::dangerous(may_delete_system)]
     fn faz2(&self);
 }
+
+// Edge case attr tests
+#[rustfmt::skip]
+#[clippy::dangerous(whee, woo,)]
+#[clippy::dangerous(whee, woo, reason = "sdfhsdf",)]
+fn dummy_1() {}
+
+// Invalid attr tests
+#[clippy::dangerous{}]
+#[clippy::dangerous[]]
+#[clippy::dangerous(,)]
+#[clippy::dangerous(whee, reason)]
+#[clippy::dangerous(whee, reason, abc)]
+#[clippy::dangerous(whee, reason =)]
+#[clippy::dangerous(whee, reason =, weh)]
+#[clippy::dangerous(whee, reason = "", weh)]
+#[clippy::dangerous(whee, reason = "" weh)]
+#[clippy::dangerous(whee, reason = 4)]
+fn dummy_2() {}
