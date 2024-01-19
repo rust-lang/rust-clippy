d5708800f12d1095f0fa12f5d22c548ffec09cde
Guillaume Gomez <guillaume1.gomez@gmail.com>
2024-01-19 12:25:35 +0100

    Update ui tests for `unusual_byte_groupings`

--- tests/ui/large_digit_groups.rs
+++ tests/ui/large_digit_groups.rs
@@ -16,14 +16,22 @@ fn main() {
         1_234.12_f32,
         1_234.123_f32,
         1.123_4_f32,
+        0b000_00_11_0,
+        0b0_00_00_110,
     );
     let _bad = (
         0b1_10110_i64,
+        //~^ ERROR: digit groups should be smaller
         0xd_e_adbee_f_usize,
+        //~^ ERROR: digits of hex, binary or octal literal not in groups of equal size
         1_23456_f32,
+        //~^ ERROR: digit groups should be smaller
         1_23456.12_f32,
+        //~^ ERROR: digit groups should be smaller
         1_23456.12345_f64,
+        //~^ ERROR: digit groups should be smaller
         1_23456.12345_6_f64,
+        //~^ ERROR: digit groups should be smaller
     );
     // Ignore literals in macros
     let _ = mac!();
