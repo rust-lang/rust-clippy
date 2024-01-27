96d30a0ebf2e62f114f377e7790c36edc5fc1f98
Philippe Cholet <44676486+Philippe-Cholet@users.noreply.github.com>
2024-01-27 15:42:24 +0000

    `missing_iterator_fold`: describe the lint

--- clippy_lints/src/missing_iterator_fold.rs
+++ clippy_lints/src/missing_iterator_fold.rs
@@ -4,21 +4,47 @@ use rustc_session::declare_lint_pass;
 
 declare_clippy_lint! {
     /// ### What it does
+    /// Checks for `Iterator` implementations not specializing `fold`.
     ///
     /// ### Why is this bad?
+    /// Specialize `fold` might provide more performant internal iteration.
+    /// Methods relying on `fold` would then be faster as well.
+    /// By default, methods that consume the entire iterator rely on it such as `for_each`, `count`...
     ///
     /// ### Example
     /// ```no_run
-    /// // example code where clippy issues a warning
+    /// struct MyIter(u32);
+    ///
+    /// impl Iterator for MyIter {
+    ///     type Item = u32;
+    ///     fn next(&mut self) -> Option<Self::Item> {
+    ///         # todo!()
+    ///         // ...
+    ///     }
+    /// }
     /// ```
     /// Use instead:
     /// ```no_run
-    /// // example code which does not raise clippy warning
+    /// struct MyIter(u32);
+    ///
+    /// impl Iterator for MyIter {
+    ///     type Item = u32;
+    ///     fn next(&mut self) -> Option<Self::Item> {
+    ///         # todo!()
+    ///         // ...
+    ///     }
+    ///     fn fold<B, F>(self, init: B, f: F) -> B
+    ///     where
+    ///         F: FnMut(B, Self::Item) -> B,
+    ///     {
+    ///         todo!()
+    ///     }
+    /// }
     /// ```
     #[clippy::version = "1.77.0"]
     pub MISSING_ITERATOR_FOLD,
     nursery,
-    "default lint description"
+    "a missing `Iterator::fold` specialization"
 }
 
 declare_lint_pass!(MissingIteratorFold => [MISSING_ITERATOR_FOLD]);
