4e8fc93fd089c761ea1d4412a02cf9cc64c10ec5
Philippe Cholet <44676486+Philippe-Cholet@users.noreply.github.com>
2024-01-27 17:43:10 +0000

    `missing_iterator_fold`: test the lint

--- tests/ui/missing_iterator_fold.rs
+++ tests/ui/missing_iterator_fold.rs
@@ -1,5 +1,12 @@
 #![warn(clippy::missing_iterator_fold)]
 
-fn main() {
-    // test code goes here
+struct Countdown(u8);
+
+impl Iterator for Countdown {
+    type Item = u8;
+
+    fn next(&mut self) -> Option<u8> {
+        self.0 = self.0.checked_sub(1)?;
+        Some(self.0)
+    }
 }
