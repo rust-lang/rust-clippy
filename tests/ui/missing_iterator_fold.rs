496139e0ff20e981c4419db23c5cc4064ef59722
Philippe-Cholet <44676486+Philippe-Cholet@users.noreply.github.com>
2024-01-30 19:17:55 +0100

    Test it does not lint when `fold` is specialized

--- tests/ui/missing_iterator_fold.rs
+++ tests/ui/missing_iterator_fold.rs
@@ -10,3 +10,21 @@ impl Iterator for Countdown {
         Some(self.0)
     }
 }
+
+struct Countdown2(u8);
+
+impl Iterator for Countdown2 {
+    type Item = u8;
+
+    fn next(&mut self) -> Option<u8> {
+        self.0 = self.0.checked_sub(1)?;
+        Some(self.0)
+    }
+
+    fn fold<B, F>(self, init: B, f: F) -> B
+    where
+        F: FnMut(B, Self::Item) -> B,
+    {
+        (0..self.0).rfold(init, f)
+    }
+}
