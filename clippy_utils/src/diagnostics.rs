136f5140f043fc1cb51c4f8cd6252d84dbefcb20
TheBlckbird <theblckbrd@proton.me>
2024-02-05 19:09:43 +0100

    Add markdown link to diagnostic

--- clippy_utils/src/diagnostics.rs
+++ clippy_utils/src/diagnostics.rs
@@ -18,7 +18,7 @@ fn docs_link(diag: &mut Diagnostic, lint: &'static Lint) {
     if env::var("CLIPPY_DISABLE_DOCS_LINKS").is_err() {
         if let Some(lint) = lint.name_lower().strip_prefix("clippy::") {
             diag.help(format!(
-                "for further information visit https://rust-lang.github.io/rust-clippy/{}/index.html#{lint}",
+                "for further information visit [rust-lang.github.io/rust-clippy/{}/index.html#{lint}](https://rust-lang.github.io/rust-clippy/{}/index.html#{lint})",
                 &option_env!("RUST_RELEASE_NUM").map_or("master".to_string(), |n| {
                     // extract just major + minor version and ignore patch versions
                     format!("rust-{}", n.rsplit_once('.').unwrap().1)
