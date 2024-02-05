f241339be89e57443d6caa63c9e42ede4c752c7d
Guillaume Gomez <guillaume1.gomez@gmail.com>
2024-02-05 15:05:20 +0100

    Allow `clippy::unstable_intrinsics_with_stable_wrapper` in transmute ui test

--- tests/ui/transmute.rs
+++ tests/ui/transmute.rs
@@ -1,4 +1,9 @@
-#![allow(dead_code, clippy::borrow_as_ptr, clippy::needless_lifetimes)]
+#![allow(
+    dead_code,
+    clippy::borrow_as_ptr,
+    clippy::needless_lifetimes,
+    clippy::unstable_intrinsics_with_stable_wrapper
+)]
 //@no-rustfix
 extern crate core;
 
