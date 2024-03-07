fc7a7752056f2388b1cd8ad5d8d51be442b10d4a
Jacherr <jwc2002@outlook.com>
2024-03-07 15:40:49 +0000

    re-bless internal and update_lints

--- clippy_lints/src/declared_lints.rs
+++ clippy_lints/src/declared_lints.rs
@@ -474,6 +474,7 @@ pub(crate) static LINTS: &[&crate::LintInfo] = &[
     crate::methods::UNNECESSARY_MIN_OR_MAX_INFO,
     crate::methods::UNNECESSARY_RESULT_MAP_OR_ELSE_INFO,
     crate::methods::UNNECESSARY_MAP_OR_INFO,
+    crate::methods::UNNECESSARY_RESULT_MAP_OR_ELSE_INFO,
     crate::methods::UNNECESSARY_SORT_BY_INFO,
     crate::methods::UNNECESSARY_TO_OWNED_INFO,
     crate::methods::UNWRAP_OR_DEFAULT_INFO,
