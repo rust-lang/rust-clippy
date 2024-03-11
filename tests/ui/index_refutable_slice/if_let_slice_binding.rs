19f4708a071e362b43ba185267456321e849dc27
Jacherr <jwc2002@outlook.com>
2024-03-11 22:15:19 +0000

    new lint: `unnecessary_indexing`
    do not lint after mutable reference is taken
    
    check path to local on conditional receiver, check mutable borrow after ref, do not lint on mutable auto borrow
    
    fix autoborrow/mutability checks
    
    remove unneded `extra_locals`
    
    inline if ststements; check locality earlier
    
    remove unnecessary impl on IndexCheckResult
    
    check for borrows in if block and change inner `Some` based on this

--- tests/ui/index_refutable_slice/if_let_slice_binding.rs
+++ tests/ui/index_refutable_slice/if_let_slice_binding.rs
@@ -1,5 +1,6 @@
 #![deny(clippy::index_refutable_slice)]
 #![allow(clippy::uninlined_format_args, clippy::needless_lifetimes)]
+#![allow(clippy::unnecessary_indexing)]
 
 enum SomeEnum<T> {
     One(T),
---
+++ tests/ui/unnecessary_indexing.rs
@@ -0,0 +1,131 @@
+//@no-rustfix
+#![allow(unused)]
+#![allow(dropping_copy_types)]
+#![allow(dropping_references)]
+#![warn(clippy::unnecessary_indexing)]
+
+fn c(x: i32) -> i32 {
+    println!("{x}");
+    10
+}
+
+struct Struct;
+impl Struct {
+    pub fn a(x: i32) -> i32 {
+        println!("{x}");
+        10
+    }
+}
+
+fn main() {
+    // lint on vecs with a call
+    let a: Vec<i32> = vec![1];
+    if !a.is_empty() {
+        let b = c(a[0]);
+    }
+
+    // lint on vecs with a method call
+    let a: Vec<i32> = vec![1];
+    if !a.is_empty() {
+        let b = Struct::a(a[0]);
+    }
+
+    // lint on arrays with a call
+    let a: &[i32] = &[1];
+    if !a.is_empty() {
+        let b = c(a[0]);
+    }
+
+    // lint on arrays with a method call
+    let a: &[i32] = &[1];
+    if !a.is_empty() {
+        let b = Struct::a(a[0]);
+    }
+
+    // lint on vecs with a local access
+    let a: Vec<i32> = vec![1];
+    if !a.is_empty() {
+        let b = a[0];
+    }
+
+    // lint on arrays with a local access
+    let a: &[i32] = &[1];
+    if !a.is_empty() {
+        let b = a[0];
+    }
+
+    // lint when access is not first line
+    let a: &[i32] = &[1];
+    if !a.is_empty() {
+        dbg!(a);
+        let b = a[0];
+    }
+
+    // lint on multiple accesses/locals
+    let a: &[i32] = &[1];
+    if !a.is_empty() {
+        dbg!(a);
+        let b = &a[0];
+        let c = a[0];
+        drop(a[0]);
+    }
+
+    // lint on multiple accesses
+    let a: &[i32] = &[1];
+    if !a.is_empty() {
+        dbg!(a);
+        drop(a[0]);
+        drop(a[0]);
+    }
+
+    // dont lint when not accessing [0]
+    let a: &[i32] = &[1, 2];
+    if !a.is_empty() {
+        let b = a[1];
+    }
+
+    // dont lint when access is dynamic
+    const T: usize = 0;
+
+    let a: &[i32] = &[1];
+    if !a.is_empty() {
+        let b = a[T];
+    }
+
+    // dont lint without unary
+    let a: &[i32] = &[1];
+    if a.is_empty() {
+        let b = a[0];
+    }
+
+    // dont lint if we have mutable reference
+    let mut a: &[i32] = &[1];
+    if !a.is_empty() {
+        drop(&mut a);
+        let b = a[0];
+    }
+
+    // dont lint if we have a mutable reference, even if the mutable reference occurs after what we are
+    // linting against
+    let mut a: &[i32] = &[1];
+    if !a.is_empty() {
+        let b = a[0];
+        drop(&mut a);
+    }
+
+    // dont lint on mutable auto borrow
+    let mut a = vec![1, 2, 3];
+    if !a.is_empty() {
+        a.push(1);
+        let b = a[0];
+        b;
+    }
+
+    // do not lint if conditional receiver is mutable reference
+    let a = &mut vec![1, 2, 3];
+    if !a.is_empty() {
+        let b = a[0];
+        a;
+        b;
+    }
+}
