98bbb3776d8a3bdc0210321de8c81e26ff0289af
Declan Kelly <dkelly.home@gmail.com>
2024-03-22 18:40:04 -0700

    Add lint which checks that duration math isn't losing precision
    **Description**
    Add a lint which checks for a binop or cast where the expression
    is converting a Duration to a floating point number, and losing
    precision along the way.
    
    The lint won't fire for most cases involving `as_nanos()` since
    converting that to floating point already gives the max precision.
    
    The lint is also restricted to an MSRV of 1.38.0, since that is
    when the `as_secs_{f32,f64}()` methods were stabilized.
    
    **Motivation**
    This change is motivated by [a rust stdlib ACP which proposed
    `as_millis_{f64,f32}`][ACP-link]. As part of that I did some code
    searches on github (see ACP for links) that showed a lot of code
    which converted Duration values to floating point using methods other
    than `as_secs_{f32,64}()`, and were losing precision because of that.
    
    This lint seems like a good way to raise awareness and prompt
    using the existing methods.
    
    [ACP-link]: https://github.com/rust-lang/libs-team/issues/349
    
    **Testing Done**
    Added UI tests, ran `cargo test`, followed the Clippy manual

---
+++ tests/ui/duration_to_float_precision_loss.rs
@@ -0,0 +1,59 @@
+#![warn(clippy::duration_to_float_precision_loss)]
+
+use std::time::Duration;
+
+const DURATION: Duration = Duration::from_nanos(1_234_567_890);
+
+fn main() {
+    let float_secs = DURATION.as_secs() as f64;
+    assert_eq!(float_secs, 1.234);
+
+    macro_rules! m {
+        ($d:expr) => {{
+            let float_secs = $d.as_secs() as f64;
+            assert_eq!(float_secs, 1.234);
+        }};
+    }
+
+    m!(DURATION);
+
+    let float_secs = DURATION.as_secs() as f32;
+    assert_eq!(float_secs, 1.234);
+
+    let float_secs = DURATION.as_millis() as f64 / 1_000.0;
+    assert_eq!(float_secs, 1.234);
+
+    let float_secs = DURATION.as_micros() as f64 / 1_000_000.0;
+    assert_eq!(float_secs, 1.234);
+
+    let float_secs = DURATION.as_nanos() as f64 / 1_000_000_000.0;
+    assert_eq!(float_secs, 1.234);
+
+    let float_millis = DURATION.as_millis() as f64;
+    assert_eq!(float_millis, 1234.0);
+
+    let float_micros = DURATION.as_micros() as f64;
+    assert_eq!(float_micros, 1_234_567.0);
+
+    // should not trigger on these conversion, since they are already the max precision
+    let float_nanos = DURATION.as_nanos() as f64;
+    assert_eq!(float_nanos, 1_234_567_890.0);
+
+    let float_micros = DURATION.as_nanos() as f64 / 1_000.0;
+    assert_eq!(float_micros, 1_234_567.0);
+
+    let float_millis = DURATION.as_nanos() as f64 / 1_000_000.0;
+    assert_eq!(float_millis, 1234.0);
+}
+
+#[clippy::msrv = "1.37"]
+fn msrv_1_37() {
+    let float_secs = DURATION.as_secs() as f64; // should not trigger lint because MSRV is below
+    assert_eq!(float_secs, 1.234);
+}
+
+#[clippy::msrv = "1.38"]
+fn msrv_1_38() {
+    let float_secs = DURATION.as_secs() as f64; // should trigger lint because MSRV is satisfied
+    assert_eq!(float_secs, 1.234);
+}
