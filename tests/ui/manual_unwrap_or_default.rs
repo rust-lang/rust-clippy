6338925b257b56841e34ff1b5285ef3d99b3b8bc
J-ZhengLi <lizheng135@huawei.com>
2024-03-28 18:02:13 +0800

    fix [`manual_unwrap_or_default`] suggestion ignoring side-effects

--- tests/ui/manual_unwrap_or_default.rs
+++ tests/ui/manual_unwrap_or_default.rs
@@ -3,36 +3,36 @@
 
 fn main() {
     let x: Option<Vec<String>> = None;
+    //~v ERROR: match can be simplified with `.unwrap_or_default()`
     match x {
-        //~^ ERROR: match can be simplified with `.unwrap_or_default()`
         Some(v) => v,
         None => Vec::default(),
     };
 
     let x: Option<Vec<String>> = None;
+    //~v ERROR: match can be simplified with `.unwrap_or_default()`
     match x {
-        //~^ ERROR: match can be simplified with `.unwrap_or_default()`
         Some(v) => v,
         _ => Vec::default(),
     };
 
     let x: Option<String> = None;
+    //~v ERROR: match can be simplified with `.unwrap_or_default()`
     match x {
-        //~^ ERROR: match can be simplified with `.unwrap_or_default()`
         Some(v) => v,
         None => String::new(),
     };
 
     let x: Option<Vec<String>> = None;
+    //~v ERROR: match can be simplified with `.unwrap_or_default()`
     match x {
-        //~^ ERROR: match can be simplified with `.unwrap_or_default()`
         None => Vec::default(),
         Some(v) => v,
     };
 
     let x: Option<Vec<String>> = None;
+    //~v ERROR: if let can be simplified with `.unwrap_or_default()`
     if let Some(v) = x {
-        //~^ ERROR: if let can be simplified with `.unwrap_or_default()`
         v
     } else {
         Vec::default()
@@ -41,8 +41,8 @@ fn main() {
 
 // Issue #12531
 unsafe fn no_deref_ptr(a: Option<i32>, b: *const Option<i32>) -> i32 {
+    // `*b` being correct depends on `a == Some(_)`
     match a {
-        // `*b` being correct depends on `a == Some(_)`
         Some(_) => match *b {
             Some(v) => v,
             _ => 0,
@@ -56,4 +56,51 @@ const fn issue_12568(opt: Option<bool>) -> bool {
         Some(s) => s,
         None => false,
     }
+}
+
+fn issue_12569() {
+    let match_none_se = match 1u32.checked_div(0) {
+        Some(v) => v,
+        None => {
+            println!("important");
+            0
+        }
+    };
+    let match_some_se = match 1u32.checked_div(0) {
+        Some(v) => {
+            println!("important");
+            v
+        },
+        None => 0
+    };
+    #[allow(clippy::manual_unwrap_or)]
+    let match_none_cm = match 1u32.checked_div(0) {
+        Some(v) => v,
+        None => {
+            // Important
+            0
+        }
+    };
+    let match_middle_cm = match 1u32.checked_div(0) {
+        Some(v) => v,
+        // Important
+        None => 0
+    };
+    let match_middle_doc_cm = match 1u32.checked_div(0) {
+        /// Important
+        Some(v) => v,
+        None => 0
+    };
+    let iflet_else_se = if let Some(v) = 1u32.checked_div(0) {
+        v
+    } else {
+        println!("important");
+        0
+    };
+    let iflet_then_cm = if let Some(v) = 1u32.checked_div(0) {
+        // Important
+        v
+    } else {
+        0
+    };
 }
