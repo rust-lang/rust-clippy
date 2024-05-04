4c70537f2f3dbd72c221ba738a813e7632f96523
Philipp Krones <hello@philkrones.com>
2024-05-04 14:55:02 +0200

    Switch Rust<>Clippy sync to JOSH
    ### Update book for backport, release and sync
    
    Now with switching to Josh, the documentation should also be updated, as
    a lot of things got simplified. This updates the documentation on how to
    do syncs with josh, how to do releases and how to do backports.
    
    ### Implement release subcommand in clippy_dev
    
    This is a QoL improvement for doing releases. The `release commit`
    command will only work with the new Josh syncs. Until then the old way,
    using `git log`, has to be used.

--- clippy_dev/src/release.rs
+++ clippy_dev/src/release.rs
@@ -1,5 +1,8 @@
 use crate::utils::{FileUpdater, UpdateStatus, Version, parse_cargo_package};
-use std::fmt::Write as _;
+
+use std::fmt::{Display, Write as _};
+
+use clap::ValueEnum;
 
 static CARGO_TOML_FILES: &[&str] = &[
     "clippy_config/Cargo.toml",
@@ -27,4 +30,62 @@ pub fn bump_version(mut version: Version) {
             }
         });
     }
+}
+
+#[derive(ValueEnum, Copy, Clone)]
+pub enum Branch {
+    Stable,
+    Beta,
+    Master,
+}
+
+impl Display for Branch {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Branch::Stable => write!(f, "stable"),
+            Branch::Beta => write!(f, "beta"),
+            Branch::Master => write!(f, "master"),
+        }
+    }
+}
+
+pub fn rustc_clippy_commit(rustc_path: String, branch: Branch) {
+    const PUSH_PR_DESCRIPTION: &str = "Subtree update of `rust-clippy`";
+
+    let mut git_fetch = std::process::Command::new("git");
+    git_fetch.current_dir(&rustc_path);
+    git_fetch.args([
+        "fetch",
+        "--recurse-submodules=no",
+        "https://github.com/rust-lang/rust",
+        &branch.to_string(),
+    ]);
+
+    git_fetch.status().expect("failed to fetch base commit");
+
+    let mut git_log = std::process::Command::new("git");
+    git_log.current_dir(rustc_path);
+    git_log.args([
+        "log",
+        "-1",
+        "--merges",
+        &format!("--grep=\"{PUSH_PR_DESCRIPTION}\""),
+        "FETCH_HEAD",
+        "--",
+        "src/tools/clippy",
+    ]);
+
+    let out = git_log.output().expect("failed to run git log");
+    let last_rustup_commit = String::from_utf8_lossy(out.stdout.trim_ascii()).to_string();
+
+    let commit = last_rustup_commit
+        .lines()
+        .find(|c| c.contains(PUSH_PR_DESCRIPTION))
+        .expect("no commit found")
+        .trim()
+        .rsplit_once('/')
+        .expect("no commit hash found")
+        .1;
+
+    println!("{commit}");
 }
