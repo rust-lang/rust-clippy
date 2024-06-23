3b9bba4a8e09ecb9b0388348ae1177648d98a7fd
binarycat <binarycat@envs.net>
2024-06-23 01:03:31 -0400

    cover most edge cases

--- tests/ui/and_then_then_some.rs
+++ tests/ui/and_then_then_some.rs
@@ -3,10 +3,10 @@
 fn main() {
 	let x = Some("foo".to_string());
 
-    /*let _y = x.clone().and_then(|v| v.starts_with('f')
+    let _y = x.clone().and_then(|v| v.starts_with('f')
         .then_some(v));
 
-	let _z = x.clone().and_then(|v| bool::then_some(v.starts_with('f'), v));*/
+	let _z = x.clone().and_then(|v| bool::then_some(v.starts_with('f'), v));
 	let _w = Option::and_then(x, |v| {
 		bool::then_some(v.starts_with('f'), v)
 	});
