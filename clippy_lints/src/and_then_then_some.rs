88ec537adc9ab22727b09e65d10012bc2c722543
binarycat <binarycat@envs.net>
2024-06-23 01:24:47 -0400

    rustfix and rustfmt

--- clippy_lints/src/and_then_then_some.rs
+++ clippy_lints/src/and_then_then_some.rs
@@ -1,24 +1,24 @@
 // noise reduction, remove before committing!
 #![allow(unused_variables)]
 
+use clippy_utils::diagnostics::span_lint_and_sugg;
+use clippy_utils::source::snippet_with_applicability;
+use clippy_utils::{fn_def_id, match_def_path};
 use rustc_errors::Applicability;
-use rustc_hir::*;
 use rustc_hir::def::Res;
+use rustc_hir::*;
 use rustc_lint::{LateContext, LateLintPass};
 use rustc_session::declare_lint_pass;
 use rustc_span::Span;
-use clippy_utils::{match_def_path, fn_def_id};
-use clippy_utils::diagnostics::span_lint_and_sugg;
-use clippy_utils::source::snippet_with_applicability;
 
 declare_clippy_lint! {
     /// ### What it does
     /// Detects usage of `Option::and_then` and `bool::then_some` that could
-	/// be replaced with `Option::filter`.
-	///
+    /// be replaced with `Option::filter`.
+    ///
     /// ### Why is this bad?
-	/// Needless complexity, uses recent and uncommon stdlib funtions instead of
-	/// one older function.
+    /// Needless complexity, uses recent and uncommon stdlib funtions instead of
+    /// one older function.
     ///
     /// ### Example
     /// ```no_run
@@ -39,146 +39,150 @@ declare_clippy_lint! {
 declare_lint_pass!(AndThenThenSome => [AND_THEN_THEN_SOME]);
 
 impl<'tcx> LateLintPass<'tcx> for AndThenThenSome {
-	fn check_expr(&mut self, cx: &LateContext<'tcx>, expr: &'tcx Expr<'tcx>) {
-		match expr.kind {
-			ExprKind::MethodCall(method_name, selfarg, [ arg ], _span) => {
-				//(expr);
-				//let option_id = cx.tcx.get_diagnostic_item(sym::Option);
-				// TODO: check if type of reciever is diagnostic item Option.
-				//let tckr = cx.typeck_results();
-				//let def_id = tckr.type_dependent_def_id(expr.hir_id).unwrap();
-				//(method_name, selfarg, arg);
-				if is_and_then(cx, expr)
-				{
-					if let Some((closure_args, predicate)) = (then_some_closure_arg(cx, arg)) {
-						//(predicate);
-						show_sugg(cx, expr.span, selfarg, closure_args, predicate);
-					}
-				}
-			}
-			ExprKind::Call(_func, [ selfarg, arg ]) => {
-				if (is_and_then(cx, expr)) {
-					if let Some((closure_args, predicate)) = (then_some_closure_arg(cx, arg)) {
-						//(predicate);
-						show_sugg(cx, expr.span, selfarg, closure_args, predicate);
-					}
-				}
-			}
-			// TODO: check for call as associated function
-			_ => {},
-		}
-	}
+    fn check_expr(&mut self, cx: &LateContext<'tcx>, expr: &'tcx Expr<'tcx>) {
+        match expr.kind {
+            ExprKind::MethodCall(method_name, selfarg, [arg], _span) => {
+                //let option_id = cx.tcx.get_diagnostic_item(sym::Option);
+                // TODO: check if type of reciever is diagnostic item Option.
+                //let tckr = cx.typeck_results();
+                //let def_id = tckr.type_dependent_def_id(expr.hir_id).unwrap();
+                //(method_name, selfarg, arg);
+                if is_and_then(cx, expr) {
+                    if let Some((closure_args, predicate)) = then_some_closure_arg(cx, arg) {
+                        //(predicate);
+                        show_sugg(cx, expr.span, selfarg, closure_args, predicate);
+                    }
+                }
+            },
+            ExprKind::Call(_func, [selfarg, arg]) => {
+                if is_and_then(cx, expr) {
+                    if let Some((closure_args, predicate)) = then_some_closure_arg(cx, arg) {
+                        //(predicate);
+                        show_sugg(cx, expr.span, selfarg, closure_args, predicate);
+                    }
+                }
+            },
+            // TODO: check for call as associated function
+            _ => {},
+        }
+    }
 }
 
 // `|v| X.then_some(v)` -> Some((span"|v|", X))
-fn then_some_closure_arg<'tcx>(cx: &LateContext<'tcx>, expr: &Expr<'tcx>)
-							  -> Option<(Span, &'tcx Expr<'tcx>)>
-{
-	(expr);
-	match expr.kind {
-		ExprKind::Closure(Closure{
-			fn_decl: FnDecl{ inputs: [ Ty{ hir_id: arg_id, ..} ], .. },
-			body,
-			..
-		}) => {
-			if let Node::Expr(expr) = (cx.tcx.hir_node(body.hir_id)) {
-				//(arg_id);
-				if let Some(body) = (peel_closure_body(cx, expr, *arg_id)) {
-					Some((cx.tcx.hir().span(*arg_id), body))
-				} else {
-					None
-				}
-			} else {
-				None
-			}
-		},
-		_ => None,
-	}
+fn then_some_closure_arg<'tcx>(cx: &LateContext<'tcx>, expr: &Expr<'tcx>) -> Option<(Span, &'tcx Expr<'tcx>)> {
+    match expr.kind {
+        ExprKind::Closure(Closure {
+            fn_decl: FnDecl {
+                inputs: [Ty { hir_id: arg_id, .. }],
+                ..
+            },
+            body,
+            ..
+        }) => {
+            if let Node::Expr(expr) = cx.tcx.hir_node(body.hir_id) {
+                //(arg_id);
+                (peel_closure_body(cx, expr, *arg_id)).map(|body| (cx.tcx.hir().span(*arg_id), body))
+            } else {
+                None
+            }
+        },
+        _ => None,
+    }
 }
 
-fn peel_closure_body<'tcx>(cx: &LateContext<'tcx>, expr: &'tcx Expr<'tcx>, closure_arg_id: HirId) -> Option<&'tcx Expr<'tcx>> {
-
-	match expr.kind {
-		// it would be nice if we could lift { x; y.a() } into { x; y }.a()
-		ExprKind::Block(Block{ stmts: [], expr: Some(wrapped_expr), ..}, _) => {
-			peel_closure_body(cx, wrapped_expr, closure_arg_id)
-		}
-		ExprKind::MethodCall(_path, selfarg, [ arg ], _span) => {
-			if is_then_some(cx, expr) &&
-				is_local_defined_at(cx, arg, closure_arg_id)
-			{
-				// the argument to then_some is the same as that given to the closure
-				Some(selfarg)
-			} else {
-				None
-			}
-		}
-		ExprKind::Call(func, [ pred, arg ]) => {
-			if (is_then_some(cx, expr)) && (is_local_defined_at(cx, arg, closure_arg_id)) {
-				Some(pred)
-				
-			} else {
-				None
-			}
-		}
-		_ => {
-			None
-		}
-	}
+fn peel_closure_body<'tcx>(
+    cx: &LateContext<'tcx>,
+    expr: &'tcx Expr<'tcx>,
+    closure_arg_id: HirId,
+) -> Option<&'tcx Expr<'tcx>> {
+    match expr.kind {
+        // it would be nice if we could lift { x; y.a() } into { x; y }.a()
+        ExprKind::Block(
+            Block {
+                stmts: [],
+                expr: Some(wrapped_expr),
+                ..
+            },
+            _,
+        ) => peel_closure_body(cx, wrapped_expr, closure_arg_id),
+        ExprKind::MethodCall(_path, selfarg, [arg], _span) => {
+            if is_then_some(cx, expr) && is_local_defined_at(cx, arg, closure_arg_id) {
+                // the argument to then_some is the same as that given to the closure
+                Some(selfarg)
+            } else {
+                None
+            }
+        },
+        ExprKind::Call(func, [pred, arg]) => {
+            if (is_then_some(cx, expr)) && (is_local_defined_at(cx, arg, closure_arg_id)) {
+                Some(pred)
+            } else {
+                None
+            }
+        },
+        _ => None,
+    }
 }
 
-fn is_local_defined_at<'tcx>(cx: &LateContext<'tcx>, local: &Expr<'_>, arg_hid: HirId) -> bool {
-	match local.kind {
-		ExprKind::Path(QPath::Resolved(_, Path{ res: Res::Local(local_hid), .. })) => {
-			// FIXME: this is the best way i could find to compare if
-			// a local refers to a specific closure argument.
-			//
-			// it breaks if the closure argument has an explicitly declared type,
-			// since the spans only align for TyKind::Infer
-			if let Node::Pat(Pat{ span: local_span, .. }) = (cx.tcx.hir_node(*local_hid)) &&
-				let Node::Ty(Ty{ span: arg_span, .. }) = (cx.tcx.hir_node(arg_hid)) &&
-				local_span == arg_span
-			{
-				true
-			} else {
-				false
-			}		
-		}
-		// is not local at all, so definitly isn't a local defined at the given position
-		_ => false,
-	}
+fn is_local_defined_at(cx: &LateContext<'_>, local: &Expr<'_>, arg_hid: HirId) -> bool {
+    match local.kind {
+        ExprKind::Path(QPath::Resolved(
+            _,
+            Path {
+                res: Res::Local(local_hid),
+                ..
+            },
+        )) => {
+            // FIXME: this is the best way i could find to compare if
+            // a local refers to a specific closure argument.
+            //
+            // it breaks if the closure argument has an explicitly declared type,
+            // since the spans only align for TyKind::Infer
+            if let Node::Pat(Pat { span: local_span, .. }) = (cx.tcx.hir_node(*local_hid))
+                && let Node::Ty(Ty { span: arg_span, .. }) = (cx.tcx.hir_node(arg_hid))
+                && local_span == arg_span
+            {
+                true
+            } else {
+                false
+            }
+        },
+        // is not local at all, so definitly isn't a local defined at the given position
+        _ => false,
+    }
 }
 
 fn show_sugg(cx: &LateContext<'_>, span: Span, selfarg: &Expr<'_>, closure_args: Span, predicate: &Expr<'_>) {
-	let mut appl = Applicability::MachineApplicable;
-	let sugg = format!(
-		"{}.filter(|{}| {})",
-		snippet_with_applicability(cx, selfarg.span, "<OPTION>", &mut appl),
-		snippet_with_applicability(cx, closure_args, "<ARGS>", &mut appl),
-		snippet_with_applicability(cx, predicate.span, "<PREDICATE>", &mut appl));
-	span_lint_and_sugg(cx, AND_THEN_THEN_SOME, span,
-					   "use of `and_then` + `then_some` is equivelent to `filter`",
-					   "use `Option::filter` instead",
-					   sugg, appl);
+    let mut appl = Applicability::MachineApplicable;
+    let sugg = format!(
+        "{}.filter(|{}| {})",
+        snippet_with_applicability(cx, selfarg.span, "<OPTION>", &mut appl),
+        snippet_with_applicability(cx, closure_args, "<ARGS>", &mut appl),
+        snippet_with_applicability(cx, predicate.span, "<PREDICATE>", &mut appl)
+    );
+    span_lint_and_sugg(
+        cx,
+        AND_THEN_THEN_SOME,
+        span,
+        "use of `and_then` + `then_some` is equivelent to `filter`",
+        "use `Option::filter` instead",
+        sugg,
+        appl,
+    );
 }
 
 fn is_then_some(cx: &LateContext<'_>, expr: &Expr<'_>) -> bool {
-	if let Some(def_id) = fn_def_id(cx, expr) {
-		(match_def_path(
-			cx, (def_id),
-			&["core", "bool", "<impl bool>", "then_some"]))
-	} else {
-		(expr);
-		false
-	}
+    if let Some(def_id) = fn_def_id(cx, expr) {
+        match_def_path(cx, def_id, &["core", "bool", "<impl bool>", "then_some"])
+    } else {
+        false
+    }
 }
 
 fn is_and_then(cx: &LateContext<'_>, expr: &Expr<'_>) -> bool {
-	if let Some(def_id) = fn_def_id(cx, expr) {
-		(match_def_path(
-			cx, (def_id),
-			&["core", "option", "Option", "and_then"]))
-	} else {
-		false
-	}
+    if let Some(def_id) = fn_def_id(cx, expr) {
+        match_def_path(cx, def_id, &["core", "option", "Option", "and_then"])
+    } else {
+        false
+    }
 }
--- tests/ui/and_then_then_some.rs
+++ tests/ui/and_then_then_some.rs
@@ -1,13 +1,10 @@
 #![warn(clippy::and_then_then_some)]
 
 fn main() {
-	let x = Some("foo".to_string());
+    let x = Some("foo".to_string());
 
-    let _y = x.clone().and_then(|v| v.starts_with('f')
-        .then_some(v));
+    let _y = x.clone().and_then(|v| v.starts_with('f').then_some(v));
 
-	let _z = x.clone().and_then(|v| bool::then_some(v.starts_with('f'), v));
-	let _w = Option::and_then(x, |v| {
-		bool::then_some(v.starts_with('f'), v)
-	});
+    let _z = x.clone().and_then(|v| bool::then_some(v.starts_with('f'), v));
+    let _w = Option::and_then(x, |v| bool::then_some(v.starts_with('f'), v));
 }
