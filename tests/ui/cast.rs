e86a94cb524180ac26dc3be58602af0bdfe6a64c
Lzu Tao <taolzu@gmail.com>
2024-07-02 21:03:00 +0700

    limit local ADT types to size_of

--- tests/ui/cast.rs
+++ tests/ui/cast.rs
@@ -511,5 +511,8 @@ pub fn issue_9613() {
     CHUNK as u32;
     u64::MIN as u32;
     //~^ ERROR: casting `u64` to `u32` may truncate the value
+    struct Foo(usize, u32);
+    core::mem::size_of::<Foo>() as u32;
     core::mem::size_of::<String>() as u32;
+    //~^ ERROR: casting `usize` to `u32` may truncate
 }
