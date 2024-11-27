0a91eaa82ac355c5281394036b8131e082117c34
Samuel Moelius <sam@moeli.us>
2024-11-27 06:25:55 -0500

    Handle field attributes in suggestions

--- tests/ui-toml/toml_inconsistent_struct_constructor/conf_inconsistent_struct_constructor.rs
+++ tests/ui-toml/toml_inconsistent_struct_constructor/conf_inconsistent_struct_constructor.rs
@@ -23,3 +23,19 @@ fn main() {
         ..Default::default()
     };
 }
+
+// https://github.com/rust-lang/rust-clippy/pull/13737#discussion_r1859261645
+mod field_attributes {
+    struct HirId;
+    struct BodyVisitor {
+        macro_unsafe_blocks: Vec<HirId>,
+        expn_depth: u32,
+    }
+    fn check_body(condition: bool) {
+        BodyVisitor {
+            #[expect(clippy::bool_to_int_with_if)] // obfuscates the meaning
+            expn_depth: if condition { 1 } else { 0 },
+            macro_unsafe_blocks: Vec::new(),
+        };
+    }
+}
