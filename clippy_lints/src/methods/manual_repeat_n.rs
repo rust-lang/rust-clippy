2c19eab913c92f2956364fa7cf66d401a02ab2bb
lapla-cogito <me@lapla.dev>
2024-12-21 01:48:46 +0900

    suggest to use core::iter::repeat_n in a no_std environment

--- clippy_lints/src/methods/manual_repeat_n.rs
+++ clippy_lints/src/methods/manual_repeat_n.rs
@@ -1,7 +1,7 @@
 use clippy_utils::diagnostics::span_lint_and_sugg;
 use clippy_utils::msrvs::{self, Msrv};
 use clippy_utils::source::{snippet, snippet_with_context};
-use clippy_utils::{expr_use_ctxt, fn_def_id, is_trait_method};
+use clippy_utils::{expr_use_ctxt, fn_def_id, is_trait_method, std_or_core};
 use rustc_errors::Applicability;
 use rustc_hir::{Expr, ExprKind};
 use rustc_lint::LateContext;
@@ -23,6 +23,7 @@ pub(super) fn check<'tcx>(
         && let ExprKind::Call(_, [repeat_arg]) = repeat_expr.kind
         && let Some(def_id) = fn_def_id(cx, repeat_expr)
         && cx.tcx.is_diagnostic_item(sym::iter_repeat, def_id)
+        && let Some(std_or_core) = std_or_core(cx)
     {
         let mut app = Applicability::MachineApplicable;
         span_lint_and_sugg(
@@ -32,7 +33,7 @@ pub(super) fn check<'tcx>(
             "this `.repeat().take()` can be written more concisely",
             "consider using `repeat_n()` instead",
             format!(
-                "std::iter::repeat_n({}, {})",
+                "{std_or_core}::iter::repeat_n({}, {})",
                 snippet_with_context(cx, repeat_arg.span, expr.span.ctxt(), "..", &mut app).0,
                 snippet(cx, take_arg.span, "")
             ),
