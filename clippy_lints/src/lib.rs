7b849af9ed0ab253c14d1d62b4d914909f2eb23d
Samuel Tardieu <sam@rfc1149.net>
2025-01-11 09:03:59 +0100

    New lint: `copy_then_borrow_mut`

--- clippy_lints/src/lib.rs
+++ clippy_lints/src/lib.rs
@@ -103,6 +103,7 @@ mod collection_is_never_read;
 mod comparison_chain;
 mod copies;
 mod copy_iterator;
+mod copy_then_borrow_mut;
 mod crate_in_macro_def;
 mod create_dir;
 mod dbg_macro;
@@ -980,5 +981,6 @@ pub fn register_lints(store: &mut rustc_lint::LintStore, conf: &'static Conf) {
     store.register_late_pass(move |_| Box::new(non_std_lazy_statics::NonStdLazyStatic::new(conf)));
     store.register_late_pass(|_| Box::new(manual_option_as_slice::ManualOptionAsSlice::new(conf)));
     store.register_late_pass(|_| Box::new(single_option_map::SingleOptionMap));
+    store.register_late_pass(|_| Box::new(copy_then_borrow_mut::CopyThenBorrowMut));
     // add lints here, do not remove this comment, it's used in `new_lint`
 }
