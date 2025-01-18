08febed0ef7da402253f25c90dc5a27d8c8e1c6e
Samuel Tardieu <sam@rfc1149.net>
2025-01-18 15:44:05 +0100

    Recognize the closure form of `Ok` in `option_map_or_err_ok`

--- clippy_lints/src/methods/option_map_or_err_ok.rs
+++ clippy_lints/src/methods/option_map_or_err_ok.rs
@@ -1,13 +1,15 @@
 use clippy_utils::diagnostics::span_lint_and_then;
 use clippy_utils::source::snippet_with_applicability;
 use clippy_utils::ty::is_type_diagnostic_item;
-use clippy_utils::{is_res_lang_ctor, path_res};
+use clippy_utils::{is_lint_allowed, is_res_lang_ctor, path_res, path_to_local};
 use rustc_errors::Applicability;
 use rustc_hir::LangItem::{ResultErr, ResultOk};
-use rustc_hir::{Expr, ExprKind};
+use rustc_hir::{ClosureKind, Expr, ExprKind, PatKind};
 use rustc_lint::LateContext;
 use rustc_span::Span;
 use rustc_span::symbol::sym;
+
+use crate::eta_reduction::REDUNDANT_CLOSURE;
 
 use super::OPTION_MAP_OR_ERR_OK;
 
@@ -25,7 +27,7 @@ pub(super) fn check<'tcx>(
         && let ExprKind::Call(call, &[arg]) = or_expr.kind
         && is_res_lang_ctor(cx, path_res(cx, call), ResultErr)
         // And finally we check that it is mapped as `Ok`.
-        && is_res_lang_ctor(cx, path_res(cx, map_expr), ResultOk)
+        && is_ok_function(cx, map_expr)
     {
         let mut app = Applicability::MachineApplicable;
         let err_snippet = snippet_with_applicability(cx, arg.span, "_", &mut app);
@@ -43,5 +45,24 @@ pub(super) fn check<'tcx>(
                 );
             },
         );
+    }
+}
+
+fn is_ok_function(cx: &LateContext<'_>, expr: &Expr<'_>) -> bool {
+    if let ExprKind::Closure(closure) = expr.kind
+        && closure.kind == ClosureKind::Closure
+        // Do not match if `redundant-closure` will be emitted on this closure anyway
+        && is_lint_allowed(cx, REDUNDANT_CLOSURE, expr.hir_id)
+        && let closure_body = cx.tcx.hir().body(closure.body)
+        && let [param] = closure_body.params
+        && let PatKind::Binding(_, var_id, _, _) = param.pat.kind
+        && let closure_expr = closure_body.value
+        && let ExprKind::Call(call, [arg]) = closure_expr.kind
+        && is_res_lang_ctor(cx, path_res(cx, call), ResultOk)
+        && path_to_local(arg) == Some(var_id)
+    {
+        true
+    } else {
+        is_res_lang_ctor(cx, path_res(cx, expr), ResultOk)
     }
 }
--- tests/ui/option_map_or_err_ok.rs
+++ tests/ui/option_map_or_err_ok.rs
@@ -6,5 +6,16 @@ fn main() {
     //~^ ERROR: called `map_or(Err(_), Ok)` on an `Option` value
     let x = &Some("a");
     let _ = x.map_or(Err("a"), Ok);
-    //~^ ERROR: called `map_or(Err(_), Ok)` on an `Option` value
+
+    #[expect(clippy::redundant_closure)]
+    {
+        // Do not lint because the `redundant-closure` lint will be emitted on
+        // the closure already.
+        let _ = x.map_or(Err("a"), |x| Ok(x));
+    }
+    #[allow(clippy::redundant_closure)]
+    {
+        let _ = x.map_or(Err("a"), |x| Ok(x));
+        //~^ ERROR: called `map_or(Err(_), Ok)` on an `Option` value
+    }
 }
