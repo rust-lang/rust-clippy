5c7a2e880eecc988de177080b25dfc5dc5c17a06
Samuel Tardieu <sam@rfc1149.net>
2025-02-16 12:20:16 +0100

    Add `collapse_let_chains` option to `collapsible_if` lint
    Since `collapsible_if` is an early lint, it is not possible to
    automatically check whether the `let_chains` unstable rustc feature is
    enabled or not, as features are not available in the `EarlyContext`
    object.

--- tests/ui-toml/collapsible_if/collapsible_if.rs
+++ tests/ui-toml/collapsible_if/collapsible_if.rs
@@ -1,3 +1,4 @@
+#![feature(let_chains)]
 #![allow(clippy::eq_op, clippy::nonminimal_bool)]
 
 #[rustfmt::skip]
@@ -32,6 +33,34 @@ fn main() {
     if x == "hello" { /* Inner comment */
         if y == "world" {
             println!("Hello world!");
+        }
+    }
+    //~^^^^^ collapsible_if
+
+    if true {
+         if true {
+             println!("No comment, linted");
+         }
+     }
+     //~^^^^^ collapsible_if
+
+    if let Some(a) = Some(3) {
+        if let Some(b) = Some(4) {
+            let _ = a + b;
+        }
+    }
+    //~^^^^^ collapsible_if
+
+    if let Some(a) = Some(3) {
+        if a + 1 == 4 {
+            let _ = a;
+        }
+    }
+    //~^^^^^ collapsible_if
+
+    if Some(3) == Some(4).map(|x| x - 1) {
+        if let Some(b) = Some(4) {
+            let _ = b;
         }
     }
     //~^^^^^ collapsible_if
