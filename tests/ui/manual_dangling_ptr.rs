88e4bf33ee1f9b0419336733bccf280430744b4e
Aaron Ang <aaron.angyd@gmail.com>
2025-02-20 23:45:59 -0800

    Add UI test annotations to `manual_dangling_ptr`

--- tests/ui/manual_dangling_ptr.rs
+++ tests/ui/manual_dangling_ptr.rs
@@ -4,17 +4,24 @@ use std::mem;
 pub fn foo(_const: *const f32, _mut: *mut i64) {}
 
 fn main() {
-    // should lint
     let _ = 4 as *const u32;
+    //~^ manual_dangling_ptr
     let _ = 8 as *mut f64;
+    //~^ manual_dangling_ptr
     let _: *const u8 = 1 as *const _;
+    //~^ manual_dangling_ptr
 
     let _ = mem::align_of::<u32>() as *const u32;
+    //~^ manual_dangling_ptr
     let _ = mem::align_of::<u64>() as *mut u64;
+    //~^ manual_dangling_ptr
 
     foo(4 as *const _, 8 as *mut _);
+    //~^ manual_dangling_ptr
+    //~| manual_dangling_ptr
+}
 
-    // should not lint
+fn should_not_lint() {
     let _ = 0x10 as *mut i64;
     let _ = mem::align_of::<u32>() as *const u64;
 
@@ -30,4 +37,6 @@ fn _msrv_1_83() {
 #[clippy::msrv = "1.84"]
 fn _msrv_1_84() {
     foo(4 as *const _, 8 as *mut _);
+    //~^ manual_dangling_ptr
+    //~| manual_dangling_ptr
 }
