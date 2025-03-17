06eeaf505e6645f095c21719903a0ffc1563a8f5
asdfish <lgray3420@gmail.com>
2025-03-16 22:20:50 -0400

    removed false positive and improved tests

--- tests/ui/cloned_ref_to_slice_refs.rs
+++ tests/ui/cloned_ref_to_slice_refs.rs
@@ -1,11 +1,5 @@
 #![warn(clippy::cloned_ref_to_slice_refs)]
 
-use std::sync::Arc;
-
-fn take_slice(_data: &[Data]) {}
-fn take_slice_mut(_data: &mut [Data]) {}
-fn take_arc(_data: &[Arc<Data>]) {}
-
 #[derive(Clone)]
 struct Data;
 
@@ -13,29 +7,58 @@ fn main() {
     {
         let data = Data;
         let data_ref = &data;
-        take_slice(&[data_ref.clone()]); //~ ERROR: this call to `clone` can be replaced with `std::slice::from_ref`
+        let _ = &[data_ref.clone()]; //~ ERROR: this call to `clone` can be replaced with `std::slice::from_ref`
     }
+
     {
-        take_slice(&[Data.clone()]); //~ ERROR: this call to `clone` can be replaced with `std::slice::from_ref`
+        let _ = &[Data.clone()]; //~ ERROR: this call to `clone` can be replaced with `std::slice::from_ref`
+    }
+
+    {
+        #[derive(Clone)]
+        struct Point(i32, i32);
+
+        let _ = &[Point(0, 0).clone()]; //~ ERROR: this call to `clone` can be replaced with `std::slice::from_ref`
+    }
+
+    // the string was cloned with the intention to not mutate
+    {
+        struct BetterString(String);
+
+        let mut message = String::from("good");
+        let sender = BetterString(message.clone());
+
+        message.push_str("bye!");
+
+        println!("{} {}", message, sender.0)
+    }
+
+    // the string was cloned with the intention to not mutate
+    {
+        let mut x = String::from("Hello");
+        let r = &[x.clone()];
+        x.push('!');
+        println!("r = `{}', x = `{x}'", r[0]);
     }
 
     // mutable borrows may have the intention to clone
     {
         let data = Data;
         let data_ref = &data;
-        take_slice_mut(&mut [data_ref.clone()]);
+        let _ = &mut [data_ref.clone()];
     }
 
     // `T::clone` is used to denote a clone with side effects
     {
+        use std::sync::Arc;
         let data = Arc::new(Data);
-        take_arc(&[Arc::clone(&data)]);
+        let _ = &[Arc::clone(&data)];
     }
 
     // slices with multiple members can only be made from a singular reference
     {
         let data_1 = Data;
         let data_2 = Data;
-        take_slice(&[data_1.clone(), data_2.clone()]);
+        let _ = &[data_1.clone(), data_2.clone()];
     }
 }
