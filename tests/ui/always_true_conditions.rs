791c60d271afa04f5a43e837fc76bfcdfc97a49c
Dominic Moser <llamapagama@gmail.com>
2025-03-18 14:42:33 -0600

    New lint: always_true_conditions

---
+++ tests/ui/always_true_conditions.rs
@@ -0,0 +1,29 @@
+#![warn(clippy::always_true_conditions)]
+#[allow(clippy::needless_if)]
+fn foo_m(name: &str) {
+    if name != "Min" || name != "Max" || name != "Middle" {
+    //~^ always_true_conditions
+        println!("always prints");
+    } else {
+        println!("never prints");
+    }
+    if name != "Min" && name != "Max" {
+        println!("condition satisfied");
+    } else {
+        println!("else");
+    }
+}
+
+fn foo_s(name: &str) {
+    if name != "Min" || name != "Max" {
+    //~^ always_true_conditions
+        println!("always prints");
+    } else {
+        println!("never prints");
+    }
+    if name != "Min" && name != "Max" {
+        println!("condition satisfied");
+    } else {
+        println!("else");
+    }
+}
