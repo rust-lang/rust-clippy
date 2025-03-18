85c6663a3bfb7ada9c1d3cd7a40b4fdd02ffffb5
WeiTheShinobi <weitheshinobi@gmail.com>
2025-03-18 22:23:01 +0800

    `struct_fields_rest_default` will reject any base value used.

--- clippy_lints/src/struct_fields_rest_default.rs
+++ clippy_lints/src/struct_fields_rest_default.rs
@@ -1,4 +1,5 @@
 use clippy_utils::diagnostics::span_lint_and_help;
+use clippy_utils::source::snippet;
 use rustc_ast::Expr;
 use rustc_ast::ast::{ExprKind, StructRest};
 use rustc_lint::{EarlyContext, EarlyLintPass, LintContext};
@@ -6,10 +7,10 @@ use rustc_session::declare_lint_pass;
 
 declare_clippy_lint! {
     /// ### What it does
-    /// Check struct initialization uses `..Default::default()` pattern to skip rest of struct field initialization.
+    /// Check struct initialization uses `..base` pattern to skip rest of struct field initialization.
     ///
     /// ### Why restrict this?
-    /// Using `..Default::default()`` can hide field initialization when new fields are added to structs,
+    /// Using `..base` can hide field initialization when new fields are added to structs,
     /// potentially leading to bugs where developers forget to explicitly set values for new fields.
     ///
     /// ### Example
@@ -55,20 +56,18 @@ declare_lint_pass!(StructFieldsDefault => [STRUCT_FIELDS_REST_DEFAULT]);
 
 impl EarlyLintPass for StructFieldsDefault {
     fn check_expr(&mut self, cx: &EarlyContext<'_>, expr: &Expr) {
-        if let ExprKind::Struct(struct_expr) = &expr.kind
+        if !expr.span.in_external_macro(cx.sess().source_map())
+            && let ExprKind::Struct(struct_expr) = &expr.kind
             && let StructRest::Base(base) = &struct_expr.rest
-            && !base.span.in_external_macro(cx.sess().source_map())
-            && let ExprKind::Call(call, _) = &base.kind
-            && let ExprKind::Path(_, path) = &call.kind
-            && let [part1, part2] = path.segments.as_slice()
-            && part1.ident.name == rustc_span::sym::Default
-            && part2.ident.name == rustc_span::kw::Default
         {
             span_lint_and_help(
                 cx,
                 STRUCT_FIELDS_REST_DEFAULT,
                 base.span,
-                "should not use `..Default::default()` to omit rest of struct field initialization",
+                format!(
+                    "should not use `..{}` to omit rest of struct field initialization",
+                    snippet(cx, base.span, "..")
+                ),
                 Some(expr.span),
                 "each field's initial value should be explicitly specified",
             );
--- tests/ui/struct_fields_rest_default.rs
+++ tests/ui/struct_fields_rest_default.rs
@@ -1,15 +1,18 @@
-//@aux-build:external_macro.rs
+//@aux-build:proc_macros.rs
 #![warn(clippy::struct_fields_rest_default)]
-
-extern crate external_macro;
-
-use external_macro::external_struct_rest_default;
+extern crate proc_macros;
 
 #[derive(Default)]
 struct Foo {
     a: i32,
     b: i32,
     c: i32,
+}
+
+impl Foo {
+    fn get_foo() -> Self {
+        Foo { a: 0, b: 0, c: 0 }
+    }
 }
 
 fn main() {
@@ -20,8 +23,33 @@ fn main() {
         //~^ struct_fields_rest_default
     };
 
+    #[rustfmt::skip]
+    let _ = Foo {
+        a: 10,
+        ..Foo::default()
+        //~^ struct_fields_rest_default
+    };
+
+    #[rustfmt::skip]
+    let _ = Foo {
+        a: 10,
+        ..Foo::get_foo()
+        //~^ struct_fields_rest_default
+    };
+
     // should not lint in external macro
-    external_struct_rest_default!();
+    proc_macros::external! {
+        #[derive(Default)]
+        struct ExternalDefault {
+            a: i32,
+            b: i32,
+        }
+
+        let _ = ExternalDefault {
+            a: 10,
+            ..Default::default()
+        };
+    }
 
     // should not lint
     let _ = Foo {
