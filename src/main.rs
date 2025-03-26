e81e429f2373f773a36ed87250238b9658bfd1c8
blyxyas <blyxyas@gmail.com>
2025-03-26 22:18:20 +0100

    Add GLadOS to Clippy

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,9 @@ use std::path::PathBuf;
 use std::process::{self, Command};
 
 use anstream::println;
+use soloud::Wav;
+use tiny_rng::Rand;
+use soloud::*;
 
 #[allow(clippy::ignored_unit_patterns)]
 fn show_help() {
@@ -140,9 +143,56 @@ where
         .wait()
         .expect("failed to wait for cargo?");
 
+    use std::time::{SystemTime, UNIX_EPOCH};
+    use tiny_rng::Rng;
+    let mut rng = Rng::from_seed(SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_millis() as u64);
     if exit_status.success() {
+        if rng.rand_u32().is_multiple_of(11) {
+            let voiceline = rng.rand_range_u32(20, 23);
+            let sl = Soloud::default().unwrap();
+            let mut wav = audio::Wav::default();
+            match voiceline {
+                20 => voiceline_20(&mut wav),
+                21 => voiceline_21(&mut wav),
+                22 => voiceline_22(&mut wav),
+                _ => return Ok(())
+            }
+            sl.play(&wav);
+            while sl.voice_count() > 0 {
+                std::thread::sleep(std::time::Duration::from_millis(100));
+            }
+        }
         Ok(())
     } else {
+        let voiceline = rng.rand_range_u32(1, 20);
+        let sl = Soloud::default().unwrap();
+        let mut wav = audio::Wav::default();
+        match voiceline {
+            1 => voiceline_1(&mut wav),
+            2 => voiceline_2(&mut wav),
+            3 => voiceline_3(&mut wav),
+            4 => voiceline_4(&mut wav),
+            5 => voiceline_5(&mut wav),
+            6 => voiceline_6(&mut wav),
+            7 => voiceline_7(&mut wav),
+            8 => voiceline_8(&mut wav),
+            9 => voiceline_9(&mut wav),
+            10 => voiceline_10(&mut wav),
+            11 => voiceline_11(&mut wav),
+            12 => voiceline_12(&mut wav),
+            13 => voiceline_13(&mut wav),
+            14 => voiceline_14(&mut wav),
+            15 => voiceline_15(&mut wav),
+            16 => voiceline_16(&mut wav),
+            17 => voiceline_17(&mut wav),
+            18 => voiceline_18(&mut wav),
+            19 => voiceline_19(&mut wav),
+            _ => return Ok(())
+        }
+        sl.play(&wav);
+        while sl.voice_count() > 0 {
+            std::thread::sleep(std::time::Duration::from_millis(100));
+        }
         Err(exit_status.code().unwrap_or(-1))
     }
 }
@@ -182,8 +232,74 @@ You can use tool lints to allow or deny lints from your code, e.g.:
     <cyan,bold>--frozen</>                Require Cargo.lock and cache are up to date
     <cyan,bold>--locked</>                Require Cargo.lock is up to date
     <cyan,bold>--offline</>               Run without accessing the network
-")
+")}
+fn voiceline_1(wav: &mut Wav) {
+    wav.load_mem(include_bytes!("../voicelines/failure/15.wav")).unwrap();
 }
+fn voiceline_2(wav: &mut Wav) {
+    wav.load_mem(include_bytes!("../voicelines/failure/1.wav")).unwrap();
+}
+fn voiceline_3(wav: &mut Wav) {
+    wav.load_mem(include_bytes!("../voicelines/failure/2.wav")).unwrap();
+}
+fn voiceline_4(wav: &mut Wav) {
+    wav.load_mem(include_bytes!("../voicelines/failure/3.wav")).unwrap();
+}
+fn voiceline_5(wav: &mut Wav) {
+    wav.load_mem(include_bytes!("../voicelines/failure/4.wav")).unwrap();
+}
+fn voiceline_6(wav: &mut Wav) {
+    wav.load_mem(include_bytes!("../voicelines/failure/5.wav")).unwrap();
+}
+fn voiceline_7(wav: &mut Wav) {
+    wav.load_mem(include_bytes!("../voicelines/failure/6.wav")).unwrap();
+}
+fn voiceline_8(wav: &mut Wav) {
+    wav.load_mem(include_bytes!("../voicelines/failure/7.wav")).unwrap();
+}
+fn voiceline_9(wav: &mut Wav) {
+    wav.load_mem(include_bytes!("../voicelines/failure/8.wav")).unwrap();
+}
+fn voiceline_10(wav: &mut Wav) {
+    wav.load_mem(include_bytes!("../voicelines/failure/9.wav")).unwrap();
+}
+fn voiceline_11(wav: &mut Wav) {
+    wav.load_mem(include_bytes!("../voicelines/failure/10.wav")).unwrap();
+}
+fn voiceline_12(wav: &mut Wav) {
+    wav.load_mem(include_bytes!("../voicelines/failure/11.wav")).unwrap();
+}
+fn voiceline_13(wav: &mut Wav) {
+    wav.load_mem(include_bytes!("../voicelines/failure/12.wav")).unwrap();
+}
+fn voiceline_14(wav: &mut Wav) {
+    wav.load_mem(include_bytes!("../voicelines/failure/13.wav")).unwrap();
+}
+fn voiceline_15(wav: &mut Wav) {
+    wav.load_mem(include_bytes!("../voicelines/failure/14.wav")).unwrap();
+}
+fn voiceline_16(wav: &mut Wav) {
+    wav.load_mem(include_bytes!("../voicelines/failure/16.wav")).unwrap();
+}
+fn voiceline_17(wav: &mut Wav) {
+    wav.load_mem(include_bytes!("../voicelines/failure/17.wav")).unwrap();
+}
+fn voiceline_18(wav: &mut Wav) {
+    wav.load_mem(include_bytes!("../voicelines/failure/18.wav")).unwrap();
+}fn voiceline_19(wav: &mut Wav) {
+    wav.load_mem(include_bytes!("../voicelines/failure/19.wav")).unwrap();
+}
+fn voiceline_20(wav: &mut Wav) {
+    wav.load_mem(include_bytes!("../voicelines/success/20.wav")).unwrap();
+}
+fn voiceline_21(wav: &mut Wav) {
+    wav.load_mem(include_bytes!("../voicelines/success/21.wav")).unwrap();
+}
+fn voiceline_22(wav: &mut Wav) {
+    wav.load_mem(include_bytes!("../voicelines/success/22.wav")).unwrap();
+}
+
+
 #[cfg(test)]
 mod tests {
     use super::ClippyCmd;
