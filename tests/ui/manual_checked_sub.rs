50dcdb271161fd911e5d4b869f2f7146f4b578df
benacq <benacq44@gmail.com>
2025-03-28 17:41:16 +0000

    In progress

--- tests/ui/manual_checked_sub.rs
+++ tests/ui/manual_checked_sub.rs
@@ -40,33 +40,6 @@ fn positive_tests() {
         some_function(a - b);
     }
 
-    // Subtracting two suffixed literals
-    if 10u32 >= 5u32 {
-        //~^ manual_checked_sub
-        let _ = 10u32 - 5u32;
-    }
-
-    let x: i32 = 10;
-    let y: i32 = 5;
-
-    // Casted variables inside an if
-    if x as u32 >= y as u32 {
-        //~^ manual_checked_sub
-        let _ = x as u32 - y as u32;
-    }
-
-    // Casted variable and literal inside an if
-    if x as u32 >= 5u32 {
-        //~^ manual_checked_sub
-        let _ = x as u32 - 5u32;
-    }
-
-    // Casting subtraction result
-    if a >= b {
-        //~^ manual_checked_sub
-        let _ = (a - b) as u64;
-    }
-
     if a >= b {
         //~^ manual_checked_sub
         macro_rules! subtract {
@@ -127,15 +100,15 @@ fn negative_tests() {
         let _ = 10 - 5;
     }
 
-    // Suffixed LHS, unsuffixed RHS (shouldn't lint)
-    if 10u32 >= 5 {
-        let _ = 10u32 - 5;
-    }
+    // // Suffixed LHS, unsuffixed RHS (shouldn't lint)
+    // if 10u32 >= 5 {
+    //     let _ = 10u32 - 5;
+    // }
 
-    //  Unsuffixed LHS, suffixed RHS (shouldn't lint)
-    if 10 >= 5u32 {
-        let _ = 10 - 5u32;
-    }
+    // //  Unsuffixed LHS, suffixed RHS (shouldn't lint)
+    // if 10 >= 5u32 {
+    //     let _ = 10 - 5u32;
+    // }
 }
 
 fn main() {
