0e66fd9c0278e1313fd23bc483a862190c7f9d83
benacq <benacq44@gmail.com>
2025-03-28 17:41:16 +0000

    In progress

--- clippy_lints/src/manual_checked_sub.rs
+++ clippy_lints/src/manual_checked_sub.rs
@@ -1,13 +1,14 @@
 use clippy_config::Conf;
 use clippy_utils::diagnostics::span_lint_and_sugg;
 use clippy_utils::msrvs::{self, Msrv};
+use clippy_utils::path_to_local;
 use clippy_utils::source::snippet_with_applicability;
+use clippy_utils::sugg::Sugg;
 use rustc_ast::{BinOpKind, LitIntType, LitKind};
 use rustc_data_structures::packed::Pu128;
 use rustc_errors::Applicability;
-use rustc_hir::def::Res;
 use rustc_hir::intravisit::{Visitor, walk_expr};
-use rustc_hir::{Expr, ExprKind, QPath};
+use rustc_hir::{Expr, ExprKind};
 use rustc_lint::{LateContext, LateLintPass};
 use rustc_middle::ty::{self};
 use rustc_session::impl_lint_pass;
@@ -54,45 +55,27 @@ impl_lint_pass!(ManualCheckedSub => [MANUAL_CHECKED_SUB]);
 
 impl<'tcx> LateLintPass<'tcx> for ManualCheckedSub {
     fn check_expr(&mut self, cx: &LateContext<'tcx>, expr: &'tcx Expr<'_>) {
-        if !self.msrv.meets(cx, msrvs::MANUAL_CHECKED_SUB) {
+        if !self.msrv.meets(cx, msrvs::CHECKED_SUB) {
             return;
         }
 
-        let applicability = Applicability::MachineApplicable;
-
-        if let ExprKind::If(drop_temp_expr, body_block, else_block) = expr.kind
-            && let ExprKind::DropTemps(condition_expr) = drop_temp_expr.kind
-            && let ExprKind::Binary(op, lhs, rhs) = condition_expr.kind
-            && is_unsigned_int(cx, lhs)
-            && is_unsigned_int(cx, rhs)
-        {
-            if let BinOpKind::Ge = op.node {
-                SubExprVisitor {
-                    cx,
-                    if_expr: expr,
-                    if_body_block: body_block,
-                    else_block,
-                    applicability,
-                    condition_lhs: lhs,
-                    condition_rhs: rhs,
-                }
-                .visit_expr(body_block);
-            }
-
-            if let BinOpKind::Gt = op.node {
-                if let ExprKind::Lit(lit) = &rhs.kind
-                    && let LitKind::Int(Pu128(0), _) = lit.node
-                {
+        if let Some(if_expr) = clippy_utils::higher::If::hir(expr) {
+            if let ExprKind::Binary(op, lhs, rhs) = if_expr.cond.kind
+                && !(matches!(lhs.kind, ExprKind::Lit(_)) && matches!(rhs.kind, ExprKind::Lit(_)))
+                && is_unsigned_int(cx, lhs)
+                && is_unsigned_int(cx, rhs)
+            {
+                if let BinOpKind::Ge | BinOpKind::Gt = op.node {
                     SubExprVisitor {
                         cx,
                         if_expr: expr,
-                        if_body_block: body_block,
-                        else_block,
-                        applicability,
+                        if_body_block: if_expr.then,
+                        else_block: if_expr.r#else,
                         condition_lhs: lhs,
                         condition_rhs: rhs,
+                        condition_op: op.node,
                     }
-                    .visit_expr(body_block);
+                    .visit_expr(if_expr.then);
                 }
             }
         }
@@ -104,9 +87,9 @@ struct SubExprVisitor<'a, 'tcx> {
     if_expr: &'tcx Expr<'tcx>,
     if_body_block: &'tcx Expr<'tcx>,
     else_block: Option<&'tcx Expr<'tcx>>,
-    applicability: Applicability,
     condition_lhs: &'tcx Expr<'tcx>,
     condition_rhs: &'tcx Expr<'tcx>,
+    condition_op: BinOpKind,
 }
 
 impl<'tcx> Visitor<'tcx> for SubExprVisitor<'_, 'tcx> {
@@ -115,17 +98,18 @@ impl<'tcx> Visitor<'tcx> for SubExprVisitor<'_, 'tcx> {
             && let BinOpKind::Sub = op.node
         {
             if let ExprKind::Lit(lit) = self.condition_rhs.kind
+                && self.condition_op == BinOpKind::Gt
                 && let LitKind::Int(Pu128(0), _) = lit.node
+                && (is_referencing_same_variable(sub_lhs, self.condition_lhs))
+            {
+                self.emit_lint(expr, Some(sub_rhs));
+            } else if self.condition_op == BinOpKind::Ge
                 && (is_referencing_same_variable(sub_lhs, self.condition_lhs)
                     || are_literals_equal(sub_lhs, self.condition_lhs))
-            {
-                self.build_suggession(expr);
-            } else if (is_referencing_same_variable(sub_lhs, self.condition_lhs)
-                || are_literals_equal(sub_lhs, self.condition_lhs))
                 && (is_referencing_same_variable(sub_rhs, self.condition_rhs)
                     || are_literals_equal(sub_rhs, self.condition_rhs))
             {
-                self.build_suggession(expr);
+                self.emit_lint(expr, None);
             }
         }
 
@@ -134,28 +118,35 @@ impl<'tcx> Visitor<'tcx> for SubExprVisitor<'_, 'tcx> {
 }
 
 impl<'tcx> SubExprVisitor<'_, 'tcx> {
-    fn build_suggession(&mut self, expr: &Expr<'tcx>) {
-        let body_snippet = snippet_with_applicability(self.cx, self.if_body_block.span, "..", &mut self.applicability);
+    // fn is_name_in_scope(&self, name: &str, id: HirId) -> bool {
+    //     let def_id = self.cx.tcx.hir().local_def_id(id);
+    //     let resolver = self.cx.tcx.resolver_for_lowering();
 
-        let binary_expr_snippet = snippet_with_applicability(self.cx, expr.span, "..", &mut self.applicability);
-        let updated_usage_context_snippet = body_snippet.as_ref().replace(binary_expr_snippet.as_ref(), "result");
-        // let else_snippet = snippet_with_applicability(self.cx, self.if_else_block.span, "..", &mut
-        // self.applicability);
+    //     // Check if the name is already defined in the current scope
+    //     resolver.is_name_defined(name)
+    // }
 
-        let lhs_snippet = snippet_with_applicability(self.cx, self.condition_lhs.span, "..", &mut self.applicability);
-        let rhs_snippet = snippet_with_applicability(self.cx, self.condition_rhs.span, "..", &mut self.applicability);
+    fn emit_lint(&mut self, expr: &Expr<'tcx>, sub_rhs_expr: Option<&'tcx Expr<'tcx>>) {
+        let mut applicability = Applicability::MachineApplicable;
 
-        let lhs_needs_parens = matches!(self.condition_lhs.kind, ExprKind::Cast(..));
+        let body_snippet = snippet_with_applicability(self.cx, self.if_body_block.span, "..", &mut applicability);
 
-        let mut suggestion = if lhs_needs_parens {
-            format!("if let Some(result) = ({lhs_snippet}).checked_sub({rhs_snippet}) {updated_usage_context_snippet}")
-        } else {
-            format!("if let Some(result) = {lhs_snippet}.checked_sub({rhs_snippet})  {updated_usage_context_snippet}")
+        let binary_expr_snippet = snippet_with_applicability(self.cx, expr.span, "..", &mut applicability);
+        let updated_usage_context_snippet = body_snippet.as_ref().replace(binary_expr_snippet.as_ref(), "diff");
+
+        let rhs_snippet = match sub_rhs_expr {
+            Some(sub_rhs) => Sugg::hir(self.cx, sub_rhs, "..").maybe_par(),
+            None => Sugg::hir(self.cx, self.condition_rhs, "..").maybe_par(),
         };
 
+        // TODO: Check diff variable is not already in scope
+        let mut suggestion = format!(
+            "if let Some(diff) = {}.checked_sub({rhs_snippet}) {updated_usage_context_snippet}",
+            Sugg::hir(self.cx, self.condition_lhs, "..").maybe_par()
+        );
+
         if let Some(else_expr) = self.else_block {
-            let else_snippet = snippet_with_applicability(self.cx, else_expr.span, "..", &mut self.applicability);
-            // suggestion.push_str(&format!(" else {else_snippet}"));
+            let else_snippet = snippet_with_applicability(self.cx, else_expr.span, "..", &mut applicability);
             write!(suggestion, " else {else_snippet}").unwrap();
         }
 
@@ -166,7 +157,7 @@ impl<'tcx> SubExprVisitor<'_, 'tcx> {
             "manual re-implementation of checked subtraction",
             "consider using `.checked_sub()`",
             suggestion,
-            self.applicability,
+            applicability,
         );
     }
 }
@@ -177,63 +168,26 @@ fn is_unsigned_int<'tcx>(cx: &LateContext<'tcx>, expr: &Expr<'tcx>) -> bool {
         return true;
     }
 
-    if let ExprKind::Lit(lit) = &expr.kind {
-        if let LitKind::Int(_, LitIntType::Unsuffixed) = &lit.node {
-            return true;
-        }
-    }
     false
 }
 
 fn are_literals_equal<'tcx>(expr1: &Expr<'tcx>, expr2: &Expr<'tcx>) -> bool {
-    if let (ExprKind::Lit(lit1), ExprKind::Lit(lit2)) = (&expr1.kind, &expr2.kind) {
-        if let (LitKind::Int(val1, suffix1), LitKind::Int(val2, suffix2)) = (&lit1.node, &lit2.node) {
-            return val1 == val2
-                && suffix1 == suffix2
-                && *suffix1 != LitIntType::Unsuffixed
-                && *suffix2 != LitIntType::Unsuffixed;
-        }
+    if let (ExprKind::Lit(lit1), ExprKind::Lit(lit2)) = (&expr1.kind, &expr2.kind)
+        && let (LitKind::Int(val1, suffix1), LitKind::Int(val2, suffix2)) = (&lit1.node, &lit2.node)
+    {
+        return val1 == val2
+            && suffix1 == suffix2
+            && *suffix1 != LitIntType::Unsuffixed
+            && *suffix2 != LitIntType::Unsuffixed;
     }
+
     false
 }
+
 fn is_referencing_same_variable<'tcx>(expr1: &'tcx Expr<'_>, expr2: &'tcx Expr<'_>) -> bool {
-    if let ExprKind::Cast(cast_expr1, _) = &expr1.kind {
-        if let Some(cast_expr1_path) = get_resolved_path_id(cast_expr1) {
-            if let ExprKind::Cast(cast_expr2, _) = &expr2.kind {
-                if let Some(cast_expr2_path) = get_resolved_path_id(cast_expr2) {
-                    return cast_expr1_path == cast_expr2_path;
-                }
-            } else {
-                if let Some(expr2_path) = get_resolved_path_id(expr2) {
-                    return cast_expr1_path == expr2_path;
-                }
-                return false;
-            }
-        }
-    } else if let ExprKind::Cast(cast_expr2, _) = &expr2.kind {
-        if let Some(cast_expr2_path) = get_resolved_path_id(cast_expr2) {
-            if let Some(expr1_path) = get_resolved_path_id(expr1) {
-                return expr1_path == cast_expr2_path;
-            }
-            return false;
-        }
+    if let (Some(id1), Some(id2)) = (path_to_local(expr1), path_to_local(expr2)) {
+        return id1 == id2;
     }
 
-    let expr1_path = get_resolved_path_id(expr1);
-    let expr2_path = get_resolved_path_id(expr2);
-
-    if let (Some(expr1_path), Some(expr2_path)) = (expr1_path, expr2_path) {
-        expr1_path == expr2_path
-    } else {
-        false
-    }
-}
-
-fn get_resolved_path_id(expr: &Expr<'_>) -> Option<Res> {
-    if let ExprKind::Path(QPath::Resolved(_, path)) = &expr.kind {
-        // path.segments.first().and_then(|segment| Some(segment.res))
-        path.segments.first().map(|segment| segment.res)
-    } else {
-        None
-    }
+    false
 }
