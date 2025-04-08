76a01466ef4b274b902645d0d7e6b70d82a0da2c
benacq <benacq44@gmail.com>
2025-04-08 07:24:51 +0000

    manual_checked_sub implementation review feedback fixes - Variable generator function

--- clippy_lints/src/manual_checked_sub.rs
+++ clippy_lints/src/manual_checked_sub.rs
@@ -7,8 +7,8 @@ use clippy_utils::{is_mutable, path_to_local};
 use rustc_ast::{BinOpKind, LitIntType, LitKind};
 use rustc_data_structures::packed::Pu128;
 use rustc_errors::Applicability;
-use rustc_hir::intravisit::{Visitor, walk_expr};
-use rustc_hir::{Expr, ExprKind};
+use rustc_hir::intravisit::{Visitor, walk_expr, walk_stmt};
+use rustc_hir::{Expr, ExprKind, PatKind, QPath};
 use rustc_lint::{LateContext, LateLintPass};
 use rustc_middle::ty::{self};
 use rustc_session::impl_lint_pass;
@@ -95,8 +95,8 @@ impl<'tcx> LateLintPass<'tcx> for ManualCheckedSub {
     }
 }
 
-struct SubExprVisitor<'a, 'tcx> {
-    cx: &'a LateContext<'tcx>,
+struct SubExprVisitor<'cx, 'tcx> {
+    cx: &'cx LateContext<'tcx>,
     if_expr: &'tcx Expr<'tcx>,
     if_body_block: &'tcx Expr<'tcx>,
     else_block: Option<&'tcx Expr<'tcx>>,
@@ -155,8 +155,15 @@ impl<'tcx> SubExprVisitor<'_, 'tcx> {
 
         let body_snippet = snippet_with_applicability(self.cx, self.if_body_block.span, "..", &mut applicability);
 
+        let checked_sub_result_var_name = match self.condition_op {
+            BinOpKind::Lt | BinOpKind::Gt => generate_unique_var_name("decremented", self.if_body_block),
+            _ => generate_unique_var_name("difference", self.if_body_block),
+        };
+
         let binary_expr_snippet = snippet_with_applicability(self.cx, expr.span, "..", &mut applicability);
-        let updated_usage_context_snippet = body_snippet.as_ref().replace(binary_expr_snippet.as_ref(), "result");
+        let updated_usage_context_snippet = body_snippet
+            .as_ref()
+            .replace(binary_expr_snippet.as_ref(), &checked_sub_result_var_name);
 
         let lhs_snippet = Sugg::hir(self.cx, self.condition_lhs, "..").maybe_paren();
 
@@ -169,19 +176,19 @@ impl<'tcx> SubExprVisitor<'_, 'tcx> {
         let mut suggestion = match self.condition_op {
             BinOpKind::Le => {
                 format!(
-                    "if let Some(result) = {rhs_snippet}.checked_sub({lhs_snippet}) {updated_usage_context_snippet}"
+                    "if let Some({checked_sub_result_var_name}) = {rhs_snippet}.checked_sub({lhs_snippet}) {updated_usage_context_snippet}"
                 )
             },
 
             BinOpKind::Lt => {
                 format!(
-                    "if let Some(result) = {}.checked_sub({rhs_snippet}) {updated_usage_context_snippet}",
+                    "if let Some({checked_sub_result_var_name}) = {}.checked_sub({rhs_snippet}) {updated_usage_context_snippet}",
                     Sugg::hir(self.cx, self.condition_rhs, "..").maybe_paren()
                 )
             },
             _ => {
                 format!(
-                    "if let Some(result) = {lhs_snippet}.checked_sub({rhs_snippet}) {updated_usage_context_snippet}"
+                    "if let Some({checked_sub_result_var_name}) = {lhs_snippet}.checked_sub({rhs_snippet}) {updated_usage_context_snippet}"
                 )
             },
         };
@@ -216,10 +223,7 @@ fn are_literals_equal<'tcx>(expr1: &Expr<'tcx>, expr2: &Expr<'tcx>) -> bool {
     if let (ExprKind::Lit(lit1), ExprKind::Lit(lit2)) = (&expr1.kind, &expr2.kind)
         && let (LitKind::Int(val1, suffix1), LitKind::Int(val2, suffix2)) = (&lit1.node, &lit2.node)
     {
-        return val1 == val2
-            && suffix1 == suffix2
-            && *suffix1 != LitIntType::Unsuffixed
-            && *suffix2 != LitIntType::Unsuffixed;
+        return val1 == val2 && suffix1 == suffix2 && *suffix1 != LitIntType::Unsuffixed;
     }
 
     false
@@ -231,4 +235,93 @@ fn is_referencing_same_variable<'tcx>(expr1: &'tcx Expr<'_>, expr2: &'tcx Expr<'
     }
 
     false
+}
+/// Generates a unique variable name based on the provided base name.
+/// If the base name already exists in the scope, appends a number to make it unique.
+fn generate_unique_var_name<'tcx>(base_name: &str, scope_expr: &'tcx Expr<'tcx>) -> String {
+    struct VarNameVisitor<'cx> {
+        base_name: &'cx str,
+        is_name_in_scope: bool,
+    }
+
+    impl<'tcx> Visitor<'tcx> for VarNameVisitor<'_> {
+        fn visit_expr(&mut self, expr: &'tcx Expr<'_>) {
+            if let ExprKind::Path(QPath::Resolved(_, path)) = &expr.kind {
+                if let Some(segment) = path.segments.last() {
+                    let name = segment.ident.name.to_string();
+                    if name == self.base_name {
+                        self.is_name_in_scope = true;
+                    }
+                }
+            }
+
+            walk_expr(self, expr);
+        }
+
+        fn visit_stmt(&mut self, stmt: &'tcx rustc_hir::Stmt<'_>) {
+            if let rustc_hir::StmtKind::Let(let_stmt) = &stmt.kind {
+                if let PatKind::Binding(_, _, ident, _) = &let_stmt.pat.kind {
+                    let name = ident.name.to_string();
+                    if name == self.base_name {
+                        self.is_name_in_scope = true;
+                    }
+                }
+            }
+
+            walk_stmt(self, stmt);
+        }
+    }
+
+    let mut visitor = VarNameVisitor {
+        base_name,
+        is_name_in_scope: false,
+    };
+
+    if let ExprKind::Block(block, _) = &scope_expr.kind {
+        for stmt in block.stmts {
+            visitor.visit_stmt(stmt);
+        }
+
+        // Visit the optional expression at the end of the block
+        if let Some(expr) = block.expr {
+            visitor.visit_expr(expr);
+        }
+    } else {
+        visitor.visit_expr(scope_expr);
+    }
+
+    if !visitor.is_name_in_scope {
+        return base_name.to_string();
+    }
+
+    // If the base name is in scope, append a number
+    // Keep incrementing until we find a name that's not in scope
+    let mut counter = 1;
+    loop {
+        let candidate = format!("{}_{}", base_name, counter);
+
+        let mut candidate_visitor = VarNameVisitor {
+            base_name: &candidate,
+            is_name_in_scope: false,
+        };
+
+        // Check if the candidate name is in scope
+        if let ExprKind::Block(block, _) = &scope_expr.kind {
+            for stmt in block.stmts {
+                candidate_visitor.visit_stmt(stmt);
+            }
+
+            if let Some(expr) = block.expr {
+                candidate_visitor.visit_expr(expr);
+            }
+        } else {
+            candidate_visitor.visit_expr(scope_expr);
+        }
+
+        if !candidate_visitor.is_name_in_scope {
+            return candidate;
+        }
+
+        counter += 1;
+    }
 }
