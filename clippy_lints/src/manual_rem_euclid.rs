8743be2099f83c3e6c765778f26c1e6a8f3df0ba
Jason Newcomb <jsnewcomb@pm.me>
2025-04-20 21:37:31 -0400

    Rewrite of the config parsing code
    * More precise spans in diagnostics
    * Allow multiple diagnostics for each config value
    * Ability to capture spans in the parsed config
    * Easier support of `..` in lists
    * Remove `termize` dependency. Use values from rustc instead.

--- clippy_lints/src/manual_rem_euclid.rs
+++ clippy_lints/src/manual_rem_euclid.rs
@@ -41,7 +41,7 @@ pub struct ManualRemEuclid {
 
 impl ManualRemEuclid {
     pub fn new(conf: &'static Conf) -> Self {
-        Self { msrv: conf.msrv }
+        Self { msrv: conf.msrv.into() }
     }
 }
 
