d9f98e3727f729e8c0df4f4b7dc615b8f7734e0a
Ada Alakbarova <ada.alakbarova@proton.me>
2025-04-23 01:15:05 +0200

    add some docs

--- clippy_lints/src/needless_path_new.rs
+++ clippy_lints/src/needless_path_new.rs
@@ -5,21 +5,27 @@ use rustc_session::declare_lint_pass;
 
 declare_clippy_lint! {
     /// ### What it does
+    /// Detects expressions being enclosed in `Path::new` when passed to a function that accepts
+    /// `impl AsRef<Path>`, when the enclosed expression could be used.
     ///
     /// ### Why is this bad?
+    /// It is unnecessarily verbose
     ///
     /// ### Example
     /// ```no_run
-    /// // example code where clippy issues a warning
+    /// # use std::{fs, path::Path};
+    /// fs::write(Path::new("foo.txt"), "foo");
     /// ```
     /// Use instead:
     /// ```no_run
-    /// // example code which does not raise clippy warning
+    /// # use std::{fs, path::Path};
+    /// fs::write("foo.txt", "foo");
     /// ```
     #[clippy::version = "1.90.0"]
     pub NEEDLESS_PATH_NEW,
     nursery,
-    "default lint description"
+    "an argument passed to a function  that accepts `impl AsRef<Path>` \
+    being enclosed in `Path::new` when the argument implements the trait"
 }
 
 declare_lint_pass!(NeedlessPathNew => [NEEDLESS_PATH_NEW]);
