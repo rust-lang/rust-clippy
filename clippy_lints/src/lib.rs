b1f33407144c8fceb603b37d0625a6b88b762387
benacq <benacq44@gmail.com>
2025-04-24 08:05:30 +0000

    Revert "Macro guard implementation and lint suggestion refector"
    This reverts commit ae4d4939eab3c85772a09df458a88b579966eeaa.

--- clippy_lints/src/lib.rs
+++ clippy_lints/src/lib.rs
@@ -943,7 +943,8 @@ pub fn register_lints(store: &mut rustc_lint::LintStore, conf: &'static Conf) {
     store.register_late_pass(move |_| Box::new(non_std_lazy_statics::NonStdLazyStatic::new(conf)));
     store.register_late_pass(|_| Box::new(manual_option_as_slice::ManualOptionAsSlice::new(conf)));
     store.register_late_pass(|_| Box::new(single_option_map::SingleOptionMap));
-    store.register_late_pass(move |_| Box::new(redundant_test_prefix::RedundantTestPrefix));
     store.register_late_pass(move |_| Box::new(manual_checked_sub::ManualCheckedSub::new(conf)));
+    store.register_late_pass(move |_| Box::new(redundant_test_prefix::RedundantTestPrefix));
+
     // add lints here, do not remove this comment, it's used in `new_lint`
 }
