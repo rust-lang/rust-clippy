f0a157c32d8fc5faedc6865f4a2e52a415932d53
Jason Newcomb <jsnewcomb@pm.me>
2025-05-01 23:53:16 -0400

    Rename `SpanRangeExt` to `SpanExt`

--- clippy_lints/src/legacy_numeric_constants.rs
+++ clippy_lints/src/legacy_numeric_constants.rs
@@ -2,7 +2,7 @@ use clippy_config::Conf;
 use clippy_utils::diagnostics::span_lint_and_then;
 use clippy_utils::is_from_proc_macro;
 use clippy_utils::msrvs::{self, Msrv};
-use clippy_utils::source::SpanRangeExt;
+use clippy_utils::source::SpanExt;
 use hir::def_id::DefId;
 use rustc_errors::Applicability;
 use rustc_hir as hir;
