d3a4cfd494a05fdc90ac85f93967a50aa8829c17
Dominic Moser <llamapagama@gmail.com>
2025-05-02 19:47:20 -0600

    took advice given!

--- clippy_lints/src/declared_lints.rs
+++ clippy_lints/src/declared_lints.rs
@@ -5,6 +5,7 @@
 pub static LINTS: &[&crate::LintInfo] = &[
     crate::absolute_paths::ABSOLUTE_PATHS_INFO,
     crate::almost_complete_range::ALMOST_COMPLETE_RANGE_INFO,
+    crate::always_true_conditions::ALWAYS_TRUE_CONDITIONS_INFO,
     crate::approx_const::APPROX_CONSTANT_INFO,
     crate::arbitrary_source_item_ordering::ARBITRARY_SOURCE_ITEM_ORDERING_INFO,
     crate::arc_with_non_send_sync::ARC_WITH_NON_SEND_SYNC_INFO,
---
+++ tests/ui/always_true_conditions.rs
@@ -0,0 +1,43 @@
+#![warn(clippy::always_true_conditions)]
+#[allow(clippy::needless_if)]
+fn foo_m(name: &str) {
+    if name != "Min" || name != "Max" || name != "Middle" {
+        //~^ always_true_conditions
+        println!("always prints");
+    } else {
+        println!("never prints");
+    }
+    if name != "Min" && name != "Max" {
+        println!("condition satisfied");
+    } else {
+        println!("else");
+    }
+}
+
+fn foo_s(name: &str) {
+    if name != "Min" || name != "Max" {
+        //~^ always_true_conditions
+        println!("always prints");
+    } else {
+        println!("never prints");
+    }
+    if name != "Min" && name != "Max" {
+        println!("condition satisfied");
+    } else {
+        println!("else");
+    }
+}
+
+fn catch_or_failure(input: &str) {
+    let b = true;
+    if b || input != "foo" {
+        println!("should not fire!");
+    }
+}
+
+fn catch_diff_var_failure(input: &str) {
+    let b = "value";
+    if b != "bar" || input != "foo" {
+        println!("should not fire!");
+    }
+}
