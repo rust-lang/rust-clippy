adf90014ff07a70254f459cdb3fd662959e73095
nils-degroot <nils@peeko.nl>
2025-05-15 12:07:41 +0200

    changelog: Add `manual_as_slice` lint

--- clippy_lints/src/lib.rs
+++ clippy_lints/src/lib.rs
@@ -201,6 +201,7 @@ mod macro_metavars_in_unsafe;
 mod macro_use;
 mod main_recursion;
 mod manual_abs_diff;
+mod manual_as_slice;
 mod manual_assert;
 mod manual_assert_eq;
 mod manual_async_fn;
@@ -870,6 +871,7 @@ rustc_lint::late_lint_methods!(
         BlockScrutinee: block_scrutinee::BlockScrutinee = block_scrutinee::BlockScrutinee,
         NonnullUncheckedOnBoxPtr: nonnull_unchecked_on_box_ptr::NonnullUncheckedOnBoxPtr = nonnull_unchecked_on_box_ptr::NonnullUncheckedOnBoxPtr::new(conf),
         NeedlessNonzeroGet: needless_nonzero_get::NeedlessNonzeroGet = needless_nonzero_get::NeedlessNonzeroGet::new(conf),
+        ManualAsSlice: manual_as_slice::ManualAsSlice = manual_as_slice::ManualAsSlice,
         // add late passes here, used by `cargo dev new_lint`
     ]]
 );
