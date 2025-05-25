ca1e42c6f14de8777406413170dc762b63bc5d22
Ada Alakbarova <ada.alakbarova@proton.me>
2025-05-25 18:16:23 +0200

    add more tests

--- tests/ui/needless_path_new.rs
+++ tests/ui/needless_path_new.rs
@@ -3,6 +3,22 @@
 use std::fs;
 use std::path::Path;
 
+fn takes_path(_: &Path) {}
+
+fn takes_path_and_impl_path(_: &Path, _: impl AsRef<Path>) {}
+
 fn main() {
-    fs::write(Path::new("foo.txt"), "foo");
+    fs::write(Path::new("foo.txt"), "foo"); //~ needless_path_new
+
+    fs::copy(
+        Path::new("foo"), //~ needless_path_new
+        Path::new("bar"), //~ needless_path_new
+    );
+
+    // no warning
+    takes_path(Path::new("foo"));
+
+    // the paramater that _could_ be passed directly, was
+    // the parameter that could't, wasn't
+    takes_path_and_impl_path(Path::new("foo"), "foo");
 }
