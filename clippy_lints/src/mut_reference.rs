58d58a5922b46eeec10acd982011049372548b5a
Ada Alakbarova <ada.alakbarova@proton.me>
2025-05-25 02:16:34 +0200

    match -> if-let

--- clippy_lints/src/mut_reference.rs
+++ clippy_lints/src/mut_reference.rs
@@ -82,18 +82,15 @@ fn check_arguments<'tcx>(
     if let ty::FnDef(..) | ty::FnPtr(..) = type_definition.kind() {
         let parameters = type_definition.fn_sig(cx.tcx).skip_binder().inputs();
         for (argument, parameter) in iter::zip(arguments, parameters) {
-            match parameter.kind() {
-                ty::Ref(_, _, Mutability::Not) | ty::RawPtr(_, Mutability::Not) => {
-                    if let ExprKind::AddrOf(BorrowKind::Ref, Mutability::Mut, _) = argument.kind {
-                        span_lint(
-                            cx,
-                            UNNECESSARY_MUT_PASSED,
-                            argument.span,
-                            format!("the {fn_kind} `{name}` doesn't need a mutable reference"),
-                        );
-                    }
-                },
-                _ => (),
+            if let ty::Ref(_, _, Mutability::Not) | ty::RawPtr(_, Mutability::Not) = parameter.kind() {
+                if let ExprKind::AddrOf(BorrowKind::Ref, Mutability::Mut, _) = argument.kind {
+                    span_lint(
+                        cx,
+                        UNNECESSARY_MUT_PASSED,
+                        argument.span,
+                        format!("the {fn_kind} `{name}` doesn't need a mutable reference"),
+                    );
+                }
             }
         }
     }
