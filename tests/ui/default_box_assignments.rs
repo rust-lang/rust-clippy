5583109c19cdc1bfccacafa852c913e3ed448fa2
+merlan #flirora <flirora@flirora.xyz>
2025-06-02 15:09:27 -0400

    Add test for macro expansion

--- tests/ui/default_box_assignments.rs
+++ tests/ui/default_box_assignments.rs
@@ -1,5 +1,11 @@
 #![warn(clippy::default_box_assignments)]
 
+macro_rules! create_default {
+    () => {
+        Default::default()
+    };
+}
+
 fn main() {
     let mut b = Box::new(1u32);
     b = Default::default();
@@ -11,7 +17,10 @@ fn main() {
     *b = Default::default();
     *b = u32::default();
 
-    // No lint for assigning to Box<T> where T is unsized
+    // No lint for call originating in macro
+    b = create_default!();
+
+    // No lint for assigning to Box<T> where T: !Default
     let mut b = Box::<str>::from("hi".to_string());
     b = Default::default();
 }
