4cdbec31117f47d9402f17f676317e1993325ba2
Valdemar Erk <valdemar@erk.dev>
2025-06-06 14:21:43 +0200

    add new lint: `rest_when_destructuring_struct`

--- clippy_lints/src/lib.rs
+++ clippy_lints/src/lib.rs
@@ -329,6 +329,7 @@ mod reference;
 mod regex;
 mod repeat_vec_with_capacity;
 mod reserve_after_initialization;
+mod rest_when_destructuring_struct;
 mod return_self_not_must_use;
 mod returns;
 mod same_name_method;
@@ -951,5 +952,6 @@ pub fn register_lints(store: &mut rustc_lint::LintStore, conf: &'static Conf) {
     store.register_late_pass(|_| Box::new(cloned_ref_to_slice_refs::ClonedRefToSliceRefs::new(conf)));
     store.register_late_pass(|_| Box::new(infallible_try_from::InfallibleTryFrom));
     store.register_late_pass(|_| Box::new(coerce_container_to_any::CoerceContainerToAny));
+    store.register_early_pass(|| Box::new(rest_when_destructuring_struct::RestWhenDestructuringStruct));
     // add lints here, do not remove this comment, it's used in `new_lint`
 }
