4e14493c4b3da781443321449d24158b295d9c4c
newt <hi@newty.dev>
2025-07-06 01:14:58 +0100

    Fix tests impacted by this change

--- tests/ui/unnecessary_clippy_cfg.rs
+++ tests/ui/unnecessary_clippy_cfg.rs
@@ -5,21 +5,27 @@
 //~^ unnecessary_clippy_cfg
 #![cfg_attr(clippy, deny(dead_code, clippy::non_minimal_cfg))]
 //~^ unnecessary_clippy_cfg
+//~| duplicated_attributes
 #![cfg_attr(clippy, deny(dead_code, clippy::non_minimal_cfg))]
 //~^ unnecessary_clippy_cfg
 //~| duplicated_attributes
+//~| duplicated_attributes
 #![cfg_attr(clippy, deny(clippy::non_minimal_cfg))]
 //~^ unnecessary_clippy_cfg
+//~| duplicated_attributes
 
 #[cfg_attr(clippy, deny(clippy::non_minimal_cfg))]
 //~^ unnecessary_clippy_cfg
 #[cfg_attr(clippy, deny(dead_code, clippy::non_minimal_cfg))]
 //~^ unnecessary_clippy_cfg
+//~| duplicated_attributes
 #[cfg_attr(clippy, deny(dead_code, clippy::non_minimal_cfg))]
 //~^ unnecessary_clippy_cfg
 //~| duplicated_attributes
+//~| duplicated_attributes
 #[cfg_attr(clippy, deny(clippy::non_minimal_cfg))]
 //~^ unnecessary_clippy_cfg
+//~| duplicated_attributes
 
 pub struct Bar;
 
