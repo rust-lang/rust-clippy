bd6fba295b9f093febcb433fc0314257955cf360
lakshay bhatia <bhatia00lakshay@gmail.com>
2025-07-10 19:58:32 +0530

    Fixed Failing job checks

--- tests/ui/large_enum_variant_no_std.rs
+++ tests/ui/large_enum_variant_no_std.rs
@@ -10,7 +10,7 @@ fn panic(_: &PanicInfo) -> ! {
 }
 
 enum Myenum {
-    //~ ERROR: large size difference between variants
+    //~^ ERROR: large size difference between variants
     Small(u8),
     Large([u8; 1024]),
 }
