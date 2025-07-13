8b920b695e2984e142a368a59554920c9ca1db9a
yanglsh <yanglsh@shanghaitech.edu.cn>
2025-07-13 21:33:00 +0800

    fix: `as_ptr_cast_mut` FP when `self` is not mutable
    This can happen in `unsafe`

--- clippy_lints/src/casts/as_ptr_cast_mut.rs
+++ clippy_lints/src/casts/as_ptr_cast_mut.rs
@@ -1,4 +1,5 @@
 use clippy_utils::diagnostics::span_lint_and_sugg;
+use clippy_utils::is_mutable;
 use clippy_utils::source::SpanRangeExt;
 use rustc_errors::Applicability;
 use rustc_hir::{Expr, ExprKind};
@@ -20,6 +21,7 @@ pub(super) fn check(cx: &LateContext<'_>, expr: &Expr<'_>, cast_expr: &Expr<'_>,
         && let Some(first_param_ty) = as_ptr_sig.skip_binder().inputs().iter().next()
         && let ty::Ref(_, _, Mutability::Not) = first_param_ty.kind()
         && let Some(recv) = receiver.span.get_source_text(cx)
+        && is_mutable(cx, receiver)
     {
         // `as_mut_ptr` might not exist
         let applicability = Applicability::MaybeIncorrect;
