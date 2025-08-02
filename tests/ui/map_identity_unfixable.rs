b4065d1f67a26e07b844adf9399d7695fa3fd30e
Ada Alakbarova <ada.alakbarova@proton.me>
2025-08-03 00:44:34 +0200

    make the unfixable case fixable by using `path_to_local_with_projections`

--- tests/ui/map_identity_unfixable.rs
+++
@@ -1,9 +0,0 @@
-//@no-rustfix
-#![warn(clippy::map_identity)]
-
-fn main() {
-    let mut index = [true, true, false, false, false, true].iter();
-    let subindex = (index.by_ref().take(3), 42);
-    let _ = subindex.0.map(|n| n).next();
-    //~^ map_identity
-}
