571765e1e3dfb900e2d4736d120d92ba9406a2ad
Ada Alakbarova <ada.alakbarova@proton.me>
2025-08-02 21:45:51 +0200

    use in `double_ended_iterator_last`

--- tests/ui/double_ended_iterator_last_unfixable.rs
+++ tests/ui/double_ended_iterator_last_unfixable.rs
@@ -9,31 +9,3 @@ fn main() {
     let _ = subindex.0.last();
     let _ = index.next();
 }
-
-fn drop_order() {
-    struct DropDeIterator(std::vec::IntoIter<S>);
-    impl Iterator for DropDeIterator {
-        type Item = S;
-        fn next(&mut self) -> Option<Self::Item> {
-            self.0.next()
-        }
-    }
-    impl DoubleEndedIterator for DropDeIterator {
-        fn next_back(&mut self) -> Option<Self::Item> {
-            self.0.next_back()
-        }
-    }
-
-    struct S(&'static str);
-    impl std::ops::Drop for S {
-        fn drop(&mut self) {
-            println!("Dropping {}", self.0);
-        }
-    }
-
-    let v = vec![S("one"), S("two"), S("three")];
-    let v = (DropDeIterator(v.into_iter()), 42);
-    println!("Last element is {}", v.0.last().unwrap().0);
-    //~^ ERROR: called `Iterator::last` on a `DoubleEndedIterator`
-    println!("Done");
-}
