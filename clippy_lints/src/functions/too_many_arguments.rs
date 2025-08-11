c0f379eda4df3d1f770f9fcb7a8b30bf9a810c4b
Ada Alakbarova <ada.alakbarova@proton.me>
2025-08-12 01:18:44 +0200

    nest `if`s

--- clippy_lints/src/functions/too_many_arguments.rs
+++ clippy_lints/src/functions/too_many_arguments.rs
@@ -18,16 +18,16 @@ pub(super) fn check_fn(
     too_many_arguments_threshold: u64,
 ) {
     // don't warn for implementations, it's not their fault
-    if !is_trait_impl_item(cx, hir_id) {
+    if !is_trait_impl_item(cx, hir_id)
         // don't lint extern functions decls, it's not their fault either
-        if kind.header().is_some_and(|header| header.abi == ExternAbi::Rust) {
-            check_arg_number(
-                cx,
-                decl,
-                span.with_hi(decl.output.span().hi()),
-                too_many_arguments_threshold,
-            );
-        }
+        && kind.header().is_some_and(|header| header.abi == ExternAbi::Rust)
+    {
+        check_arg_number(
+            cx,
+            decl,
+            span.with_hi(decl.output.span().hi()),
+            too_many_arguments_threshold,
+        );
     }
 }
 
