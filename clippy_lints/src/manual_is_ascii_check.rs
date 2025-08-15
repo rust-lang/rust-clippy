94b4bcdce0615cf866af2fff385628674719c655
Ada Alakbarova <ada.alakbarova@proton.me>
2025-08-15 11:51:04 +0200

    extract common code out of the `if-else`

--- clippy_lints/src/manual_is_ascii_check.rs
+++ clippy_lints/src/manual_is_ascii_check.rs
@@ -97,13 +97,12 @@ impl<'tcx> LateLintPass<'tcx> for ManualIsAsciiCheck {
             return;
         }
 
-        if let Some(macro_call) = matching_root_macro_call(cx, expr.span, sym::matches_macro)
+        let (arg, span, range) = if let Some(macro_call) = matching_root_macro_call(cx, expr.span, sym::matches_macro)
             && let ExprKind::Match(recv, [arm, ..], _) = expr.kind
         {
             let recv = peel_ref_operators(cx, recv);
-            let ty_sugg = get_ty_sugg(cx, recv);
             let range = check_pat(&arm.pat.kind);
-            check_is_ascii(cx, macro_call.span, recv, &range, ty_sugg);
+            (recv, macro_call.span, range)
         } else if let ExprKind::MethodCall(path, receiver, [arg], ..) = expr.kind
             && path.ident.name == sym::contains
             && let Some(higher::Range {
@@ -114,10 +113,14 @@ impl<'tcx> LateLintPass<'tcx> for ManualIsAsciiCheck {
             && !matches!(cx.typeck_results().expr_ty(arg).peel_refs().kind(), ty::Param(_))
         {
             let arg = peel_ref_operators(cx, arg);
-            let ty_sugg = get_ty_sugg(cx, arg);
             let range = check_expr_range(start, end);
-            check_is_ascii(cx, expr.span, arg, &range, ty_sugg);
-        }
+            (arg, expr.span, range)
+        } else {
+            return;
+        };
+
+        let ty_sugg = get_ty_sugg(cx, arg);
+        check_is_ascii(cx, span, arg, &range, ty_sugg);
     }
 }
 
