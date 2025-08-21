616c420e8f054b812be34c86cf03b62cb040a817
Valdemar Erk <valdemar@erk.dev>
2025-08-21 08:18:41 +0200

    use let chains to get more in line with the general style

--- clippy_lints/src/rest_when_destructuring_struct.rs
+++ clippy_lints/src/rest_when_destructuring_struct.rs
@@ -47,60 +47,54 @@ declare_lint_pass!(RestWhenDestructuringStruct => [REST_WHEN_DESTRUCTURING_STRUC
 
 impl<'tcx> LateLintPass<'tcx> for RestWhenDestructuringStruct {
     fn check_pat(&mut self, cx: &rustc_lint::LateContext<'tcx>, pat: &'tcx rustc_hir::Pat<'tcx>) {
-        if pat.span.in_external_macro(cx.tcx.sess.source_map()) {
-            return;
-        }
+        if let rustc_hir::PatKind::Struct(path, fields, true) = pat.kind
+            && !pat.span.in_external_macro(cx.tcx.sess.source_map())
+            && !is_from_proc_macro(cx, pat)
+            && let qty = cx.typeck_results().qpath_res(&path, pat.hir_id)
+            && let ty = cx.typeck_results().pat_ty(pat)
+            && let ty::Adt(a, _) = ty.kind()
+        {
+            let vid = qty
+                .opt_def_id()
+                .map_or(VariantIdx::ZERO, |x| a.variant_index_with_id(x));
+            let mut rest_fields = a.variants()[vid]
+                .fields
+                .iter()
+                .map(|field| field.ident(cx.tcx))
+                .filter(|pf| !fields.iter().any(|x| x.ident == *pf))
+                .map(|x| format!("{x}: _"));
+            let fmt_fields = rest_fields.join(", ");
 
-        if is_from_proc_macro(cx, pat) {
-            return;
-        }
+            let sm = cx.sess().source_map();
 
-        if let rustc_hir::PatKind::Struct(path, fields, true) = pat.kind {
-            let qty = cx.typeck_results().qpath_res(&path, pat.hir_id);
-            let ty = cx.typeck_results().pat_ty(pat);
-            if let ty::Adt(a, _) = ty.kind() {
-                let vid = qty
-                    .opt_def_id()
-                    .map_or(VariantIdx::ZERO, |x| a.variant_index_with_id(x));
-                let mut rest_fields = a.variants()[vid]
-                    .fields
-                    .iter()
-                    .map(|field| field.ident(cx.tcx))
-                    .filter(|pf| !fields.iter().any(|x| x.ident == *pf))
-                    .map(|x| format!("{x}: _"));
-                let fmt_fields = rest_fields.join(", ");
+            // It is not possible to get the span of the et cetera symbol at HIR level
+            // so we have to get it in a bit of a roundabout way:
 
-                let sm = cx.sess().source_map();
+            // Find the end of the last field if any.
+            let last_field = fields.iter().last().map(|x| x.span.shrink_to_hi());
+            // If no last field take the whole pattern.
+            let last_field = last_field.unwrap_or(pat.span.shrink_to_lo());
+            // Create a new span starting and ending just before the first .
+            let before_dot = sm.span_extend_to_next_char(last_field, '.', true).shrink_to_hi();
+            // Extend the span to the end of the line
+            let rest_of_line = sm.span_extend_to_next_char(before_dot, '\n', true);
+            // Shrink the span so it only contains dots
+            let dotdot = sm.span_take_while(rest_of_line, |x| *x == '.');
 
-                // It is not possible to get the span of the et cetera symbol at HIR level
-                // so we have to get it in a bit of a roundabout way:
-
-                // Find the end of the last field if any.
-                let last_field = fields.iter().last().map(|x| x.span.shrink_to_hi());
-                // If no last field take the whole pattern.
-                let last_field = last_field.unwrap_or(pat.span.shrink_to_lo());
-                // Create a new span starting and ending just before the first .
-                let before_dot = sm.span_extend_to_next_char(last_field, '.', true).shrink_to_hi();
-                // Extend the span to the end of the line
-                let rest_of_line = sm.span_extend_to_next_char(before_dot, '\n', true);
-                // Shrink the span so it only contains dots
-                let dotdot = sm.span_take_while(rest_of_line, |x| *x == '.');
-
-                span_lint_and_then(
-                    cx,
-                    REST_WHEN_DESTRUCTURING_STRUCT,
-                    pat.span,
-                    "struct destructuring with rest (..)",
-                    |diag| {
-                        diag.span_suggestion_verbose(
-                            dotdot,
-                            "consider explicitly ignoring remaining fields with wildcard patterns (x: _)",
-                            fmt_fields,
-                            rustc_errors::Applicability::MachineApplicable,
-                        );
-                    },
-                );
-            }
+            span_lint_and_then(
+                cx,
+                REST_WHEN_DESTRUCTURING_STRUCT,
+                pat.span,
+                "struct destructuring with rest (..)",
+                |diag| {
+                    diag.span_suggestion_verbose(
+                        dotdot,
+                        "consider explicitly ignoring remaining fields with wildcard patterns (x: _)",
+                        fmt_fields,
+                        rustc_errors::Applicability::MachineApplicable,
+                    );
+                },
+            );
         }
     }
 }
