1fe66407b4d963c37fb77bf1445518be1b4cf81b
dbaranov34 <baranov34@gmail.com>
2025-09-09 14:24:12 +0400

    cleanup and clippy

--- clippy_lints/src/useless_pathbuf_conversion.rs
+++ clippy_lints/src/useless_pathbuf_conversion.rs
@@ -32,34 +32,32 @@ declare_lint_pass!(UselessPathbufConversion => [USELESS_PATHBUF_CONVERSION]);
 impl<'tcx> LateLintPass<'tcx> for UselessPathbufConversion {
     fn check_expr(&mut self, cx: &LateContext<'tcx>, expr: &'tcx Expr<'_>) {
         // Only care about &PathBuf::from(...)
-        if let ExprKind::AddrOf(_, _, inner) = &expr.kind {
-            if let ExprKind::Call(func, args) = &inner.kind {
-                if let ExprKind::Path(ref qpath) = func.kind {
-                    if let Some(def_id) = cx.qpath_res(qpath, func.hir_id).opt_def_id() {
-                        // check that the function is `from`
-                        if cx.tcx.item_name(def_id) != sym::from {
-                            return;
-                        }
+        if let ExprKind::AddrOf(_, _, inner) = &expr.kind
+            && let ExprKind::Call(func, args) = &inner.kind
+            && let ExprKind::Path(ref qpath) = func.kind
+            && let Some(def_id) = cx.qpath_res(qpath, func.hir_id).opt_def_id()
+        {
+            // check that the function is `from`
+            if cx.tcx.item_name(def_id) != sym::from {
+                return;
+            }
 
-                        // get the type of the function's return value
-                        let ty = cx.typeck_results().expr_ty(inner);
+            // get the type of the function's return value
+            let ty = cx.typeck_results().expr_ty(inner);
 
-                        if is_type_diagnostic_item(cx, ty, sym::PathBuf) {
-                            if let Some(arg) = args.get(0) {
-                                let sugg = format!("Path::new({})", snippet(cx, arg.span, ".."));
-                                span_lint_and_sugg(
-                                    cx,
-                                    USELESS_PATHBUF_CONVERSION,
-                                    expr.span,
-                                    "unnecessary `PathBuf::from` when a `&Path` is enough",
-                                    "consider using",
-                                    sugg,
-                                    rustc_errors::Applicability::MachineApplicable,
-                                );
-                            }
-                        }
-                    }
-                }
+            if is_type_diagnostic_item(cx, ty, sym::PathBuf)
+                && let Some(arg) = args.first()
+            {
+                let sugg = format!("Path::new({})", snippet(cx, arg.span, ".."));
+                span_lint_and_sugg(
+                    cx,
+                    USELESS_PATHBUF_CONVERSION,
+                    expr.span,
+                    "unnecessary `PathBuf::from` when a `&Path` is enough",
+                    "consider using",
+                    sugg,
+                    rustc_errors::Applicability::MachineApplicable,
+                );
             }
         }
     }
