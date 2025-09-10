69f2493d11fe08e78a142cea9cddd38fa9c2d09e
Ada Alakbarova <ada.alakbarova@proton.me>
2025-09-10 10:10:57 +0200

    clean-up a bit

--- clippy_lints/src/manual_assert.rs
+++ clippy_lints/src/manual_assert.rs
@@ -1,6 +1,8 @@
 use clippy_utils::diagnostics::span_lint_and_then;
 use clippy_utils::macros::{is_panic, root_macro_call};
-use clippy_utils::{higher, is_else_clause, is_parent_stmt, peel_blocks_with_stmt, span_extract_comment, sugg};
+use clippy_utils::source::snippet_opt;
+use clippy_utils::sugg::Sugg;
+use clippy_utils::{higher, is_else_clause, is_parent_stmt, peel_blocks_with_stmt, span_extract_comment};
 use rustc_errors::Applicability;
 use rustc_hir::{Expr, ExprKind};
 use rustc_lint::{LateContext, LateLintPass, LintContext};
@@ -41,8 +43,8 @@ impl<'tcx> LateLintPass<'tcx> for ManualAssert {
             && let then = peel_blocks_with_stmt(then)
             && let Some(macro_call) = root_macro_call(then.span)
             && is_panic(cx, macro_call.def_id)
-            && !cx.tcx.sess.source_map().is_multiline(cond.span)
-            && let Ok(panic_snippet) = cx.sess().source_map().span_to_snippet(macro_call.span)
+            && !cx.sess().source_map().is_multiline(cond.span)
+            && let Some(panic_snippet) = snippet_opt(cx.sess(), macro_call.span)
             && let Some(panic_snippet) = panic_snippet.strip_suffix(')')
             && let Some((_, format_args_snip)) = panic_snippet.split_once('(')
             // Don't change `else if foo { panic!(..) }` to `else { assert!(foo, ..) }` as it just
@@ -55,7 +57,7 @@ impl<'tcx> LateLintPass<'tcx> for ManualAssert {
             if !comments.is_empty() {
                 comments += "\n";
             }
-            let cond_sugg = !sugg::Sugg::hir_with_context(cx, cond, expr.span.ctxt(), "..", &mut applicability);
+            let cond_sugg = !Sugg::hir_with_context(cx, cond, expr.span.ctxt(), "..", &mut applicability);
             let semicolon = if is_parent_stmt(cx, expr.hir_id) { ";" } else { "" };
             let sugg = format!("assert!({cond_sugg}, {format_args_snip}){semicolon}");
             // we show to the user the suggestion without the comments, but when applying the fix, include the
