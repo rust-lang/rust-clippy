398e0ec068e791e94e36e382ee445c23a2761698
Ada Alakbarova <ada.alakbarova@proton.me>
2025-09-18 19:00:22 +0200

    rename `implicit_saturating_{add,sub}` and `inverted_saturating_sub`

--- tests/ui/manual_arithmetic_check.rs
+++ tests/ui/manual_arithmetic_check.rs
@@ -1,4 +1,4 @@
-#![warn(clippy::implicit_saturating_sub, clippy::inverted_saturating_sub)]
+#![warn(clippy::manual_saturating_sub, clippy::almost_saturating_sub)]
 #![allow(clippy::if_same_then_else)]
 
 fn main() {
@@ -7,16 +7,16 @@ fn main() {
     let c = 8u32;
 
     let result = if a > b { a - b } else { 0 };
-    //~^ implicit_saturating_sub
+    //~^ manual_saturating_sub
 
     let result = if b < a { a - b } else { 0 };
-    //~^ implicit_saturating_sub
+    //~^ manual_saturating_sub
 
     let result = if a < b { 0 } else { a - b };
-    //~^ implicit_saturating_sub
+    //~^ manual_saturating_sub
 
     let result = if b > a { 0 } else { a - b };
-    //~^ implicit_saturating_sub
+    //~^ manual_saturating_sub
 
     // Should not warn!
     let result = if a > b { a - b } else { a - c };
