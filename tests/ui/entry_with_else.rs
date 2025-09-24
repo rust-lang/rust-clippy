816cc5443099e779c6204e51ca135f2223cdae7b
Ada Alakbarova <ada.alakbarova@proton.me>
2025-09-24 22:18:35 +0200

    clean-up, add some comments

--- tests/ui/entry_with_else.rs
+++ tests/ui/entry_with_else.rs
@@ -1,4 +1,4 @@
-#![allow(unused, clippy::needless_pass_by_value, clippy::collapsible_if)]
+#![allow(clippy::needless_pass_by_value, clippy::collapsible_if)]
 #![warn(clippy::map_entry)]
 
 use std::collections::{BTreeMap, HashMap};
