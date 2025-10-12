b1b966735f5fe835dcc943fe70e3279c1d22693c
Ada Alakbarova <ada.alakbarova@proton.me>
2025-10-12 11:33:58 +0200

    misc: reduce indentation
    kept as a separate commit for a better diff

--- clippy_lints/src/operators/op_ref.rs
+++ clippy_lints/src/operators/op_ref.rs
@@ -37,126 +37,64 @@ pub(crate) fn check<'tcx>(
             (cx.tcx.lang_items().partial_ord_trait(), true)
         },
     };
-    if let Some(trait_id) = trait_id {
-        match (&left.kind, &right.kind) {
-            // do not suggest to dereference literals
-            (&ExprKind::Lit(..), _) | (_, &ExprKind::Lit(..)) => {},
-            // &foo == &bar
-            (&ExprKind::AddrOf(BorrowKind::Ref, _, l), &ExprKind::AddrOf(BorrowKind::Ref, _, r)) => {
-                let lty = cx.typeck_results().expr_ty(l);
-                let rty = cx.typeck_results().expr_ty(r);
-                let lcpy = is_copy(cx, lty);
-                let rcpy = is_copy(cx, rty);
-                if let Some((self_ty, other_ty)) = in_impl(cx, e, trait_id)
-                    && ((are_equal(cx, rty, self_ty) && are_equal(cx, lty, other_ty))
-                        || (are_equal(cx, rty, other_ty) && are_equal(cx, lty, self_ty)))
-                {
-                    return; // Don't lint
-                }
-                // either operator autorefs or both args are copyable
-                if (requires_ref || (lcpy && rcpy)) && implements_trait(cx, lty, trait_id, &[rty.into()]) {
-                    span_lint_and_then(
-                        cx,
-                        OP_REF,
-                        e.span,
-                        "needlessly taken reference of both operands",
-                        |diag| {
-                            let mut applicability = Applicability::MachineApplicable;
-                            let (lsnip, _) = snippet_with_context(cx, l.span, e.span.ctxt(), "...", &mut applicability);
-                            let (rsnip, _) = snippet_with_context(cx, r.span, e.span.ctxt(), "...", &mut applicability);
-                            diag.multipart_suggestion(
-                                "use the values directly",
-                                vec![(left.span, lsnip.to_string()), (right.span, rsnip.to_string())],
-                                applicability,
-                            );
-                        },
-                    );
-                } else if lcpy
-                    && !rcpy
-                    && implements_trait(cx, lty, trait_id, &[cx.typeck_results().expr_ty(right).into()])
-                {
-                    span_lint_and_then(
-                        cx,
-                        OP_REF,
-                        e.span,
-                        "needlessly taken reference of left operand",
-                        |diag| {
-                            let mut applicability = Applicability::MachineApplicable;
-                            let (lsnip, _) = snippet_with_context(cx, l.span, e.span.ctxt(), "...", &mut applicability);
-                            diag.span_suggestion(left.span, "use the left value directly", lsnip, applicability);
-                        },
-                    );
-                } else if !lcpy
-                    && rcpy
-                    && implements_trait(cx, cx.typeck_results().expr_ty(left), trait_id, &[rty.into()])
-                {
-                    span_lint_and_then(
-                        cx,
-                        OP_REF,
-                        e.span,
-                        "needlessly taken reference of right operand",
-                        |diag| {
-                            let mut applicability = Applicability::MachineApplicable;
-                            let (rsnip, _) = snippet_with_context(cx, r.span, e.span.ctxt(), "...", &mut applicability);
-                            diag.span_suggestion(
-                                right.span,
-                                "use the right value directly",
-                                rsnip,
-                                Applicability::MachineApplicable,
-                            );
-                        },
-                    );
-                }
-            },
-            // &foo == bar
-            (&ExprKind::AddrOf(BorrowKind::Ref, _, l), _) => {
-                let lty = cx.typeck_results().expr_ty(l);
-                if let Some((self_ty, other_ty)) = in_impl(cx, e, trait_id) {
-                    let rty = cx.typeck_results().expr_ty(right);
-                    if (are_equal(cx, rty, self_ty) && are_equal(cx, lty, other_ty))
-                        || (are_equal(cx, rty, other_ty) && are_equal(cx, lty, self_ty))
-                    {
-                        return; // Don't lint
-                    }
-                }
-                let lcpy = is_copy(cx, lty);
-                if (requires_ref || lcpy)
-                    && implements_trait(cx, lty, trait_id, &[cx.typeck_results().expr_ty(right).into()])
-                {
-                    span_lint_and_then(
-                        cx,
-                        OP_REF,
-                        e.span,
-                        "needlessly taken reference of left operand",
-                        |diag| {
-                            let mut applicability = Applicability::MachineApplicable;
-                            let (lsnip, _) = snippet_with_context(cx, l.span, e.span.ctxt(), "...", &mut applicability);
-                            diag.span_suggestion(
-                                left.span,
-                                "use the left value directly",
-                                lsnip,
-                                Applicability::MachineApplicable,
-                            );
-                        },
-                    );
-                }
-            },
-            // foo == &bar
-            (_, &ExprKind::AddrOf(BorrowKind::Ref, _, r)) => {
-                let rty = cx.typeck_results().expr_ty(r);
-                if let Some((self_ty, other_ty)) = in_impl(cx, e, trait_id) {
-                    let lty = cx.typeck_results().expr_ty(left);
-                    if (are_equal(cx, rty, self_ty) && are_equal(cx, lty, other_ty))
-                        || (are_equal(cx, rty, other_ty) && are_equal(cx, lty, self_ty))
-                    {
-                        return; // Don't lint
-                    }
-                }
-                let rcpy = is_copy(cx, rty);
-                if (requires_ref || rcpy)
-                    && implements_trait(cx, cx.typeck_results().expr_ty(left), trait_id, &[rty.into()])
-                {
-                    span_lint_and_then(cx, OP_REF, e.span, "taken reference of right operand", |diag| {
+    let Some(trait_id) = trait_id else {
+        return;
+    };
+
+    match (&left.kind, &right.kind) {
+        // do not suggest to dereference literals
+        (&ExprKind::Lit(..), _) | (_, &ExprKind::Lit(..)) => {},
+        // &foo == &bar
+        (&ExprKind::AddrOf(BorrowKind::Ref, _, l), &ExprKind::AddrOf(BorrowKind::Ref, _, r)) => {
+            let lty = cx.typeck_results().expr_ty(l);
+            let rty = cx.typeck_results().expr_ty(r);
+            let lcpy = is_copy(cx, lty);
+            let rcpy = is_copy(cx, rty);
+            if let Some((self_ty, other_ty)) = in_impl(cx, e, trait_id)
+                && ((are_equal(cx, rty, self_ty) && are_equal(cx, lty, other_ty))
+                    || (are_equal(cx, rty, other_ty) && are_equal(cx, lty, self_ty)))
+            {
+                return; // Don't lint
+            }
+            // either operator autorefs or both args are copyable
+            if (requires_ref || (lcpy && rcpy)) && implements_trait(cx, lty, trait_id, &[rty.into()]) {
+                span_lint_and_then(
+                    cx,
+                    OP_REF,
+                    e.span,
+                    "needlessly taken reference of both operands",
+                    |diag| {
+                        let mut applicability = Applicability::MachineApplicable;
+                        let (lsnip, _) = snippet_with_context(cx, l.span, e.span.ctxt(), "...", &mut applicability);
+                        let (rsnip, _) = snippet_with_context(cx, r.span, e.span.ctxt(), "...", &mut applicability);
+                        diag.multipart_suggestion(
+                            "use the values directly",
+                            vec![(left.span, lsnip.to_string()), (right.span, rsnip.to_string())],
+                            applicability,
+                        );
+                    },
+                );
+            } else if lcpy && !rcpy && implements_trait(cx, lty, trait_id, &[cx.typeck_results().expr_ty(right).into()])
+            {
+                span_lint_and_then(
+                    cx,
+                    OP_REF,
+                    e.span,
+                    "needlessly taken reference of left operand",
+                    |diag| {
+                        let mut applicability = Applicability::MachineApplicable;
+                        let (lsnip, _) = snippet_with_context(cx, l.span, e.span.ctxt(), "...", &mut applicability);
+                        diag.span_suggestion(left.span, "use the left value directly", lsnip, applicability);
+                    },
+                );
+            } else if !lcpy && rcpy && implements_trait(cx, cx.typeck_results().expr_ty(left), trait_id, &[rty.into()])
+            {
+                span_lint_and_then(
+                    cx,
+                    OP_REF,
+                    e.span,
+                    "needlessly taken reference of right operand",
+                    |diag| {
                         let mut applicability = Applicability::MachineApplicable;
                         let (rsnip, _) = snippet_with_context(cx, r.span, e.span.ctxt(), "...", &mut applicability);
                         diag.span_suggestion(
@@ -165,11 +103,71 @@ pub(crate) fn check<'tcx>(
                             rsnip,
                             Applicability::MachineApplicable,
                         );
-                    });
+                    },
+                );
+            }
+        },
+        // &foo == bar
+        (&ExprKind::AddrOf(BorrowKind::Ref, _, l), _) => {
+            let lty = cx.typeck_results().expr_ty(l);
+            if let Some((self_ty, other_ty)) = in_impl(cx, e, trait_id) {
+                let rty = cx.typeck_results().expr_ty(right);
+                if (are_equal(cx, rty, self_ty) && are_equal(cx, lty, other_ty))
+                    || (are_equal(cx, rty, other_ty) && are_equal(cx, lty, self_ty))
+                {
+                    return; // Don't lint
                 }
-            },
-            _ => {},
-        }
+            }
+            let lcpy = is_copy(cx, lty);
+            if (requires_ref || lcpy)
+                && implements_trait(cx, lty, trait_id, &[cx.typeck_results().expr_ty(right).into()])
+            {
+                span_lint_and_then(
+                    cx,
+                    OP_REF,
+                    e.span,
+                    "needlessly taken reference of left operand",
+                    |diag| {
+                        let mut applicability = Applicability::MachineApplicable;
+                        let (lsnip, _) = snippet_with_context(cx, l.span, e.span.ctxt(), "...", &mut applicability);
+                        diag.span_suggestion(
+                            left.span,
+                            "use the left value directly",
+                            lsnip,
+                            Applicability::MachineApplicable,
+                        );
+                    },
+                );
+            }
+        },
+        // foo == &bar
+        (_, &ExprKind::AddrOf(BorrowKind::Ref, _, r)) => {
+            let rty = cx.typeck_results().expr_ty(r);
+            if let Some((self_ty, other_ty)) = in_impl(cx, e, trait_id) {
+                let lty = cx.typeck_results().expr_ty(left);
+                if (are_equal(cx, rty, self_ty) && are_equal(cx, lty, other_ty))
+                    || (are_equal(cx, rty, other_ty) && are_equal(cx, lty, self_ty))
+                {
+                    return; // Don't lint
+                }
+            }
+            let rcpy = is_copy(cx, rty);
+            if (requires_ref || rcpy)
+                && implements_trait(cx, cx.typeck_results().expr_ty(left), trait_id, &[rty.into()])
+            {
+                span_lint_and_then(cx, OP_REF, e.span, "taken reference of right operand", |diag| {
+                    let mut applicability = Applicability::MachineApplicable;
+                    let (rsnip, _) = snippet_with_context(cx, r.span, e.span.ctxt(), "...", &mut applicability);
+                    diag.span_suggestion(
+                        right.span,
+                        "use the right value directly",
+                        rsnip,
+                        Applicability::MachineApplicable,
+                    );
+                });
+            }
+        },
+        _ => {},
     }
 }
 
