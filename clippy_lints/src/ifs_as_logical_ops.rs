8d877b39713ffef2ff32acd5072fa5ea216ad4c8
Barun Parruck <barun.parruck@gmail.com>
2025-10-12 20:07:37 +0100

    Comments

--- clippy_lints/src/ifs_as_logical_ops.rs
+++ clippy_lints/src/ifs_as_logical_ops.rs
@@ -1,5 +1,7 @@
 use clippy_utils::diagnostics::span_lint_and_sugg;
 use clippy_utils::source::{HasSession, SpanRangeExt, walk_span_to_context};
+use clippy_utils::sugg::Sugg;
+use clippy_utils::{is_else_clause, peel_blocks};
 use rustc_ast::LitKind;
 use rustc_errors::Applicability;
 use rustc_hir::{Expr, ExprKind};
@@ -44,15 +46,21 @@ impl<'tcx> LateLintPass<'tcx> for IfsAsLogicalOps {
     fn check_expr(&mut self, cx: &LateContext<'tcx>, e: &'tcx Expr<'tcx>) {
         if let ExprKind::If(cond, cond_inner, Some(els)) = e.kind
             && let ExprKind::Block(if_block, _label) = cond_inner.kind
+            // Make sure the if block is not an if-let block.
+            && let ExprKind::DropTemps(_) = cond.kind
             // Check if the if-block has only a return statement
             && if_block.stmts.is_empty()
             && let Some(if_expr) = if_block.expr
+            // And that there are no comments or empty expansions for this block.
+            && (if_block.span.lo()..if_expr.span.lo()).check_source_text(cx, |src| src.trim_end() == "{")
             // And that the else block consists of only the boolean 'false'.
             && let ExprKind::Block(else_block, _label) = els.kind
             && else_block.stmts.is_empty()
             && let Some(else_expr) = else_block.expr
             && let ExprKind::Lit(lit) = else_expr.kind
             && matches!(lit.node, LitKind::Bool(false))
+            // And that there are no comments or empty expansions for this block either.
+            && (else_block.span.lo()..else_expr.span.lo()).check_source_text(cx, |src| src.trim_end() == "{")
             // We do not emit this lint if the expression diverges.
             && !cx.typeck_results().expr_ty(if_expr).is_never()
             // Make sure that the expression is only in a single macro context
@@ -62,18 +70,29 @@ impl<'tcx> LateLintPass<'tcx> for IfsAsLogicalOps {
             && ctxt == else_expr.span.ctxt()
             && ctxt == lit.span.ctxt()
             && !ctxt.in_external_macro(cx.tcx.sess().source_map())
-            && let Some(lhs_snippet) = walk_span_to_context(cond.span, ctxt)
-                .and_then(|span| span.get_source_text(cx))
-            && let Some(rhs_snippet) = walk_span_to_context(if_expr.span, ctxt)
-                .and_then(|span| span.get_source_text(cx))
         {
+            // Do not lint if the statement is trivially a boolean.
+            if let ExprKind::Lit(lit_ptr) = peel_blocks(if_expr).kind
+                && let LitKind::Bool(_) = lit_ptr.node
+            {
+                return;
+            }
+            let mut sugg = Sugg::hir(cx, cond, "_");
+            let rhs_sugg = Sugg::hir(cx, if_expr, "_");
+
+            sugg = sugg.and(&rhs_sugg);
+
+            if is_else_clause(cx.tcx, e) {
+                sugg = sugg.blockify();
+            }
+
             span_lint_and_sugg(
                 cx,
                 IFS_AS_LOGICAL_OPS,
                 e.span,
                 "if expression that could be written as a logical and expression",
                 "try",
-                format!("{lhs_snippet} && {rhs_snippet}"),
+                sugg.to_string(),
                 if ctxt.is_root() {
                     Applicability::MachineApplicable
                 } else {
--- tests/ui/ifs_as_logical_ops.rs
+++ tests/ui/ifs_as_logical_ops.rs
@@ -1,5 +1,6 @@
 #![warn(clippy::ifs_as_logical_ops)]
 #![allow(clippy::needless_bool)]
+#![allow(clippy::match_like_matches_macro)]
 
 fn main() {
     // test code goes here
@@ -31,13 +32,64 @@ fn test_that_complex_expressions_do_not_produce_lint(b1: bool, b2: bool) -> bool
     }
 }
 
+fn test_example_with_comment_does_not_lint(chars: Vec<char>) -> bool {
+    if !chars.is_empty() {
+        // SAFETY: Always starts with ^[ and ends with m.
+        chars.len() > 5
+    } else {
+        false
+    }
+}
+
+fn test_example_with_debug_does_not_lint(b1: bool) -> bool {
+    if b1 {
+        dbg!("Something");
+        true
+    } else {
+        false
+    }
+}
+
+fn test_example_with_if_let_does_not_lint(b1: bool, b2: Option<i32>) -> bool {
+    if let Some(30) = b2 { true } else { false }
+}
+
+fn test_else_if_does_lint(b1: bool, b2: bool, b3: bool) -> bool {
+    if b1 {
+        // There is some expansion here.
+        let _ = 40;
+        false
+    } else if b2 {
+        b3
+    } else {
+        false
+    }
+    //~^^^^^ ifs_as_logical_ops
+}
+
+fn needless_bool_clash_does_not_lint(x: bool) -> bool {
+    if x { true } else { false }
+}
+
 macro_rules! always_return_true {
     () => {
         true
     };
 }
 
-fn test_that_macro_expressions_produce_lint_correctly(b1: bool) -> bool {
+fn needless_bool_macro_clash_does_not_lint(b1: bool) -> bool {
     if b1 { always_return_true!() } else { false }
-    //~^ ifs_as_logical_ops
+}
+
+macro_rules! nothing {
+    () => {};
+}
+
+fn test_empty_expansion_does_not_lint(b1: bool, b2: bool) -> bool {
+    if b1 {
+        nothing!();
+        b2
+    } else {
+        false
+    }
 }
