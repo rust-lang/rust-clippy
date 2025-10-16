991360f8f8ad4ce65e053dd73f75aac4b5e3c5c3
Samuel Tardieu <sam@rfc1149.net>
2025-10-16 23:04:09 +0200

    Support `f16` and `f128` in `cast_nan_to_int` lint

--- tests/ui/cast_nan_to_int.rs
+++ tests/ui/cast_nan_to_int.rs
@@ -1,5 +1,5 @@
-// FIXME(f16_f128): add tests when constants are available
-
+#![feature(f128)]
+#![feature(f16)]
 #![warn(clippy::cast_nan_to_int)]
 #![allow(clippy::eq_op)]
 
@@ -20,6 +20,12 @@ fn main() {
     //~^ cast_nan_to_int
 
     let _ = (f32::INFINITY / f32::NEG_INFINITY) as usize;
+    //~^ cast_nan_to_int
+
+    let _ = f16::NAN as usize;
+    //~^ cast_nan_to_int
+
+    let _ = f128::NAN as u128;
     //~^ cast_nan_to_int
 
     // those won't be linted:
