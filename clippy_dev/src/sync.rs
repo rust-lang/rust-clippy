874276935e1526b28edae8a14b9b32f753338c57
Philipp Krones <hello@philkrones.com>
2025-10-18 21:46:14 +0200

    fixup! Implement sync subcommand in clippy_dev

--- clippy_dev/src/sync.rs
+++ clippy_dev/src/sync.rs
@@ -6,10 +6,9 @@ use std::process::exit;
 
 use xshell::{Shell, cmd};
 
+const JOSH_FILTER: &str = ":rev(d9fb15c4b1ebe9e7dc419e07f53af681d7860cbe:prefix=src/tools/clippy):/src/tools/clippy:from(2a5c4ae5ed0566c19267dd319b904daf1e3dbaf9:prune=trivial-merge)";
 // const JOSH_FILTER: &str =
-// ":rev(d9fb15c4b1ebe9e7dc419e07f53af681d7860cbe:prefix=src/tools/clippy):/src/tools/clippy:
-// from(27e6879445d30bbd340680a8353f342e63596d97:prune=trivial-merge)";
-const JOSH_FILTER: &str = ":rev(d9fb15c4b1ebe9e7dc419e07f53af681d7860cbe:prefix=src/tools/clippy):/src/tools/clippy";
+// ":rev(d9fb15c4b1ebe9e7dc419e07f53af681d7860cbe:prefix=src/tools/clippy):/src/tools/clippy";
 const JOSH_PORT: &str = "42042";
 const TOOLCHAIN_TOML: &str = "rust-toolchain.toml";
 const UTILS_README: &str = "clippy_utils/README.md";
