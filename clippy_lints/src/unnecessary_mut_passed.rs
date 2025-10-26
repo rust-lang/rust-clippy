2eb959e62ad56c1f1c38a1e1b07c99abbc0262ba
Ada Alakbarova <ada.alakbarova@proton.me>
2025-10-26 18:14:43 +0100

    feat(unnecessary_mut_passed): support `&raw` references

--- clippy_lints/src/unnecessary_mut_passed.rs
+++ clippy_lints/src/unnecessary_mut_passed.rs
@@ -85,16 +85,30 @@ fn check_arguments<'tcx>(
         let parameters = type_definition.fn_sig(cx.tcx).skip_binder().inputs();
         for (argument, parameter) in iter::zip(arguments, parameters) {
             if let ty::Ref(_, _, Mutability::Not) | ty::RawPtr(_, Mutability::Not) = parameter.kind()
-                && let ExprKind::AddrOf(BorrowKind::Ref, Mutability::Mut, arg) = argument.kind
+                && let ExprKind::AddrOf(borrow_kind, Mutability::Mut, arg) = argument.kind
             {
-                let applicability = Applicability::MachineApplicable;
+                emit(cx, name, fn_kind, argument, borrow_kind, arg);
+            }
+        }
+    }
+}
 
-                let span_to_remove = {
-                    let span_until_arg = argument.span.until(arg.span);
-                    if let Some(Some(ref_pos)) = span_until_arg.with_source_text(cx, |src| {
+fn emit(cx: &LateContext<'_>, name: &str, fn_kind: &str, argument: &Expr<'_>, borrow_kind: BorrowKind, arg: &Expr<'_>) {
+    let applicability = Applicability::MachineApplicable;
+
+    span_lint_and_then(
+        cx,
+        UNNECESSARY_MUT_PASSED,
+        argument.span,
+        format!("the {fn_kind} `{name}` doesn't need a mutable reference"),
+        |diag| {
+            let span_until_arg = argument.span.until(arg.span);
+            match borrow_kind {
+                BorrowKind::Ref => {
+                    let span_to_remove = if let Some(Some(ref_pos)) = span_until_arg.with_source_text(cx, |src| {
                         src
-                            // we don't use `strip_prefix` here, because `argument` might be enclosed in parens, in
-                            // which case `&` is no longer the prefix
+                            // we don't use `strip_prefix` here, because `argument` might be enclosed in
+                            // parens, in which case `&` is no longer the prefix
                             .find('&')
                             // just a sanity check, in case some proc-macro messes up the spans
                             .filter(|ref_pos| src[*ref_pos..].contains("mut"))
@@ -103,19 +117,42 @@ fn check_arguments<'tcx>(
                         span_until_arg.split_at(lo).1
                     } else {
                         return;
-                    }
-                };
+                    };
+                    diag.span_suggestion_verbose(span_to_remove, "remove this `mut`", String::new(), applicability);
+                },
+                BorrowKind::Raw => {
+                    let span_to_remove =
+                        if let Some(Some(ref_pos)) = span_until_arg.with_source_text(cx, |src: &str| {
+                            // we don't use `strip_prefix` here, because `argument` might be enclosed in
+                            // parens, and there might be arbitrary whitespace between things
+                            let src_after_addr_raw = src.split_once('&')?.1.split_once("raw")?.1.trim_start();
 
-                span_lint_and_then(
-                    cx,
-                    UNNECESSARY_MUT_PASSED,
-                    argument.span,
-                    format!("the {fn_kind} `{name}` doesn't need a mutable reference"),
-                    |diag| {
-                        diag.span_suggestion_verbose(span_to_remove, "remove this `mut`", String::new(), applicability);
-                    },
-                );
+                            Some(src_after_addr_raw)
+                                // just a sanity check, in case some proc-macro messes up the spans
+                                .filter(|trimmed| trimmed.contains("mut"))
+                                .map(|trimmed| src.len() - trimmed.len())
+                        }) && let Ok(lo) = u32::try_from(ref_pos)
+                        {
+                            span_until_arg.split_at(lo).1
+                        } else {
+                            return;
+                        };
+                    diag.span_suggestion_verbose(
+                        span_to_remove,
+                        "make this a `const` ptr",
+                        // the span points at `&raw mut x`
+                        //                          ^^^^
+                        // so we append a space to our suggestion
+                        String::from("const "),
+                        applicability,
+                    );
+                },
+                BorrowKind::Pin => {
+                    // it's fine to only "check" this after we've emitted the lint -- if the
+                    // reference was an `&pin`, passing it into a function requiring a ptr wouldn't
+                    // have type-checked in the first place
+                },
             }
-        }
-    }
+        },
+    );
 }
