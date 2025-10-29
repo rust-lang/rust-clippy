ddc2f297ce65df8e227565de0fee311620cb05be
Alex Macleod <alex@macleod.io>
2025-10-29 14:56:48 +0000

    Remove the pre-expansion pass

--- clippy_lints/src/attrs/deprecated_cfg_attr.rs
+++ clippy_lints/src/attrs/deprecated_cfg_attr.rs
@@ -1,70 +1,66 @@
-use super::{Attribute, DEPRECATED_CFG_ATTR, DEPRECATED_CLIPPY_CFG_ATTR, unnecessary_clippy_cfg};
-use clippy_utils::diagnostics::span_lint_and_sugg;
+use super::{Attribute, DEPRECATED_CFG_ATTR, DEPRECATED_CLIPPY_CFG_ATTR};
+use clippy_utils::diagnostics::{span_lint_and_sugg, span_lint_and_then, span_lint_hir_and_then};
 use clippy_utils::msrvs::{self, MsrvStack};
 use clippy_utils::sym;
 use rustc_ast::AttrStyle;
 use rustc_errors::Applicability;
-use rustc_lint::EarlyContext;
+use rustc_hir::attrs::CfgEntry;
+use rustc_hir::def_id::{LOCAL_CRATE, LocalDefId};
+use rustc_lint::{EarlyContext, LateContext};
 
-pub(super) fn check(cx: &EarlyContext<'_>, attr: &Attribute, msrv: &MsrvStack) {
+pub(super) fn check_rustfmt(cx: &EarlyContext<'_>, attrs: &[Attribute], msrv: &MsrvStack) {
+    for attr in attrs {
+        check_rustfmt_attr(cx, attr, msrv);
+    }
+}
+
+fn check_rustfmt_attr(cx: &EarlyContext<'_>, attr: &Attribute, msrv: &MsrvStack) {
     // check cfg_attr
-    if attr.has_name(sym::cfg_attr)
+    if attr.has_name(sym::cfg_attr_trace)
         && let Some(items) = attr.meta_item_list()
         && items.len() == 2
         && let Some(feature_item) = items[0].meta_item()
-    {
         // check for `rustfmt`
-        if feature_item.has_name(sym::rustfmt)
-            && msrv.meets(msrvs::TOOL_ATTRIBUTES)
-            // check for `rustfmt_skip` and `rustfmt::skip`
-            && let Some(skip_item) = &items[1].meta_item()
-            && (skip_item.has_name(sym::rustfmt_skip)
-                || skip_item
-                    .path
-                    .segments
-                    .last()
-                    .expect("empty path in attribute")
-                    .ident
-                    .name
-                    == sym::skip)
-            // Only lint outer attributes, because custom inner attributes are unstable
-            // Tracking issue: https://github.com/rust-lang/rust/issues/54726
-            && attr.style == AttrStyle::Outer
-        {
-            span_lint_and_sugg(
-                cx,
-                DEPRECATED_CFG_ATTR,
-                attr.span,
-                "`cfg_attr` is deprecated for rustfmt and got replaced by tool attributes",
-                "use",
-                "#[rustfmt::skip]".to_string(),
-                Applicability::MachineApplicable,
-            );
-        } else {
-            check_deprecated_cfg_recursively(cx, feature_item);
-            if let Some(behind_cfg_attr) = items[1].meta_item() {
-                unnecessary_clippy_cfg::check(cx, feature_item, behind_cfg_attr, attr);
-            }
-        }
-    }
-}
-
-pub(super) fn check_clippy(cx: &EarlyContext<'_>, attr: &Attribute) {
-    if attr.has_name(sym::cfg)
-        && let Some(list) = attr.meta_item_list()
+        && feature_item.has_name(sym::rustfmt)
+        && msrv.meets(msrvs::TOOL_ATTRIBUTES)
+        // check for `rustfmt_skip` and `rustfmt::skip`
+        && let Some(skip_item) = &items[1].meta_item()
+        && (skip_item.has_name(sym::rustfmt_skip)
+            || skip_item
+                .path
+                .segments
+                .last()
+                .expect("empty path in attribute")
+                .ident
+                .name
+                == sym::skip)
+        // Only lint outer attributes, because custom inner attributes are unstable
+        // Tracking issue: https://github.com/rust-lang/rust/issues/54726
+        && attr.style == AttrStyle::Outer
     {
-        for item in list.iter().filter_map(|item| item.meta_item()) {
-            check_deprecated_cfg_recursively(cx, item);
-        }
+        span_lint_and_then(
+            cx,
+            DEPRECATED_CFG_ATTR,
+            attr.span,
+            "`cfg_attr` is deprecated for rustfmt",
+            |diag| {
+                diag.span_suggestion_verbose(
+                    attr.span,
+                    "use the `rustfmt::skip` tool attribute instead",
+                    "#[rustfmt::skip]",
+                    Applicability::MachineApplicable,
+                );
+            },
+        );
     }
 }
 
-fn check_deprecated_cfg_recursively(cx: &EarlyContext<'_>, attr: &rustc_ast::MetaItem) {
+pub(super) fn check(cx: &EarlyContext<'_>, attr: &rustc_ast::MetaItem) {
     if let Some(ident) = attr.ident() {
         if matches!(ident.name, sym::any | sym::all | sym::not) {
             let Some(list) = attr.meta_item_list() else { return };
             for item in list.iter().filter_map(|item| item.meta_item()) {
-                check_deprecated_cfg_recursively(cx, item);
+                check(cx, item);
             }
         } else {
             check_cargo_clippy_attr(cx, attr);
@@ -83,5 +79,42 @@ fn check_cargo_clippy_attr(cx: &EarlyContext<'_>, item: &rustc_ast::MetaItem) {
             "clippy".to_string(),
             Applicability::MachineApplicable,
         );
+    }
+}
+
+pub fn check_stripped(cx: &LateContext<'_>) {
+    for stripped in cx.tcx.stripped_cfg_items(LOCAL_CRATE) {
+        if let Some(parent_module) = stripped.parent_module.as_local() {
+            check_cfg_entry(cx, &stripped.cfg.0, parent_module);
+        }
+    }
+}
+
+fn check_cfg_entry(cx: &LateContext<'_>, cfg: &CfgEntry, parent_module: LocalDefId) {
+    match cfg {
+        &CfgEntry::NameValue {
+            name: sym::feature,
+            name_span: _,
+            value: Some((sym::cargo_clippy, _)),
+            span,
+        } => {
+            span_lint_hir_and_then(
+                cx,
+                DEPRECATED_CLIPPY_CFG_ATTR,
+                cx.tcx.local_def_id_to_hir_id(parent_module),
+                span,
+                "`feature = \"cargo-clippy\"` was replaced by `clippy`",
+                |diag| {
+                    diag.span_suggestion(span, "replace with", "clippy", Applicability::MachineApplicable);
+                },
+            );
+        },
+        CfgEntry::All(children, _) | CfgEntry::Any(children, _) => {
+            for child in children {
+                check_cfg_entry(cx, child, parent_module);
+            }
+        },
+        CfgEntry::Not(child, _) => check_cfg_entry(cx, child, parent_module),
+        _ => {},
     }
 }
--- tests/ui/non_minimal_cfg.rs
+++ tests/ui/non_minimal_cfg.rs
@@ -1,19 +1,49 @@
-#![allow(unused)]
+//@compile-flags: --cfg foo --cfg bar
 
-#[cfg(all(windows))]
+#[cfg(all(foo))]
 //~^ non_minimal_cfg
-fn hermit() {}
+fn all() {}
 
-#[cfg(any(windows))]
+#[cfg(any(foo))]
 //~^ non_minimal_cfg
-fn wasi() {}
-
-#[cfg(all(any(unix), all(not(windows))))]
-//~^ non_minimal_cfg
-//~| non_minimal_cfg
-fn the_end() {}
-
-#[cfg(any())]
 fn any() {}
 
-fn main() {}
+#[cfg(any(any(foo), all(not(bar))))]
+//~^ non_minimal_cfg
+//~| non_minimal_cfg
+fn mixed() {}
+
+#[cfg(all())]
+//~^ non_minimal_cfg
+fn empty_all() {}
+
+#[cfg(any(true, any()))]
+//~^ non_minimal_cfg
+fn empty_any() {}
+
+#[cfg_attr(all(foo), doc = "all")]
+//~^ non_minimal_cfg
+#[cfg_attr(any(any(foo), all(not(bar))), doc = "mixed")]
+//~^ non_minimal_cfg
+//~| non_minimal_cfg
+#[cfg_attr(any(not_active), doc = "catches not active cfg_attrs")]
+//~^ non_minimal_cfg
+fn cfg_attr() {}
+
+macro_rules! in_macro {
+    ($($tt:tt)*) => {
+        #[cfg(any($($tt)*))]
+        struct FromMacro;
+    }
+}
+
+in_macro!(foo);
+
+#[clippy::msrv = "1.87"]
+#[cfg_attr(any(), inline)]
+fn msrv_1_87() {}
+
+#[clippy::msrv = "1.88"]
+#[cfg_attr(any(), inline)]
+//~^ non_minimal_cfg
+fn msrv_1_88() {}
