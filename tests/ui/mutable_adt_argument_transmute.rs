241501719caff704a41e5d63ad7314fd20147f3f
mendelsshop <mendelsshop@gmail.com>
2025-10-31 09:35:59 -0400

    more tests for mutable_adt_argument_transmute

--- tests/ui/mutable_adt_argument_transmute.rs
+++ tests/ui/mutable_adt_argument_transmute.rs
@@ -4,5 +4,15 @@ fn main() {
     unsafe {
         let _: Option<&mut i32> = std::mem::transmute(Some(&5i32));
         //~^ mutable_adt_argument_transmute
+        let _: Result<&mut i32, ()> = std::mem::transmute(Result::<&i32, ()>::Ok(&5i32));
+        //~^ mutable_adt_argument_transmute
+        let _: Result<Option<&mut String>, ()> =
+            std::mem::transmute(Result::<Option<&String>, ()>::Ok(Some(&"foo".to_string())));
+        //~^ mutable_adt_argument_transmute
+        let _: Result<&mut f32, &usize> = std::mem::transmute(Result::<&f32, &usize>::Ok(&2f32));
+        //~^ mutable_adt_argument_transmute
+        let _: Result<(), &mut usize> = std::mem::transmute(Result::<(), &usize>::Ok(()));
+        //~^ mutable_adt_argument_transmute
+        let _: Option<&i32> = std::mem::transmute(Some(&5i32));
     }
 }
