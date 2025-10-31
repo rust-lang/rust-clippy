bb52984b05becd354be12e69431d7a8d2e9e1cda
mendelsshop <mendelsshop@gmail.com>
2025-10-30 22:50:33 -0400

    adding test for mutable_adt_argument_transmute lint

--- tests/ui/mutable_adt_argument_transmute.rs
+++ tests/ui/mutable_adt_argument_transmute.rs
@@ -1,5 +1,8 @@
 #![warn(clippy::mutable_adt_argument_transmute)]
 
 fn main() {
-    // test code goes here
+    unsafe {
+        let _: Option<&mut i32> = std::mem::transmute(Some(&5i32));
+        //~^ mutable_adt_argument_transmute
+    }
 }
