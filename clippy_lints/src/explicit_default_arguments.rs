4ecd83b94cd8d5f9aa596f78000e3a795183bce0
Matheus L. P. <contact@matheuslp.com>
2025-11-15 23:24:42 -0600

    feat: lint structs & refactor more

--- clippy_lints/src/explicit_default_arguments.rs
+++ clippy_lints/src/explicit_default_arguments.rs
@@ -1,14 +1,11 @@
-use std::borrow::Cow;
-
 use clippy_utils::MaybePath;
-use clippy_utils::diagnostics::span_lint_and_sugg;
+use clippy_utils::diagnostics::{span_lint, span_lint_and_sugg, span_lint_hir};
 use clippy_utils::source::snippet;
 use rustc_data_structures::fx::FxHashMap;
 use rustc_hir::def::{DefKind, Res};
-use rustc_hir::{FnRetTy, GenericArg, ItemKind, QPath, TyKind};
-use rustc_hir_analysis::lower_ty;
+use rustc_hir::{self as hir, FnRetTy, ItemKind, Path, PathSegment, QPath, TyKind};
 use rustc_lint::{LateContext, LateLintPass};
-use rustc_middle::ty::{self, EarlyBinder, GenericParamDefKind};
+use rustc_middle::ty::{self, GenericArg, GenericParamDef, GenericParamDefKind, ParamTy, Ty, TyCtxt};
 use rustc_session::declare_lint_pass;
 
 declare_clippy_lint! {
@@ -40,92 +37,165 @@ declare_clippy_lint! {
 
 declare_lint_pass!(ExplicitDefaultArguments => [EXPLICIT_DEFAULT_ARGUMENTS]);
 
+// TODO: walk through types recursively, this is where the walking in lockstep thing comes in
+
+/// Map 1: Iterates through the aliased type's generic args and finds the defaults given by the type
+/// alias definition. Returns a map of the index in the aliased ty's generics args to its found
+/// default.
+///
+/// Map 2: Iterates through the aliased type's generics args and finds the index of it in the
+/// actual alias definition. Returns a map of the index in the aliased type's generics args to the
+/// corresponding index in the alias definition's generics params.
+fn match_generics<'tcx>(
+    cx: &LateContext<'tcx>,
+    aliased_ty_args: &[GenericArg<'tcx>],
+    alias_ty_params: &[GenericParamDef],
+) -> (FxHashMap<usize, Ty<'tcx>>, FxHashMap<usize, u32>) {
+    aliased_ty_args
+        .iter()
+        .enumerate()
+        .filter_map(|(i, generic_arg)| {
+            generic_arg
+                .as_type()
+                .map(|ty| {
+                    if let ty::Param(param) = ty.kind() {
+                        Some((i, param))
+                    } else {
+                        None
+                    }
+                })
+                .flatten()
+        })
+        .fold(
+            (FxHashMap::default(), FxHashMap::default()),
+            |(mut map1, mut map2), (i, param)| {
+                if let Some(
+                    alias_ty_param @ GenericParamDef {
+                        kind: GenericParamDefKind::Type { has_default: true, .. },
+                        ..
+                    },
+                ) = alias_ty_params.iter().find(|param_def| param_def.name == param.name)
+                {
+                    map1.insert(i, cx.tcx.type_of(alias_ty_param.def_id).skip_binder());
+                    map2.insert(i, alias_ty_param.index);
+                }
+                (map1, map2)
+            },
+        )
+}
+// NOTE: this whole algorithm avoids using `lower_ty
+fn check_alias_args<'tcx>(cx: &LateContext<'tcx>, resolved_ty: Ty<'tcx>, hir_ty: &hir::Ty<'tcx>) {
+    println!("resolved alias (ty::Ty): {resolved_ty}");
+    println!(
+        "instantiated alias (hir::Ty): {}",
+        snippet(&cx.tcx, hir_ty.span, "<error>")
+    );
+    let (alias_ty_params, aliased_ty_args, hir_ty_args) = {
+        let TyKind::Path(
+            qpath @ QPath::Resolved(
+                _,
+                Path {
+                    segments:
+                        [
+                            ..,
+                            PathSegment {
+                                args: Some(hir_ty_generics),
+                                ..
+                            },
+                        ],
+                    ..
+                },
+            ),
+        ) = hir_ty.kind
+        else {
+            // We aren't parsing a path or it doesn't have generics
+            return;
+        };
+        let Res::Def(DefKind::TyAlias, alias_def_id) = cx.qpath_res(&qpath, hir_ty.hir_id()) else {
+            // The ty doesn't refer to a type alias
+            return;
+        };
+        // TODO: fill-in generic args and stuff, maybe not here
+        let aliased_ty = cx.tcx.type_of(alias_def_id).skip_binder();
+        println!("aliased ty: {aliased_ty}");
+        let ty::Adt(_, aliased_ty_args) = aliased_ty.kind() else {
+            // The ty alias doesn't refer to an ADT
+            return;
+        };
+        (
+            &cx.tcx.generics_of(alias_def_id).own_params,
+            aliased_ty_args,
+            hir_ty_generics,
+        )
+    };
+    let ty::Adt(_, resolved_ty_args) = resolved_ty.kind() else {
+        return;
+    };
+    let (map1, map2) = match_generics(cx, aliased_ty_args.as_slice(), alias_ty_params.as_slice());
+
+    println!("map1: {map1:?}");
+    println!("map2: {map2:?}");
+
+    for (i, generic_arg) in resolved_ty_args.iter().enumerate() {
+        let is_arg_default = map1.get(&i).copied() == generic_arg.as_type();
+        // Was the default explicitly written, or was it there because just because it got resolved?
+        if let Some(ty) = generic_arg.as_type()
+            && let Some(default_arg_val) = map1.get(&i)
+            && let Some(j) = map2.get(&i)
+            // If something was specified and the resolved form of the type alias had the default,
+            // then it is redundant
+            && hir_ty_args.args.get(*j as usize).is_some()
+        {
+            println!(
+                "&ty ({ty}) == default_arg_val ({default_arg_val}) = {}",
+                &ty == default_arg_val
+            );
+            let redudant_ty = hir_ty_args.args.get(*j as usize).unwrap();
+            span_lint_hir(
+                &cx,
+                EXPLICIT_DEFAULT_ARGUMENTS,
+                redudant_ty.hir_id(),
+                redudant_ty.span(),
+                "redudant usage of default argument",
+            );
+            // println!(
+            //     "\tIt was there! `{}`",
+            //     snippet(&cx.tcx, hir_ty_args.args.get(*j as usize).unwrap().span(), "<error>")
+            // );
+        } else {
+            // println!(
+            //     "&ty ({ty}) == default_arg_val ({default_arg_val}) = {}",
+            //     &ty == default_arg_val
+            // );
+            println!("\tIt was **not** there.");
+        }
+    }
+
+    return;
+}
+
 #[allow(unused)]
 impl<'tcx> LateLintPass<'tcx> for ExplicitDefaultArguments {
     // Also check expressions for turbofish, casts, constructor params and type qualified paths
     fn check_item(&mut self, cx: &LateContext<'tcx>, item: &'tcx rustc_hir::Item<'tcx>) {
-        let _/*(generics, hir_ty, ty)*/  = match item.kind {
+        let tys_to_check: Vec<(Ty<'_>, &hir::Ty<'_>)> = match item.kind {
             // ItemKind::Static(_, _, ty, _) => ty,
             // ItemKind::Const(_, _, ty, _) => ty,
             ItemKind::Fn { sig, ident, .. } => {
-                println!("checking func `{ident}`");
+                println!("\nchecking func `{ident}`");
                 // TODO: check inputs too
-                let resolved_ty = {
-                    let poly_fn_sig = cx.tcx.fn_sig(item.owner_id).skip_binder();
-                    poly_fn_sig.output().skip_binder()
-                };
+                let poly_fn_sig = cx.tcx.fn_sig(item.owner_id).skip_binder();
 
+                let output_ty = poly_fn_sig.output().skip_binder();
                 // `None` = unit type
-                let FnRetTy::Return(instantiated_alias_hir) = sig.decl.output else {
+                let FnRetTy::Return(output_hir_ty) = sig.decl.output else {
                     return;
                 };
-                // TODO: check `TyKind::Path(qpath @ QPath::TypeRelative(_, path))` too
-                let (aliased_ty, alias_ty_generic_params)  = if let TyKind::Path(qpath @ QPath::Resolved(_, path)) = instantiated_alias_hir.kind {
-                    let Res::Def(DefKind::TyAlias, def_id) = cx.qpath_res(&qpath, instantiated_alias_hir.hir_id()) else {
-                        return;
-                    };
-                    // TODO: fill-in generic args and stuff
-                    (cx.tcx.type_of(def_id).skip_binder(), &cx.tcx.generics_of(def_id).own_params)
-                } else {
-                    return
-                };
-                let alias_ty_generic_defaults: Vec<_> =
-                    alias_ty_generic_params
-                        .iter()
-                        .filter_map(|param| matches!(param.kind, GenericParamDefKind::Type { has_default: true, .. }).then(|| cx.tcx.type_of(param.def_id)))
-                        .collect();
-                println!("resolved alias (ty::Ty): {resolved_ty}");
-                println!("instantiated alias (hir::Ty): {}", snippet(&cx.tcx, instantiated_alias_hir.span, "<error>"));
-                // Defaults to the alias type
-                println!("aliased ty: {aliased_ty}");
-                let ty::Adt(_, aliased_ty_generic_args) = aliased_ty.kind() else {
-                    return;
-                };
-                let ty::Adt(_, resolved_ty_generic_args) = resolved_ty.kind() else {
-                    return;
-                };
-
-                /// Index of a resolved ty generic param to its default
-                let mut map1 = FxHashMap::default();
-                /// Index of a resolved ty generic param to its index in the alias type generics
-                let mut map2 = FxHashMap::default();
-                for (i, generic_arg) in aliased_ty_generic_args.iter().enumerate() {
-                    if let Some(generic_arg_ty) = generic_arg.as_type()
-                        && let ty::Param(param) = generic_arg_ty.kind()
-                        && let Some(param_def) = alias_ty_generic_params.iter().find(|param_def| param_def.name == param.name)
-                        // Does it have a default defined in the type alias?
-                        && let GenericParamDefKind::Type { has_default: true, .. } = param_def.kind {
-                        // Was the default explicitly written, or was it there because just because it got resolved?
-                            map1.insert(i, cx.tcx.type_of(param_def.def_id).skip_binder());
-                            map2.insert(i, param_def.index);
-                    }
-                }
-                println!("map1: {map1:?}");
-                println!("map2: {map2:?}");
-
-                for (i, generic_arg) in resolved_ty_generic_args.iter().enumerate() {
-                    if let Some(ty) = generic_arg.as_type()
-                        && let Some(default_arg_val) = map1.get(&i)
-                        {
-                            println!("&ty ({ty}) == default_arg_val ({default_arg_val}) = {}", &ty == default_arg_val);
-                            if let TyKind::Path(QPath::Resolved(_, path)) = instantiated_alias_hir.kind
-                                && let Some(last_seg) = path.segments.last()
-                                && let Some(generic_args) = last_seg.args
-                                && let Some(i) = map2.get(&i)
-                                // If something was specified and the resolved form of the type alias had the default,
-                                // then it is redundant
-                                && generic_args.args.get(*i as usize).is_some() {
-                            println!("\tIt was there!");
-                            // generic_args.args.iter().position(|arg| arg)
-                        } else {
-                            println!("\tIt was **not** there.");
-                        }
-
-                    }
-                }
-                // println!("alias type generics: {:#?}", alias_ty_generic_params);
-                println!("alias type generic param defaults: {alias_ty_generic_defaults:?}");
-                println!();
+                let inputs_ty = poly_fn_sig.inputs().skip_binder();
+                let inputs_hir_tys = sig.decl.inputs;
+                let mut result = vec![(output_ty, output_hir_ty)];
+                result.extend(inputs_ty.iter().copied().zip(inputs_hir_tys.iter()).collect::<Vec<_>>());
+                result
             },
             // rustc_hir::ItemKind::Macro(ident, macro_def, macro_kinds) => todo!(),
             // rustc_hir::ItemKind::Mod(ident, _) => todo!(),
@@ -133,7 +203,12 @@ impl<'tcx> LateLintPass<'tcx> for ExplicitDefaultArguments {
             // rustc_hir::ItemKind::GlobalAsm { asm, fake_body } => todo!(),
             // rustc_hir::ItemKind::TyAlias(ident, generics, ty) => todo!(),
             // rustc_hir::ItemKind::Enum(ident, generics, enum_def) => todo!(),
-            // rustc_hir::ItemKind::Struct(ident, generics, variant_data) => todo!(),
+            ItemKind::Struct(_, _, variant_data) => variant_data
+                .fields()
+                .iter()
+                .map(|field| cx.tcx.type_of(field.def_id).skip_binder())
+                .zip(variant_data.fields().iter().map(|field| field.ty))
+                .collect(),
             // rustc_hir::ItemKind::Union(ident, generics, variant_data) => todo!(),
             // rustc_hir::ItemKind::Trait(constness, is_auto, safety, ident, generics, generic_bounds, trait_item_ids)
             // => todo!(), rustc_hir::ItemKind::TraitAlias(ident, generics, generic_bounds) => todo!(),
@@ -141,6 +216,9 @@ impl<'tcx> LateLintPass<'tcx> for ExplicitDefaultArguments {
             // rustc_hir::ItemKind::Impl(_) => todo!(),
             _ => return,
         };
+        for (resolved_ty, hir_ty) in tys_to_check {
+            check_alias_args(cx, resolved_ty, hir_ty);
+        }
     }
     // fn check_path(&mut self, cx: &LateContext<'tcx>, path: &rustc_hir::Path<'tcx>, _:
     // rustc_hir::HirId) {     println!("`{}` defaults:", snippet(cx, path.span, "<error>"));
