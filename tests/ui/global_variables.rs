b438103333bd68923b2ed91f35f34cca387e77e8
EFanZh <efanzh@gmail.com>
2025-11-20 08:41:17 +0800

    Add `global_variables` lint

---
+++ tests/ui/global_variables.rs
@@ -0,0 +1,50 @@
+#![warn(clippy::global_variables)]
+
+use std::sync::Mutex;
+use std::sync::atomic::AtomicU32;
+
+macro_rules! define_global_variables_with_macro {
+    () => {
+        static GLOBAL_VARIABLE_0: AtomicU32 = AtomicU32::new(2);
+        //~^ global_variables
+
+        static GLOBAL_VARIABLE_1: Mutex<u32> = Mutex::new(3);
+        //~^ global_variables
+    };
+}
+
+#[allow(clippy::missing_const_for_thread_local)]
+fn main() {
+    define_global_variables_with_macro!();
+
+    static GLOBAL_VARIABLE_2: AtomicU32 = AtomicU32::new(2);
+    //~^ global_variables
+
+    static GLOBAL_VARIABLE_3: Mutex<u32> = Mutex::new(3);
+    //~^ global_variables
+
+    static GLOBAL_VARIABLE_4: Option<AtomicU32> = Some(AtomicU32::new(0));
+    //~^ global_variables
+
+    static NOT_GLOBAL_VARIABLE_0: u32 = 1;
+
+    // ZST fields (like `PhantomData`) should be ignored.
+    static NOT_GLOBAL_VARIABLE_1: Vec<AtomicU32> = Vec::new();
+
+    // Having a initializer with a freeze value should be ignored.
+    static NOT_GLOBAL_VARIABLE_2: Option<AtomicU32> = None;
+
+    // Thread-local variables are ignored.
+    thread_local! {
+        static THREAD_LOCAL_VARIABLE_0: u32 = 0;
+        static THREAD_LOCAL_VARIABLE_1: AtomicU32 = AtomicU32::new(0);
+        static THREAD_LOCAL_VARIABLE_2: u32 = const { 0 };
+        static THREAD_LOCAL_VARIABLE_3: AtomicU32 = const { AtomicU32::new(0) };
+
+        static THREAD_LOCAL_VARIABLE_4: () = {
+            // Global variables inside a thread-local initializer are also considered.
+            static GLOBAL_VARIABLE_IN_THREAD_LOCAL: AtomicU32 = AtomicU32::new(0);
+            //~^ global_variables
+        };
+    }
+}
