3f9d0f28394a584c8a5277a8e1eb93fca92d9370
Mr-Leshiy <leshiy12345678@gmail.com>
2026-01-25 17:07:34 +0100

    process correctly more scenarios, update tests

--- tests/ui/drop_for_static.rs
+++ tests/ui/drop_for_static.rs
@@ -12,8 +12,25 @@ static A1: FooWithDrop = FooWithDrop;
 //~^ drop_for_static
 static A2: FooWithoutDrop = FooWithoutDrop;
 
-static A3: [FooWithDrop; 1] = [FooWithDrop];
+static A3: &FooWithDrop = &FooWithDrop;
 //~^ drop_for_static
-static A4: [FooWithoutDrop; 1] = [FooWithoutDrop];
+static A4: &FooWithoutDrop = &FooWithoutDrop;
+
+static A5: (FooWithoutDrop, FooWithDrop) = (FooWithoutDrop, FooWithDrop);
+//~^ drop_for_static
+static A6: (FooWithoutDrop, FooWithoutDrop) = (FooWithoutDrop, FooWithoutDrop);
+
+static A7: [FooWithDrop; 1] = [FooWithDrop];
+//~^ drop_for_static
+static A8: [FooWithoutDrop; 1] = [FooWithoutDrop];
+
+static A9: &[FooWithDrop] = &[FooWithDrop];
+//~^ drop_for_static
+static A10: &[FooWithoutDrop] = &[FooWithoutDrop];
+
+struct Nested<T>(T);
+static B9: Nested::<FooWithDrop> = Nested(FooWithDrop);
+static B10: Nested::<FooWithoutDrop> = Nested(FooWithoutDrop);
+
 
 fn main() {}
