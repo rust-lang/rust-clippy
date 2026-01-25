04a457767de854d5ebf1152d18cc256b8c0d2dc6
Mr-Leshiy <leshiy12345678@gmail.com>
2026-01-25 20:31:20 +0100

    wip

--- clippy_lints/src/drop_for_static.rs
+++ clippy_lints/src/drop_for_static.rs
@@ -1,6 +1,9 @@
 use clippy_utils::diagnostics::span_lint;
-use rustc_hir::{Item, ItemKind, MutTy, Ty, TyKind};
+use rustc_hir::def_id::DefId;
+use rustc_hir::intravisit::{Visitor, walk_path};
+use rustc_hir::{HirId, Item, ItemKind, MutTy, Path, Ty, TyKind};
 use rustc_lint::{LateContext, LateLintPass};
+use rustc_middle::hir::nested_filter;
 use rustc_session::declare_lint_pass;
 
 declare_clippy_lint! {
@@ -40,19 +43,19 @@ declare_clippy_lint! {
 declare_lint_pass!(DropForStatic => [DROP_FOR_STATIC]);
 
 impl LateLintPass<'_> for DropForStatic {
-    fn check_item(&mut self, cx: &LateContext<'_>, item: &Item<'_>) {
+    fn check_item<'a>(&mut self, cx: &LateContext<'a>, item: &Item<'a>) {
         if let Some(drop_trait_def_id) = cx.tcx.lang_items().drop_trait()
-            && let ItemKind::Static(_, _, Ty { kind, hir_id, .. }, _) = item.kind
+            && let ItemKind::Static(_, _, Ty { kind, hir_id, span }, _) = item.kind
         {
             let mut walk_kinds = vec![kind];
             while let Some(kind) = walk_kinds.pop() {
                 match kind {
-                    TyKind::Path(path) => {
-                        let def_id = cx.qpath_res(path, *hir_id).def_id();
-                        let ty = cx.tcx.type_of(def_id).instantiate_identity();
-                        cx.tcx.for_each_relevant_impl(drop_trait_def_id, ty, |_| {
+                    TyKind::Path(qpath) => {
+                        let mut visitor = PathVisitor::new(cx, drop_trait_def_id);
+                        visitor.visit_qpath(&qpath, *hir_id, *span);
+                        if visitor.drop_for_static_found {
                             span_lint(cx, DROP_FOR_STATIC, item.span, "static items with drop implementation");
-                        });
+                        }
                     },
                     TyKind::Array(ty, _) | TyKind::Slice(ty) | TyKind::Ref(_, MutTy { ty, .. }) => {
                         walk_kinds.push(&ty.kind);
@@ -62,5 +65,38 @@ impl LateLintPass<'_> for DropForStatic {
                 }
             }
         }
+    }
+}
+
+struct PathVisitor<'a, 'tcx> {
+    cx: &'a LateContext<'tcx>,
+    drop_trait_def_id: DefId,
+    drop_for_static_found: bool,
+}
+impl<'tcx> PathVisitor<'_, 'tcx> {
+    fn new<'a>(cx: &'a LateContext<'tcx>, drop_trait_def_id: DefId) -> PathVisitor<'a, 'tcx> {
+        PathVisitor {
+            cx,
+            drop_trait_def_id,
+            drop_for_static_found: false,
+        }
+    }
+}
+
+impl<'tcx> Visitor<'tcx> for PathVisitor<'_, 'tcx> {
+    type NestedFilter = nested_filter::All;
+
+    fn visit_path(&mut self, path: &Path<'tcx>, _: HirId) {
+        if let Some(def_id) = path.res.opt_def_id() {
+            let ty = self.cx.tcx.type_of(def_id).instantiate_identity();
+            self.cx.tcx.for_each_relevant_impl(self.drop_trait_def_id, ty, |_| {
+                self.drop_for_static_found = true;
+            });
+        }
+        walk_path(self, path);
+    }
+
+    fn maybe_tcx(&mut self) -> Self::MaybeTyCtxt {
+        self.cx.tcx
     }
 }
--- tests/ui/drop_for_static.rs
+++ tests/ui/drop_for_static.rs
@@ -28,8 +28,33 @@ static A9: &[FooWithDrop] = &[FooWithDrop];
 //~^ drop_for_static
 static A10: &[FooWithoutDrop] = &[FooWithoutDrop];
 
-struct Nested<T>(T);
-static B9: Nested<FooWithDrop> = Nested(FooWithDrop);
-static B10: Nested<FooWithoutDrop> = Nested(FooWithoutDrop);
+// ----------------------
+// nested types scenarios
+// ----------------------
+
+struct Nested<T1, T2 = ()>(T1, T2);
+static B1: Nested<FooWithDrop> = Nested(FooWithDrop, ());
+//~^ drop_for_static
+static B2: Nested<FooWithoutDrop> = Nested(FooWithoutDrop, ());
+
+static B3: Nested<FooWithoutDrop, Nested<FooWithDrop>> = Nested(FooWithoutDrop, Nested(FooWithDrop, ()));
+//~^ drop_for_static
+static B4: Nested<FooWithoutDrop, Nested<FooWithoutDrop>> = Nested(FooWithoutDrop, Nested(FooWithoutDrop, ()));
+
+static B5: Nested<&FooWithDrop> = Nested(&FooWithDrop, ());
+//~^ drop_for_static
+static B6: Nested<&FooWithoutDrop> = Nested(&FooWithoutDrop, ());
+
+static B7: Nested<(FooWithoutDrop, FooWithDrop)> = Nested((FooWithoutDrop, FooWithDrop), ());
+//~^ drop_for_static
+static B8: Nested<(FooWithoutDrop, FooWithoutDrop)> = Nested((FooWithoutDrop, FooWithoutDrop), ());
+
+static B9: Nested<[FooWithDrop; 1]> = Nested([FooWithDrop], ());
+//~^ drop_for_static
+static B10: Nested<[FooWithoutDrop; 1]> = Nested([FooWithoutDrop], ());
+
+static B11: Nested<&[FooWithDrop]> = Nested(&[FooWithDrop], ());
+//~^ drop_for_static
+static B12: Nested<&[FooWithoutDrop]> = Nested(&[FooWithoutDrop], ());
 
 fn main() {}
