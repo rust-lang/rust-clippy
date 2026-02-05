691e2263690537f825e217d9ae03b45ad2637a5c
Yuri Astrakhan <YuriAstrakhan@gmail.com>
2026-02-05 00:35:07 -0500

    fix: handle false negative for `str_to_string`
    Replace `ToString::to_string` with `ToOwned::to_owned` when the function is passed as is:
    
    ```rust
    fn issue16511(x: Option<&str>) -> String {
        // Replace with ToOwned::to_owned
        x.map(ToString::to_string)
    }
    ```

--- tests/ui/str_to_string.rs
+++ tests/ui/str_to_string.rs
@@ -22,3 +22,32 @@ fn issue16271(key: &[u8]) {
     let _value = t!(str::from_utf8(key)).to_string();
     //~^ str_to_string
 }
+
+struct GenericWrapper<T>(T);
+
+impl<T> GenericWrapper<T> {
+    fn mapper<U, F: FnOnce(T) -> U>(self, f: F) -> U {
+        f(self.0)
+    }
+}
+
+fn issue16511(x: Option<&str>) {
+    let _ = x.map(ToString::to_string);
+    //~^ str_to_string
+
+    let _ = x.map(str::to_string);
+    //~^ str_to_string
+
+    let _ = ["a", "b"].iter().map(ToString::to_string);
+    //~^ str_to_string
+
+    fn mapper<F: Fn(&str) -> String>(f: F) -> String {
+        f("hello")
+    }
+    let _ = mapper(ToString::to_string);
+    //~^ str_to_string
+
+    let w = GenericWrapper("hello");
+    let _ = w.mapper(ToString::to_string);
+    //~^ str_to_string
+}
