4f8b2be7d927c572ec11da33ab400b6a1a2a2115
BenjaminBrienen <Benjamin.Brienen@outlook.com>
2026-02-11 19:36:04 +0100

    add all separator whitespace

--- clippy_lints/src/doc/doc_paragraphs_missing_punctuation.rs
+++ clippy_lints/src/doc/doc_paragraphs_missing_punctuation.rs
@@ -136,7 +136,27 @@ fn trim_trailing_symbols(s: &str) -> &str {
             0x1FA70..=0x1FAFF | // Symbols and Pictographs Extended-A
             0x1F680..=0x1F6FF | // Transport and Map Symbols
             0x2600..=0x26FF | // Miscellaneous Symbols
-            0xFE00..=0xFE0F // Variation selectors
+            0xFE00..=0xFE0F | // Variation selectors
+            // Whitespace
+            0x0020 | // SPACE
+            0x00A0 | // NO-BREAK SPACE
+            0x1680 | // OGHAM SPACE MARK
+            0x2000 | // EN QUAD
+            0x2001 | // EM QUAD
+            0x2002 | // EN SPACE
+            0x2003 | // EM SPACE
+            0x2004 | // THREE-PER-EM SPACE
+            0x2005 | // FOUR-PER-EM SPACE
+            0x2006 | // SIX-PER-EM SPACE
+            0x2007 | // FIGURE SPACE
+            0x2008 | // PUNCTUATION SPACE
+            0x2009 | // THIN SPACE
+            0x200A | // HAIR SPACE
+            0x202F | // NARROW NO-BREAK SPACE
+            0x205F | // MEDIUM MATHEMATICAL SPACE
+            0x3000 | // IDEOGRAPHIC SPACE
+            0x2029 | // PARAGRAPH SEPARATOR
+            0x2028 // LINE SEPARATOR
         ))
 }
 
