0b87cf800b4f01308df08db1997a2b92ff121fe0
ceptontech <>
2026-02-25 10:52:36 -0800

    feat(manual_is_variant): Check for `.iter().any()`
    Now the linter checks for calling the `iter` or `into_iter` method
    followed by calling the `all` or `any` method.
    
    We have used these methods by mistake ourselves.  The developer was not
    familiar with the `is_some_and` method.  He tried to use the `any`
    method on an option.  The compiler suggested using `iter` first.  The
    developer did not get to learn about the idiomatic method.
    
    changelog: [`manual_is_variant`]: check for calling the `iter` or
    `into_iter` method followed by calling the `all` or `any` method

--- clippy_lints/src/methods/mod.rs
+++ clippy_lints/src/methods/mod.rs
@@ -3951,6 +3951,10 @@ declare_clippy_lint! {
     /// result.map(|a| a > 10) == Ok(true);
     /// option.map(|a| a > 10) != Some(true);
     /// result.map(|a| a > 10) != Ok(true);
+    ///
+    /// option.into_iter().any(|a| a > 10);
+    /// result.into_iter().any(|a| a > 10);
+    /// option.into_iter().all(|a| a > 10);
     /// ```
     /// Use instead:
     /// ```no_run
@@ -3963,6 +3967,10 @@ declare_clippy_lint! {
     /// result.is_ok_and(|a| a > 10);
     /// option.is_none_or(|a| a > 10);
     /// !result.is_ok_and(|a| a > 10);
+    ///
+    /// option.is_some_and(|a| a > 10);
+    /// result.is_ok_and(|a| a > 10);
+    /// option.is_none_or(|a| a > 10);
     /// ```
     #[clippy::version = "1.77.0"]
     pub MANUAL_IS_VARIANT_AND,
@@ -5078,6 +5086,7 @@ impl Methods {
                     zst_offset::check(cx, expr, recv);
                 },
                 (sym::all, [arg]) => {
+                    manual_is_variant_and::check_iter_reduce(cx, expr, recv, arg, span, self.msrv, name);
                     needless_character_iteration::check(cx, expr, recv, arg, true);
                     match method_call(recv) {
                         Some((sym::cloned, recv2, [], _, _)) => {
@@ -5107,6 +5116,7 @@ impl Methods {
                     }
                 },
                 (sym::any, [arg]) => {
+                    manual_is_variant_and::check_iter_reduce(cx, expr, recv, arg, span, self.msrv, name);
                     needless_character_iteration::check(cx, expr, recv, arg, false);
                     match method_call(recv) {
                         Some((sym::cloned, recv2, [], _, _)) => iter_overeager_cloned::check(
