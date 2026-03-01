34456d2d4458385d1de83679e451f2078b9aa189
Samuel Onoja <samiodev@icloud.com>
2026-03-01 21:29:25 +0100

    use HirId for break/continue targeting, add label support and autofix

--- clippy_lints/src/loops/while_let_loop.rs
+++ clippy_lints/src/loops/while_let_loop.rs
@@ -1,13 +1,14 @@
 use super::WHILE_LET_LOOP;
-use clippy_utils::diagnostics::span_lint_and_sugg;
-use clippy_utils::source::{snippet, snippet_indent, snippet_opt, snippet_with_context};
+use clippy_utils::diagnostics::span_lint_and_then;
+use clippy_utils::source::{reindent_multiline, snippet, snippet_indent, snippet_opt, snippet_with_context};
 use clippy_utils::ty::needs_ordered_drop;
 use clippy_utils::visitors::{any_temporaries_need_ordered_drop, for_each_expr_without_closures};
 use clippy_utils::{higher, peel_blocks};
 use rustc_ast::BindingMode;
 use rustc_errors::Applicability;
 use rustc_hir::{
-    Block, Destination, Expr, ExprKind, LetStmt, MatchSource, Pat, PatKind, Path, QPath, Stmt, StmtKind, Ty,
+    Block, Destination, Expr, ExprKind, HirId, LetStmt, LoopSource, MatchSource, Pat, PatKind, Path, QPath, Stmt,
+    StmtKind, Ty,
 };
 use rustc_lint::LateContext;
 use std::fmt::Write;
@@ -42,7 +43,7 @@ pub(super) fn check<'tcx>(cx: &LateContext<'tcx>, expr: &'tcx Expr<'_>, loop_blo
         (pat, init, None, None)
     } else if let Some(els_block) = els
         && let Some((pat, _)) = let_info
-        && let Some(hoistable) = extract_hoistable_stmts(els_block)
+        && let Some(hoistable) = extract_hoistable_stmts(els_block, expr.hir_id)
     {
         (pat, init, None, Some(hoistable))
     } else if let ExprKind::Match(scrutinee, [arm1, arm2], MatchSource::Normal) = init.kind
@@ -55,7 +56,14 @@ pub(super) fn check<'tcx>(cx: &LateContext<'tcx>, expr: &'tcx Expr<'_>, loop_blo
         return;
     };
 
-    could_be_while_let(cx, expr, has_trailing_exprs, let_info, let_pat, let_expr, inner_expr, hoistable_stmts);
+    if (has_trailing_exprs || hoistable_stmts.is_some())
+        && (needs_ordered_drop(cx, cx.typeck_results().expr_ty(let_expr))
+            || any_temporaries_need_ordered_drop(cx, let_expr))
+    {
+        return;
+    }
+
+    could_be_while_let(cx, expr, loop_block, let_info, let_pat, let_expr, inner_expr, hoistable_stmts);
 }
 
 /// Checks if `block` contains a single unlabeled `break` expression or statement, possibly embedded
@@ -82,22 +90,20 @@ fn is_simple_break_expr(expr: &Expr<'_>) -> bool {
 fn could_be_while_let<'tcx>(
     cx: &LateContext<'tcx>,
     expr: &'tcx Expr<'_>,
-    has_trailing_exprs: bool,
+    loop_block: &'tcx Block<'_>,
     let_info: Option<(&'tcx Pat<'tcx>, Option<&'tcx Ty<'tcx>>)>,
     let_pat: &'tcx Pat<'tcx>,
     let_expr: &'tcx Expr<'tcx>,
     inner_expr: Option<&'tcx Expr<'tcx>>,
     hoistable_stmts: Option<&'tcx [Stmt<'tcx>]>,
 ) {
-    if has_trailing_exprs
-        && (needs_ordered_drop(cx, cx.typeck_results().expr_ty(let_expr))
-            || any_temporaries_need_ordered_drop(cx, let_expr))
-    {
-        // Switching to a `while let` loop will extend the lifetime of some values.
-        return;
-    }
-
     let indent = snippet_indent(cx, expr.span).unwrap_or_default();
+
+    let label_prefix = if let ExprKind::Loop(_, Some(label), LoopSource::Loop, _) = expr.kind {
+        format!("{}: ", label.ident)
+    } else {
+        String::new()
+    };
 
     // NOTE: we used to build a body here instead of using
     // ellipsis, this was removed because:
@@ -119,7 +125,6 @@ fn could_be_while_let<'tcx>(
         " .. ".into()
     };
 
-    // Build the hoisted statements string in case we have statements to hoist
     let hoisted_content = if let Some(stmts) = hoistable_stmts {
         let mut hoisted = String::new();
         let outer_ctxt = expr.span.ctxt();
@@ -130,25 +135,47 @@ fn could_be_while_let<'tcx>(
             } else {
                 ""
             };
-            let _ = write!(hoisted, "\n{indent}{stmt_str}{semi}");
+            let reindented = reindent_multiline(&stmt_str, true, Some(indent.len()));
+            let _ = write!(hoisted, "\n{indent}{reindented}{semi}");
         }
         hoisted
     } else {
         String::new()
     };
 
-    span_lint_and_sugg(
+    let human_suggestion = format!(
+        "{label_prefix}while let {} = {} {{{inner_content}}}{hoisted_content}",
+        snippet(cx, let_pat.span, ".."),
+        snippet(cx, let_expr.span, ".."),
+    );
+
+    span_lint_and_then(
         cx,
         WHILE_LET_LOOP,
         expr.span,
         "this loop could be written as a `while let` loop",
-        "try",
-        format!(
-            "while let {} = {} {{{inner_content}}}{hoisted_content}",
-            snippet(cx, let_pat.span, ".."),
-            snippet(cx, let_expr.span, ".."),
-        ),
-        Applicability::HasPlaceholders,
+        |diag| {
+            diag.span_suggestion(expr.span, "try", &human_suggestion, Applicability::HasPlaceholders);
+
+            if inner_expr.is_none() {
+                let while_let_header = format!(
+                    "{label_prefix}while let {} = {} {{",
+                    snippet(cx, let_pat.span, ".."),
+                    snippet(cx, let_expr.span, ".."),
+                );
+
+                let first_stmt_span = loop_block.stmts[0].span;
+                let replace_span = expr.span.with_hi(first_stmt_span.hi());
+
+                let mut parts = vec![(replace_span, while_let_header)];
+
+                if !hoisted_content.is_empty() {
+                    parts.push((expr.span.shrink_to_hi(), hoisted_content.clone()));
+                }
+
+                diag.tool_only_multipart_suggestion("try", parts, Applicability::MachineApplicable);
+            }
+        },
     );
 }
 
@@ -163,10 +190,10 @@ fn is_trivial_assignment(pat: &Pat<'_>, init: &Expr<'_>) -> bool {
     }
 }
 
-/// Checks if a block ends with an unlabeled `break` and returns the statements before it..
-/// or `None` if the block doesn't end with a simple break or if any statement before
-/// the break could exit the loop (via return, labeled break, etc.).
-fn extract_hoistable_stmts<'tcx>(block: &'tcx Block<'tcx>) -> Option<&'tcx [Stmt<'tcx>]> {
+/// Checks if a block ends with an unlabeled `break` and returns the statements before it,
+/// or `None` if any statement before the break contains a `break` or `continue` targeting
+/// the loop identified by `loop_id`.
+fn extract_hoistable_stmts<'tcx>(block: &'tcx Block<'tcx>, loop_id: HirId) -> Option<&'tcx [Stmt<'tcx>]> {
     let stmts_before_break = match (block.stmts, block.expr) {
         (stmts, Some(e)) if is_simple_break_expr(e) => stmts,
         (stmts, None) if !stmts.is_empty() => {
@@ -183,17 +210,16 @@ fn extract_hoistable_stmts<'tcx>(block: &'tcx Block<'tcx>) -> Option<&'tcx [Stmt
         return None;
     }
 
-    // Check that none of the statements before break contain return, labeled break,
-    // or other control flow that could exit the loop differently
-    let has_early_exit = stmts_before_break.iter().any(|stmt| {
+    // Reject statements containing a `break`/`continue` targeting the loop
+    // being transformed. Breaks/continues to other loops and returns are fine to hoist.
+    let has_problematic_control_flow = stmts_before_break.iter().any(|stmt| {
         for_each_expr_without_closures(stmt, |e| match e.kind {
-            ExprKind::Ret(..)
-            | ExprKind::Break(Destination { label: Some(_), .. }, _)
-            | ExprKind::Continue(Destination { label: Some(_), .. }) => ControlFlow::Break(()),
+            ExprKind::Break(Destination { target_id: Ok(id), .. }, _)
+            | ExprKind::Continue(Destination { target_id: Ok(id), .. }) if id == loop_id => ControlFlow::Break(()),
             _ => ControlFlow::Continue(()),
         })
         .is_some()
     });
 
-    (!has_early_exit).then_some(stmts_before_break)
+    (!has_problematic_control_flow).then_some(stmts_before_break)
 }
