33078b7c66649a494608359eeac97f9933c752de
moses7054 <moses7054@gmail.com>
2026-03-06 17:10:51 +0530

    lint to all function forms with autofix

--- clippy_lints/src/inline_trait_bounds.rs
+++ clippy_lints/src/inline_trait_bounds.rs
@@ -1,7 +1,12 @@
-use clippy_utils::diagnostics::span_lint_and_help;
-use rustc_ast::ast::{GenericParam, GenericParamKind};
+use clippy_utils::diagnostics::span_lint_and_then;
+use clippy_utils::source::{HasSession, snippet};
+use rustc_ast::NodeId;
+use rustc_ast::ast::{Fn, FnRetTy, GenericParam, GenericParamKind};
+use rustc_ast::visit::{FnCtxt, FnKind};
+use rustc_errors::Applicability;
 use rustc_lint::{EarlyContext, EarlyLintPass};
 use rustc_session::declare_lint_pass;
+use rustc_span::Span;
 
 declare_clippy_lint! {
     /// ### What it does
@@ -27,21 +32,98 @@ declare_clippy_lint! {
     restriction,
     "enforce that `where` bounds are used for all trait bounds"
 }
+
 declare_lint_pass!(InlineTraitBounds => [INLINE_TRAIT_BOUNDS]);
 
 impl EarlyLintPass for InlineTraitBounds {
-    fn check_generic_param(&mut self, cx: &EarlyContext<'_>, param: &GenericParam) {
-        if let GenericParamKind::Type { .. } = param.kind
-            && !param.bounds.is_empty()
-        {
-            span_lint_and_help(
-                cx,
-                INLINE_TRAIT_BOUNDS,
-                param.span(),
-                "inline trait bounds",
-                None,
-                "consider a `where` bound instead",
-            );
+    fn check_fn(&mut self, cx: &EarlyContext<'_>, kind: FnKind<'_>, _: Span, _: NodeId) {
+        let FnKind::Fn(ctxt, _vis, f) = kind else {
+            return;
+        };
+
+        // Skip foreign functions (extern "C" etc.)
+        if !matches!(ctxt, FnCtxt::Free | FnCtxt::Assoc(..)) {
+            return;
+        }
+
+        if f.sig.span.in_external_macro(cx.sess().source_map()) {
+            return;
+        }
+
+        lint_fn(cx, f);
+    }
+}
+
+fn lint_fn(cx: &EarlyContext<'_>, f: &Fn) {
+    let generics = &f.generics;
+    let offenders: Vec<&GenericParam> = generics
+        .params
+        .iter()
+        .filter(|param| {
+            !param.bounds.is_empty() && matches!(param.kind, GenericParamKind::Lifetime | GenericParamKind::Type { .. })
+        })
+        .collect();
+    if offenders.is_empty() {
+        return;
+    }
+
+    let predicates = offenders
+        .iter()
+        .map(|param| build_predicate_text(cx, param))
+        .collect::<Vec<_>>();
+
+    let mut edits = Vec::new();
+
+    for param in offenders {
+        if let Some(colon) = param.colon_span {
+            let remove_span = colon.to(param.bounds.last().unwrap().span());
+
+            edits.push((remove_span, String::new()));
         }
     }
+
+    let predicate_text = predicates.join(", ");
+
+    let where_clause = &generics.where_clause;
+    if where_clause.has_where_token {
+        let (insert_at, suffix) = if let Some(last_pred) = where_clause.predicates.last() {
+            // existing `where` with predicates: append after last predicate
+            (last_pred.span.shrink_to_hi(), format!(", {predicate_text}"))
+        } else {
+            // `where` token present but empty predicate list
+            (where_clause.span.shrink_to_hi(), format!(" {predicate_text}"))
+        };
+
+        edits.push((insert_at, suffix));
+    } else {
+        let insert_at = match &f.sig.decl.output {
+            FnRetTy::Default(span) => span.shrink_to_lo(),
+            FnRetTy::Ty(ty) => ty.span.shrink_to_hi(),
+        };
+        edits.push((insert_at, format!(" where {predicate_text}")));
+    }
+
+    span_lint_and_then(
+        cx,
+        INLINE_TRAIT_BOUNDS,
+        generics.span,
+        "inline trait bounds used",
+        |diag| {
+            diag.multipart_suggestion("move bounds to a `where` clause", edits, Applicability::MaybeIncorrect);
+        },
+    );
+}
+
+fn build_predicate_text(cx: &EarlyContext<'_>, param: &GenericParam) -> String {
+    // bounds is guaranteed non-empty by the filter in `lint_fn`
+    let first = param.bounds.first().unwrap();
+    let last = param.bounds.last().unwrap();
+
+    let bounds_span = first.span().to(last.span());
+
+    let lhs = snippet(cx, param.ident.span, "..");
+
+    let rhs = snippet(cx, bounds_span, "..");
+
+    format!("{lhs}: {rhs}")
 }
