926bc917c33c324d9c522c7e1e2f82371b14e7d0
Charlie Chang <s94040511s@gmail.com>
2026-03-07 23:10:11 +0800

    Add `undocumented_as_casts` lint to enforce comments on `as` casts

--- clippy_lints/src/lib.rs
+++ clippy_lints/src/lib.rs
@@ -371,6 +371,7 @@ mod transmute;
 mod tuple_array_conversions;
 mod types;
 mod unconditional_recursion;
+mod undocumented_as_casts;
 mod undocumented_unsafe_blocks;
 mod unicode;
 mod uninhabited_references;
@@ -870,6 +871,7 @@ rustc_lint::late_lint_methods!(
         BlockScrutinee: block_scrutinee::BlockScrutinee = block_scrutinee::BlockScrutinee,
         NonnullUncheckedOnBoxPtr: nonnull_unchecked_on_box_ptr::NonnullUncheckedOnBoxPtr = nonnull_unchecked_on_box_ptr::NonnullUncheckedOnBoxPtr::new(conf),
         NeedlessNonzeroGet: needless_nonzero_get::NeedlessNonzeroGet = needless_nonzero_get::NeedlessNonzeroGet::new(conf),
+        UndocumentedAsCasts: undocumented_as_casts::UndocumentedAsCasts = undocumented_as_casts::UndocumentedAsCasts,
         // add late passes here, used by `cargo dev new_lint`
     ]]
 );
