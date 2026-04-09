7af5f686b9b0113ca5cf9f6f14ada4ea03e09495
Shashank <109198739+shashu8660@users.noreply.github.com>
2026-04-09 19:56:59 +0530

    fixed short span in the match same arm

---
+++ tests/ui/span_in_match_same_arm.rs
@@ -0,0 +1,12 @@
+#![warn(clippy::match_same_arms)]
+
+fn main() {
+    let x = 2;
+
+    match x {
+        1 => println!("same"),
+        2 => println!("different"),
+        3 => println!("same"),//~ ERROR: these match arms have identical bodies
+        _ => println!("other"),
+    }
+}
