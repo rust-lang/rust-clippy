6b1bf3e0ef064ba75c42d3359a81226afbbd5161
Shashank <109198739+shashu8660@users.noreply.github.com>
2026-04-09 20:04:06 +0530

    case 2 check for more then 2 arm with same body

--- tests/ui/span_in_match_same_arm.rs
+++ tests/ui/span_in_match_same_arm.rs
@@ -6,7 +6,8 @@ fn main() {
     match x {
         1 => println!("same"),
         2 => println!("different"),
-        3 => println!("same"),//~ ERROR: these match arms have identical bodies
+        3 => println!("same"), //~ ERROR: these match arms have identical bodies
+        4 => println!("same"),
         _ => println!("other"),
     }
 }
