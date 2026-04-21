d583d549b41dee1dd77023e4fb320c9b59210fcb
linshuy2 <linshuy2@illinois.edu>
2026-04-21 15:13:08 +0000

    fix: `map_unwrap_or` suggests wrongly for conflict borrowing

--- clippy_lints/src/methods/map_unwrap_or_else.rs
+++ clippy_lints/src/methods/map_unwrap_or_else.rs
@@ -1,12 +1,19 @@
+use std::collections::hash_map::Entry;
+use std::ops::ControlFlow;
+
 use clippy_utils::diagnostics::span_lint_and_sugg;
+use clippy_utils::mir::{PossibleBorrowerMap, enclosing_mir, expr_local};
 use clippy_utils::msrvs::{self, Msrv};
-use clippy_utils::res::MaybeDef as _;
+use clippy_utils::res::{MaybeDef as _, MaybeResPath};
 use clippy_utils::source::snippet;
 use clippy_utils::sym;
-use clippy_utils::usage::mutated_variables;
+use clippy_utils::visitors::for_each_expr_without_closures;
+use rustc_ast::Mutability;
+use rustc_data_structures::fx::FxHashMap;
 use rustc_errors::Applicability;
-use rustc_hir as hir;
+use rustc_hir::{self as hir, ExprKind};
 use rustc_lint::LateContext;
+use rustc_middle::mir::Local;
 
 use super::MAP_UNWRAP_OR;
 
@@ -30,14 +37,17 @@ pub(super) fn check<'tcx>(
 
     // Don't make a suggestion that may fail to compile due to mutably borrowing
     // the same variable twice.
-    let Some(map_mutated_vars) = mutated_variables(recv, cx) else {
-        return false;
-    };
-    let Some(unwrap_mutated_vars) = mutated_variables(unwrap_arg, cx) else {
-        return false;
-    };
-    if map_mutated_vars.intersection(&unwrap_mutated_vars).next().is_some() {
-        return false;
+    if let Some(mir) = enclosing_mir(cx.tcx, expr.hir_id) {
+        let borrower_map = PossibleBorrowerMap::new(cx, mir);
+        let recv_borrowed_vars = borrowed_variables(cx, recv, &borrower_map);
+        let map_borrowed_vars = borrowed_variables(cx, map_arg, &borrower_map);
+        let unwrap_borrowed_vars = borrowed_variables(cx, unwrap_arg, &borrower_map);
+        if borrows_in_conflict(&recv_borrowed_vars, &map_borrowed_vars)
+            || borrows_in_conflict(&recv_borrowed_vars, &unwrap_borrowed_vars)
+            || borrows_in_conflict(&map_borrowed_vars, &unwrap_borrowed_vars)
+        {
+            return false;
+        }
     }
 
     // lint message
@@ -65,4 +75,66 @@ pub(super) fn check<'tcx>(
     }
 
     false
+}
+
+fn borrowed_variables<'a, 'tcx>(
+    cx: &LateContext<'tcx>,
+    expr: &'tcx hir::Expr<'tcx>,
+    borrower_map: &'a PossibleBorrowerMap<'a, 'tcx>,
+) -> FxHashMap<Local, Mutability> {
+    let mut used_vars = FxHashMap::default();
+    for_each_expr_without_closures(expr, |expr| {
+        if let Some(hir_id) = expr
+            .res_local_id()
+            // Closures become `Local`s referencing their bodies in the MIR
+            .or_else(|| matches!(expr.kind, ExprKind::Closure(_)).then_some(expr.hir_id))
+            && let entry = used_vars.entry(hir_id)
+            && matches!(entry, Entry::Vacant(_))
+            && let Some(local) = expr_local(cx.tcx, expr)
+        {
+            entry.insert_entry(local);
+        }
+
+        ControlFlow::<()>::Continue(())
+    });
+
+    let mut borrowed_vars = FxHashMap::default();
+    #[expect(
+        rustc::potential_query_instability,
+        reason = "collecting borrowed variables is not sensitive to ordering"
+    )]
+    for local in used_vars.values() {
+        for (borrower, mutability) in borrower_map
+            .map
+            .iter()
+            .filter_map(|(borrower, borrowed)| borrowed.get(local).map(|mutability| (*borrower, *mutability)))
+        {
+            match borrowed_vars.entry(borrower) {
+                Entry::Vacant(entry) => {
+                    entry.insert(mutability);
+                },
+                Entry::Occupied(mut entry) => {
+                    if mutability.is_mut() && entry.get().is_not() {
+                        entry.insert(Mutability::Mut);
+                    }
+                },
+            }
+        }
+    }
+
+    borrowed_vars
+}
+
+fn borrows_in_conflict(
+    borrowed_vars1: &FxHashMap<Local, Mutability>,
+    borrowed_vars2: &FxHashMap<Local, Mutability>,
+) -> bool {
+    #[expect(
+        rustc::potential_query_instability,
+        reason = "checking borrowed variables is not sensitive to ordering"
+    )]
+    borrowed_vars1.iter().any(|(borrower, mutability)| {
+        matches!(borrowed_vars2.get(borrower), 
+            Some(other_mutability) if mutability.is_mut() || other_mutability.is_mut())
+    })
 }
