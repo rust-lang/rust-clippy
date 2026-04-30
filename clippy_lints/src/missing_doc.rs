0a7bf2e4df6c36867355ea83ac303de2afaaa295
Kokoro2336 <2529677678@qq.com>
2026-04-30 18:43:30 +0800

    fix(missing-doc): filter logic.

--- clippy_lints/src/missing_doc.rs
+++ clippy_lints/src/missing_doc.rs
@@ -1,6 +1,6 @@
 use clippy_config::Conf;
 use clippy_utils::diagnostics::span_lint;
-use clippy_utils::{is_doc_hidden, is_from_proc_macro, is_in_test};
+use clippy_utils::{is_cfg_test, is_doc_hidden, is_from_proc_macro, is_in_test, is_test_function};
 use rustc_hir::attrs::AttributeKind;
 use rustc_hir::def_id::LocalDefId;
 use rustc_hir::{
@@ -71,7 +71,7 @@ impl MissingDoc {
 
     fn is_missing_docs(&self, cx: &LateContext<'_>, def_id: LocalDefId, hir_id: HirId) -> bool {
         // Ignore every item in a test module or a #[test] annotated function
-        if is_in_test(cx.tcx, hir_id) {
+        if is_in_test(cx.tcx, hir_id) || is_cfg_test(cx.tcx, hir_id) || is_test_function(cx.tcx, def_id) {
             return false;
         }
 
