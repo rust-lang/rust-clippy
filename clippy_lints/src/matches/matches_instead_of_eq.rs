8b1fe868348b5c67c0eb30dd4fcc7a534200423b
Guillaume Gomez <contact@guillaume-gomez.fr>
2026-05-17 02:05:14 +0200

    Remove fallback for `matches_instead_of_eq`

--- clippy_lints/src/matches/matches_instead_of_eq.rs
+++ clippy_lints/src/matches/matches_instead_of_eq.rs
@@ -1,4 +1,4 @@
-use clippy_utils::diagnostics::{span_lint_and_help, span_lint_and_sugg};
+use clippy_utils::diagnostics::span_lint_and_sugg;
 use clippy_utils::source::snippet_opt;
 use clippy_utils::ty::implements_trait;
 use clippy_utils::{get_parent_expr, sym};
@@ -72,56 +72,44 @@ pub(super) fn check(
     if arms
         .iter()
         .any(|arm| is_valid_pattern_for_eq(arm.pat) && arm.guard.is_none())
+        && let Some((snippet2, mut ty2)) = arms.iter().find_map(|arm| snippet_and_ty_of_arm(cx, arm))
+        && let ExprKind::Match(m, ..) = expr.kind
+        && let Some(snippet1) = snippet_opt(cx, m.span)
     {
-        if let Some((snippet2, mut ty2)) = arms.iter().find_map(|arm| snippet_and_ty_of_arm(cx, arm))
-            && let ExprKind::Match(m, ..) = expr.kind
-            && let Some(snippet1) = snippet_opt(cx, m.span)
-        {
-            let (span, comparison) = get_correct_eq_span(cx, expr);
-            let mut derefs1 = String::new();
-            let mut derefs2 = String::new();
-            while !implements_trait(cx, expr_ty, partial_eq_def_id, &[ty2.into()]) {
-                if let ty::Ref(_, inner_ty, _) = expr_ty.kind() {
-                    expr_ty = *inner_ty;
-                    derefs1.push('*');
-                } else if let ty::Ref(_, inner_ty, _) = ty2.kind() {
-                    ty2 = *inner_ty;
-                    derefs2.push('*');
-                } else {
-                    // Hum... okay?
-                    return;
-                }
+        let (span, comparison) = get_correct_eq_span(cx, expr);
+        let mut derefs1 = String::new();
+        let mut derefs2 = String::new();
+        while !implements_trait(cx, expr_ty, partial_eq_def_id, &[ty2.into()]) {
+            if let ty::Ref(_, inner_ty, _) = expr_ty.kind() {
+                expr_ty = *inner_ty;
+                derefs1.push('*');
+            } else if let ty::Ref(_, inner_ty, _) = ty2.kind() {
+                ty2 = *inner_ty;
+                derefs2.push('*');
+            } else {
+                // Hum... okay?
+                return;
+            }
+        }
+
+        cx.tcx.for_each_relevant_impl(partial_eq_def_id, expr_ty, |impl_id| {
+            if !cx.tcx.is_automatically_derived(impl_id) {
+                return;
             }
 
-            cx.tcx.for_each_relevant_impl(partial_eq_def_id, expr_ty, |impl_id| {
-                if !cx.tcx.is_automatically_derived(impl_id) {
-                    return;
-                }
-
-                let trait_ref = cx.tcx.impl_trait_ref(impl_id);
-                if trait_ref.instantiate_identity().skip_norm_wip().args.type_at(1) != ty2 {
-                    return;
-                }
-                span_lint_and_sugg(
-                    cx,
-                    MATCHES_INSTEAD_OF_EQ,
-                    span,
-                    format!("this expression can be replaced with a `{comparison}` comparison"),
-                    "replace with",
-                    format!("{derefs1}{snippet1} {comparison} {derefs2}{snippet2}"),
-                    Applicability::MaybeIncorrect,
-                );
-            });
-        } else {
-            // If for unknown reasons, `snippet_opt` failed...
-            span_lint_and_help(
+            let trait_ref = cx.tcx.impl_trait_ref(impl_id);
+            if trait_ref.instantiate_identity().skip_norm_wip().args.type_at(1) != ty2 {
+                return;
+            }
+            span_lint_and_sugg(
                 cx,
                 MATCHES_INSTEAD_OF_EQ,
-                expr.span.source_callsite(),
-                "this expression can be replaced with a `==` comparison",
-                None,
-                "this type implements `PartialEq`, so you can use `==` instead of `matches!`",
+                span,
+                format!("this expression can be replaced with a `{comparison}` comparison"),
+                "replace with",
+                format!("{derefs1}{snippet1} {comparison} {derefs2}{snippet2}"),
+                Applicability::MaybeIncorrect,
             );
-        }
+        });
     }
 }
