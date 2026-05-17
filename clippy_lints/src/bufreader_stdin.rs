fc21fd796914d3305f72b1ae46fbeaabea8e3556
JafarTano <81587305+JaafarTanoukhi@users.noreply.github.com>
2026-05-17 21:30:55 +0300

    ran `cargo dev fmt`

--- clippy_lints/src/bufreader_stdin.rs
+++ clippy_lints/src/bufreader_stdin.rs
@@ -1,8 +1,9 @@
-use clippy_utils::{diagnostics::span_lint_and_sugg, source::snippet};
+use clippy_utils::diagnostics::span_lint_and_sugg;
+use clippy_utils::source::snippet;
+use clippy_utils::sym;
 use rustc_errors::Applicability;
 use rustc_hir::{Expr, ExprKind, QPath, TyKind};
 use rustc_lint::{LateContext, LateLintPass};
-use clippy_utils::{sym};
 use rustc_session::impl_lint_pass;
 
 declare_clippy_lint! {
@@ -31,10 +32,9 @@ declare_clippy_lint! {
 
 impl_lint_pass!(BufreaderStdin => [BUFREADER_STDIN]);
 
-pub struct BufreaderStdin {
-}
+pub struct BufreaderStdin {}
 
-impl BufreaderStdin { 
+impl BufreaderStdin {
     pub fn new() -> Self {
         Self {}
     }
@@ -42,18 +42,18 @@ impl BufreaderStdin {
 
 impl<'tcx> LateLintPass<'tcx> for BufreaderStdin {
     fn check_expr(&mut self, cx: &LateContext<'tcx>, expr: &'tcx Expr<'_>) {
-        if expr.span.from_expansion(){
+        if expr.span.from_expansion() {
             return;
         }
 
         if let ExprKind::Call(func, [arg]) = expr.kind
-        && let ExprKind::Path(QPath::TypeRelative(ty, segment)) = func.kind
-        && segment.ident.name == sym::new
-        && let TyKind::Path(ref qpath) = ty.kind
-        && let Some(did) = cx.qpath_res(qpath, ty.hir_id).opt_def_id()
-        && cx.tcx.def_path_str(did).ends_with("std::io::BufReader")
-        && let arg_ty = cx.typeck_results().expr_ty(arg)
-        && let Some(arg_did) = arg_ty.ty_adt_def().map(rustc_middle::ty::AdtDef::did)
+            && let ExprKind::Path(QPath::TypeRelative(ty, segment)) = func.kind
+            && segment.ident.name == sym::new
+            && let TyKind::Path(ref qpath) = ty.kind
+            && let Some(did) = cx.qpath_res(qpath, ty.hir_id).opt_def_id()
+            && cx.tcx.def_path_str(did).ends_with("std::io::BufReader")
+            && let arg_ty = cx.typeck_results().expr_ty(arg)
+            && let Some(arg_did) = arg_ty.ty_adt_def().map(rustc_middle::ty::AdtDef::did)
         {
             if cx.tcx.is_diagnostic_item(sym::Stdin, arg_did) {
                 span_lint_and_sugg(
@@ -65,8 +65,7 @@ impl<'tcx> LateLintPass<'tcx> for BufreaderStdin {
                     format!("{}.lock()", snippet(cx, arg.span, "..")),
                     Applicability::MachineApplicable,
                 );
-            }
-            else if cx.tcx.def_path_str(arg_did).ends_with("std::io::StdinLock"){
+            } else if cx.tcx.def_path_str(arg_did).ends_with("std::io::StdinLock") {
                 span_lint_and_sugg(
                     cx,
                     BUFREADER_STDIN,
