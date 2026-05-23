c8505905893ee4d1e603142e6e127b3aefe083d9
Tamme Dittrich <tamme@tweedegolf.com>
2026-05-23 12:22:34 +0200

    Only lint futures that implement drop

--- tests/ui/forget_future.rs
+++ tests/ui/forget_future.rs
@@ -15,6 +15,9 @@ fn main() {
     let fut = MyFuture;
     mem::forget(fut);
     //~^ forget_future
+
+    #[expect(clippy::forget_non_drop)]
+    mem::forget(NoDropFuture);
 }
 
 async fn foo() {}
@@ -31,4 +34,14 @@ impl Future for MyFuture {
 
 impl Drop for MyFuture {
     fn drop(&mut self) {}
+}
+
+struct NoDropFuture;
+
+impl Future for NoDropFuture {
+    type Output = ();
+
+    fn poll(self: Pin<&mut Self>, _cx: &mut Context<'_>) -> Poll<Self::Output> {
+        Poll::Ready(())
+    }
 }
