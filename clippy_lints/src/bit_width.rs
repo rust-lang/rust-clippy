9fe3ba78e58942884a8686773c0de77e82fd7cd7
alv-around <8678242+alv-around@users.noreply.github.com>
2026-06-30 13:26:57 +0200

    Apply suggestions from code review
    Co-authored-by: Ada Alakbarova <58857108+ada4a@users.noreply.github.com>

--- clippy_lints/src/bit_width.rs
+++ clippy_lints/src/bit_width.rs
@@ -1,5 +1,5 @@
 use clippy_config::Conf;
-use clippy_utils::diagnostics::{span_lint_and_help, span_lint_and_sugg};
+use clippy_utils::diagnostics::{span_lint_and_sugg, span_lint_and_then};
 use clippy_utils::msrvs::{self, Msrv};
 use clippy_utils::source::snippet_with_context;
 use clippy_utils::{is_from_proc_macro, sym};
@@ -26,7 +26,7 @@ declare_clippy_lint! {
     /// let x: u32 = 5;
     /// let bit_width = x.bit_width();
     /// ```
-    #[clippy::version = "1.97.0"]
+    #[clippy::version = "1.98.0"]
     pub MANUAL_BIT_WIDTH,
     pedantic,
     "manually reimplementing `bit_width`"
@@ -34,11 +34,11 @@ declare_clippy_lint! {
 
 declare_clippy_lint! {
     /// ### What it does
-    /// Checks for usage of `T::BITS - x.leading_zeros()` where T and x are of different types
+    /// Checks for usage of `T::BITS - x.leading_zeros()` where T and x are of different types.
     ///
     /// ### Why is this bad?
-    /// Substracting leading_zeros from the number of bits of another type is
-    /// a buggy implementation of the `bit_width` method,
+    /// Substracting `leading_zeros` from the number of bits of another type might be
+    /// a buggy implementation of the `bit_width` method.
     ///
     /// ### Example
     /// ```no_run
@@ -48,9 +48,9 @@ declare_clippy_lint! {
     /// Use instead:
     /// ```no_run
     /// let x: u64 = 5;
-    /// let bit_width = u64::BITS - x.leading_zeros();
+    /// let bit_width = x.bit_width();
     /// ```
-    #[clippy::version = "1.97.0"]
+    #[clippy::version = "1.98.0"]
     pub MISMATCHED_BIT_WIDTH_TYPE,
     suspicious,
     "type mismatch in bit width calculation"
@@ -100,35 +100,61 @@ impl LateLintPass<'_> for ManualBitWidth {
                     && leading_zeros.ident.name == sym::leading_zeros
                     && let ExprKind::Path(QPath::TypeRelative(hir_ty, segment)) = left.kind
                     && segment.ident.name == sym::BITS
-                    && let ty = cx.typeck_results().expr_ty(recv)
+                    && let right_ty = cx.typeck_results().expr_ty(recv)
+                    && let left_ty = cx.typeck_results().node_type(hir_ty.hir_id)
                     && self.msrv.meets(cx, msrvs::BIT_WIDTH)
                     && left.span.eq_ctxt(right.span)
                     && !is_from_proc_macro(cx, expr) =>
             {
-                // manual implementation of bit_width
-                if cx.typeck_results().node_type(hir_ty.hir_id) == ty {
-                    let int_type_invariant = match ty.kind() {
-                        // usize::BITS or uint::BITS
-                        ty::Uint(_) => IntTypeVariant::UnsignedInt,
-                        ty::Int(_) => IntTypeVariant::SignedInt,
-                        // NonZero::<uint>::BITS
-                        ty::Adt(adt, args) if cx.tcx.is_diagnostic_item(sym::NonZero, adt.did()) => {
-                            match args.type_at(0).kind() {
-                                ty::Uint(_) => IntTypeVariant::NonZeroUnsigned,
-                                ty::Int(_) => IntTypeVariant::NonZeroSigned,
-                                _ => return,
-                            }
-                        },
-                        _ => return,
-                    };
-                    emit_manual_bit_width(cx, recv, expr, int_type_invariant);
-                // calling types of `T::BITS` and `x.leading_zeros()` are not aligned
+                let type_invariant = match right_ty.kind() {
+                    // int::BITS or uint::BITS
+                    ty::Uint(_) => IntTypeVariant::UnsignedInt,
+                    ty::Int(_) => IntTypeVariant::SignedInt,
+                    // NonZero::<int/uint>::BITS
+                    ty::Adt(adt, args) if cx.tcx.is_diagnostic_item(sym::NonZero, adt.did()) => {
+                        match args.type_at(0).kind() {
+                            ty::Uint(_) => IntTypeVariant::NonZeroUnsigned,
+                            ty::Int(_) => IntTypeVariant::NonZeroSigned,
+                            _ => return,
+                        }
+                    },
+                    _ => return,
+                };
+
+                if let Some(left_width) = get_bit_width(cx, left_ty)
+                    && let Some(right_width) = get_bit_width(cx, right_ty)
+                    && left_width == right_width
+                {
+                    // manual implementation of bit_width
+                    emit_manual_bit_width(cx, recv, expr, type_invariant);
                 } else {
-                    emit_type_mismatch(cx, left, ty);
+                    // mismatched calling types
+                    emit_type_mismatch(cx, recv, expr, type_invariant, right_ty);
                 }
             },
             _ => {},
         }
+    }
+}
+
+fn get_bit_width(cx: &LateContext<'_>, ty: Ty<'_>) -> Option<u64> {
+    // num.bit_width() return None for `usize` and `isize` so we need this wrapper
+    let size_safe_bit_width = |x: Option<u64>| x.or_else(|| Some(cx.tcx.data_layout.pointer_size().bits()));
+
+    match ty.kind() {
+        // int::BITS or uint::BITS
+        ty::Uint(num) => size_safe_bit_width(num.bit_width()),
+        ty::Int(num) => size_safe_bit_width(num.bit_width()),
+        // NonZero::<int/uint>::BITS
+        ty::Adt(adt, args) if cx.tcx.is_diagnostic_item(sym::NonZero, adt.did()) => {
+            let arg = args.type_at(0);
+            match arg.kind() {
+                ty::Uint(num) => size_safe_bit_width(num.bit_width()),
+                ty::Int(num) => size_safe_bit_width(num.bit_width()),
+                _ => None,
+            }
+        },
+        _ => None,
     }
 }
 
@@ -154,15 +180,31 @@ fn emit_manual_bit_width(
     );
 }
 
-fn emit_type_mismatch(cx: &LateContext<'_>, recv: &Expr<'_>, x_ty: Ty<'_>) {
-    let ty_str = x_ty.to_string();
-
-    span_lint_and_help(
+fn emit_type_mismatch(
+    cx: &LateContext<'_>,
+    recv: &Expr<'_>,
+    full_expr: &Expr<'_>,
+    int_type_invariant: IntTypeVariant,
+    x_ty: Ty<'_>,
+) {
+    span_lint_and_then(
         cx,
         MISMATCHED_BIT_WIDTH_TYPE,
-        recv.span,
-        "`T::BITS` should match the type of the value calling `.leading_zeros()`",
-        None,
-        format!("consider using `{ty_str}::BITS`"),
+        full_expr.span,
+        "possible buggy implementation of `bit_width`",
+        |diag| {
+            diag.note("in order to calculate the bit width, `T::BITS` should match the type of the value calling `.leading_zeros()`");
+
+            let mut app = Applicability::MaybeIncorrect;
+            let (recv_snip, _) = snippet_with_context(cx, recv.span, full_expr.span.ctxt(), "_", &mut app);
+            let suggestion = int_type_invariant.suggestion();
+
+            diag.span_suggestion_verbose(
+                full_expr.span,
+                format!("if you meant to use `{x_ty}::BITS`, use"),
+                format!("{recv_snip}{suggestion}"),
+                app,
+            );
+        },
     );
 }
--- tests/ui/mismatched_bit_width_type.rs
+++ tests/ui/mismatched_bit_width_type.rs
@@ -1,17 +1,15 @@
 #![warn(clippy::mismatched_bit_width_type)]
 
-use core::num::{self, NonZero, NonZeroI32, NonZeroU32};
+use core::num::{self, NonZero};
 
 fn main() {
     // left and right expression have different calling types
-    let w: u32 = 5;
-    let _ = u64::BITS - w.leading_zeros(); //~ mismatched_bit_width_type
-    let x: i64 = -5;
-    let _ = u16::BITS - x.leading_zeros(); //~ mismatched_bit_width_type
-    let y = NonZero::<u64>::new(5).unwrap();
-    let _ = i32::BITS - y.leading_zeros(); //~ mismatched_bit_width_type
-    let z = NonZero::<isize>::new(5).unwrap();
-    let _ = NonZeroU32::BITS - z.leading_zeros(); //~ mismatched_bit_width_type
+    let x: u32 = 5;
+
+    let _ = u8::BITS - x.leading_zeros(); //~ mismatched_bit_width_type
+    let _ = i16::BITS - x.leading_zeros(); //~ mismatched_bit_width_type
+    let _ = NonZero::<u64>::BITS - x.leading_zeros(); //~ mismatched_bit_width_type
+    let _ = NonZero::<isize>::BITS - x.leading_zeros(); //~ mismatched_bit_width_type
 
     // negative case.
     // left expression is a literal
