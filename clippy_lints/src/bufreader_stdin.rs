e0e52523d29534163943f2e927bf2f9f5651051b
JafarTano <81587305+JaafarTanoukhi@users.noreply.github.com>
2026-07-22 02:15:46 +0300

    addressed PR comments

--- clippy_lints/src/bufreader_stdin.rs
+++ clippy_lints/src/bufreader_stdin.rs
@@ -1,4 +1,4 @@
-use clippy_utils::diagnostics::span_lint_and_sugg;
+use clippy_utils::diagnostics::{span_lint, span_lint_and_sugg};
 use clippy_utils::source::snippet;
 use clippy_utils::sym;
 use rustc_errors::Applicability;
@@ -11,14 +11,16 @@ declare_clippy_lint! {
     /// Checks for usage of `BufReader::new` with `Stdin` or `StdinLock`.
     ///
     /// ### Why is this bad?
-    /// `Stdin` is already buffered. Re-buffering it only increase the memcpy calls.
+    /// `Stdin` is already buffered. Re-buffering it only increases the memcpy calls.
     ///
     /// ### Example
     ///
     /// ```ignore
     /// let reader = std::io::BufReader::new(std::io::stdin());
     /// ```
+    ///
     /// Use instead:
+    ///
     /// ```ignore
     /// let stdin = std::io::stdin();
     /// let reader = stdin.lock();
@@ -26,7 +28,7 @@ declare_clippy_lint! {
 
     #[clippy::version = "1.97.0"]
     pub BUFREADER_STDIN,
-    perf,
+    pedantic,
     "using `BufReader::new` with `Stdin` or `StdinLock` is unnecessary and less efficient"
 }
 
@@ -42,39 +44,45 @@ impl BufreaderStdin {
 
 impl<'tcx> LateLintPass<'tcx> for BufreaderStdin {
     fn check_expr(&mut self, cx: &LateContext<'tcx>, expr: &'tcx Expr<'_>) {
-        if expr.span.from_expansion() {
-            return;
-        }
-
         if let ExprKind::Call(func, [arg]) = expr.kind
             && let ExprKind::Path(QPath::TypeRelative(ty, segment)) = func.kind
             && segment.ident.name == sym::new
             && let TyKind::Path(ref qpath) = ty.kind
             && let Some(did) = cx.qpath_res(qpath, ty.hir_id).opt_def_id()
-            && cx.tcx.def_path_str(did).ends_with("std::io::BufReader")
-            && let arg_ty = cx.typeck_results().expr_ty(arg)
-            && let Some(arg_did) = arg_ty.ty_adt_def().map(rustc_middle::ty::AdtDef::did)
+            && cx.tcx.is_diagnostic_item(sym::IoBufReader, did)
+            && let Some(arg_did) = cx
+                .typeck_results()
+                .expr_ty(arg)
+                .ty_adt_def()
+                .map(rustc_middle::ty::AdtDef::did)
         {
-            if cx.tcx.is_diagnostic_item(sym::Stdin, arg_did) {
-                span_lint_and_sugg(
-                    cx,
-                    BUFREADER_STDIN,
-                    expr.span,
-                    "using `BufReader::new` with `Stdin`",
+            let snip = snippet(cx, arg.span, "..");
+            let applicability = if snip == ".." {
+                Applicability::HasPlaceholders
+            } else {
+                Applicability::MaybeIncorrect
+            };
+
+            let (msg, help, sugg) = if cx.tcx.is_diagnostic_item(sym::Stdin, arg_did) {
+                (
+                    "wrapping already buffered `Stdin` into a `BufReader`",
                     "instead of wrapping `Stdin` in `BufReader`, use the `lock` method directly",
-                    format!("{}.lock()", snippet(cx, arg.span, "..")),
-                    Applicability::MachineApplicable,
-                );
-            } else if cx.tcx.def_path_str(arg_did).ends_with("std::io::StdinLock") {
-                span_lint_and_sugg(
-                    cx,
-                    BUFREADER_STDIN,
-                    expr.span,
-                    "using `BufReader::new` with `StdinLock`",
+                    format!("{snip}.lock()"),
+                )
+            } else if cx.tcx.is_diagnostic_item(sym::StdinLock, arg_did) {
+                (
+                    "wrapping already buffered `StdinLock` into a `BufReader`",
                     "instead of wrapping `StdinLock` in `BufReader`, use it directly",
-                    snippet(cx, arg.span, "..").to_string(),
-                    Applicability::MachineApplicable,
-                );
+                    snip.to_string(),
+                )
+            } else {
+                return;
+            };
+
+            if arg.span.from_expansion() {
+                span_lint(cx, BUFREADER_STDIN, expr.span, msg);
+            } else {
+                span_lint_and_sugg(cx, BUFREADER_STDIN, expr.span, msg, help, sugg, applicability);
             }
         }
     }
--- clippy_lints/src/lib.rs
+++ clippy_lints/src/lib.rs
@@ -869,6 +869,7 @@ rustc_lint::late_lint_methods!(
         RedundantElse: redundant_else::RedundantElse = redundant_else::RedundantElse,
         RestWhenDestructuringStruct: rest_when_destructuring_struct::RestWhenDestructuringStruct = rest_when_destructuring_struct::RestWhenDestructuringStruct,
         BlockScrutinee: block_scrutinee::BlockScrutinee = block_scrutinee::BlockScrutinee,
+        BufreaderStdin: bufreader_stdin::BufreaderStdin = bufreader_stdin::BufreaderStdin::new(),
         // add late passes here, used by `cargo dev new_lint`
     ]]
 );
