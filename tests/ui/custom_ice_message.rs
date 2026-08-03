ba47895f5aee2ae4d79aa146ab7850948b743e33
Jason Newcomb <jsnewcomb@pm.me>
2026-08-03 15:40:26 -0400

    Use `rustc_delayed_bug_from_inside_query` to test the ICE message hook.

--- tests/ui/custom_ice_message.rs
+++ tests/ui/custom_ice_message.rs
@@ -1,15 +1,13 @@
 //@rustc-env:RUST_BACKTRACE=0
 //@normalize-stderr-test: "Clippy version: .*" -> "Clippy version: foo"
-//@normalize-stderr-test: "produce_ice.rs:\d*:\d*" -> "produce_ice.rs"
-//@normalize-stderr-test: "', .*clippy_lints" -> "', clippy_lints"
-//@normalize-stderr-test: "'rustc'" -> "'<unnamed>'"
+//@normalize-stderr-test: "custom_ice_message.rs:\d*:\d*" -> "custom_ice_message.rs"
 //@normalize-stderr-test: "rustc 1\.\d+.* running on .*" -> "rustc <version> running on <target>"
 //@normalize-stderr-test: "(?ms)query stack during panic:\n.*end of query stack\n" -> ""
+//@normalize-stderr-test: "note: delayed at .*" -> "note: delayed at <location>"
+//@normalize-stderr-test: "note: compiler flags: .*" -> "note: compiler flags: <flags>"
 
-#![deny(clippy::produce_ice)]
-#![allow(clippy::missing_clippy_version_attribute)]
+#![feature(rustc_attrs)]
 
-fn it_looks_like_you_are_trying_to_kill_clippy() {}
-//~^ ice: Would you like some help with that?
-
+#[rustc_delayed_bug_from_inside_query]
 fn main() {}
+//~^ ice: delayed bug triggered by
