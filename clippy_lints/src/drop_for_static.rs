7691a5b6d907d0f6ba332585753c42ab1d2b5a02
Mr-Leshiy <leshiy12345678@gmail.com>
2026-09-05 13:30:51 +0200

    fix build

--- clippy_lints/src/drop_for_static.rs
+++ clippy_lints/src/drop_for_static.rs
@@ -1,7 +1,6 @@
 use clippy_utils::diagnostics::span_lint;
 use rustc_hir::{Item, ItemKind};
-use rustc_lint::{LateContext, LateLintPass};
-use rustc_session::declare_lint_pass;
+use rustc_lint::{LateContext, LateLintPass, declare_lint_pass};
 
 declare_clippy_lint! {
     /// ### What it does
