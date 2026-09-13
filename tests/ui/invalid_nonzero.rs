76d700741711e9f999dc2ff3f74e512eadca0007
Bishops-exe <bishops-exe@users.noreply.github.com>
2026-09-13 13:15:48 +0200

    test: update `invalid_nonzero` tests

--- tests/ui/invalid_nonzero.rs
+++ tests/ui/invalid_nonzero.rs
@@ -1,5 +1,7 @@
+//@aux-build:proc_macros.rs
 #![warn(clippy::invalid_nonzero)]
-#![allow(clippy::unnecessary_literal_unwrap)]
+
+extern crate proc_macros;
 
 use std::num::{NonZero, NonZeroI32, NonZeroU8, NonZeroU16, NonZeroUsize};
 
@@ -7,8 +9,6 @@ const ZERO: u8 = 0;
 
 fn main() {
     let _ = NonZeroU16::new(0);
-    //~^ invalid_nonzero
-    let _ = NonZeroU16::new(0).unwrap();
     //~^ invalid_nonzero
     let _ = NonZeroU8::new(ZERO);
     //~^ invalid_nonzero
@@ -24,4 +24,12 @@ fn main() {
     let _ = NonZeroU16::new(1).unwrap();
     let x = 5;
     let _ = NonZeroU8::new(x);
+
+    proc_macros::external! {
+        let _ = NonZeroU16::new(0);
+    }
+    proc_macros::with_span! {
+        span
+        let _ = NonZeroU16::new(0);
+    }
 }
