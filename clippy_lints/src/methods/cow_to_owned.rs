fd06af2d09f4fb9363793d1faadfe19cb13b1057
A4-Tacks <wdsjxhno1001@163.com>
2026-09-22 17:28:26 +0800

    Replace is_clone_like with is_to_owned_like

--- clippy_lints/src/methods/cow_to_owned.rs
+++ clippy_lints/src/methods/cow_to_owned.rs
@@ -6,18 +6,14 @@ use rustc_hir::Expr;
 use rustc_lint::LateContext;
 use rustc_span::{Span, Symbol};
 
-use crate::methods::is_clone_like;
-
 use super::COW_TO_OWNED;
 
 pub fn check(cx: &LateContext<'_>, method_name: Symbol, expr: &Expr<'_>, recv: &Expr<'_>, span: Span) {
-    debug_assert_ne!(method_name, sym::to_owned, "hit Cow::to_owned");
-
     // To avoid accidental move, only temporary values are currently being checked.
     // See https://github.com/rust-lang/rust-clippy/issues/2387#issuecomment-5473343990
     if let Some(method_parent_id) = cx.typeck_results().type_dependent_def_id(expr.hir_id).opt_parent(cx)
         && cx.typeck_results().expr_ty(recv).is_diag_item(cx, sym::Cow)
-        && is_clone_like(cx, method_name, method_parent_id)
+        && is_to_owned_like(cx, method_name, method_parent_id)
         && is_expr_temporary_value(cx, recv)
     {
         let msg = format!(
@@ -27,5 +23,17 @@ pub fn check(cx: &LateContext<'_>, method_name: Symbol, expr: &Expr<'_>, recv: &
         let sugg = "into_owned".to_owned();
         let app = Applicability::MachineApplicable;
         span_lint_and_sugg(cx, COW_TO_OWNED, span, msg, "try", sugg, app);
+    }
+}
+
+fn is_to_owned_like(cx: &LateContext<'_>, method_name: Symbol, method_parent_id: rustc_hir::def_id::DefId) -> bool {
+    match method_name {
+        sym::to_os_string => method_parent_id.opt_impl_ty(cx).is_diag_item(cx, sym::OsStr),
+        sym::to_path_buf => method_parent_id.opt_impl_ty(cx).is_diag_item(cx, sym::Path),
+        sym::to_string => method_parent_id.is_diag_item(cx, sym::ToString),
+        sym::to_vec => method_parent_id
+            .opt_impl_ty(cx)
+            .is_some_and(|ty| ty.instantiate_identity().skip_norm_wip().is_slice()),
+        _ => false,
     }
 }
