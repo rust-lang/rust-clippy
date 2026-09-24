db49bcee5540512c54a60aff1273315be84a2677
medzernik <medzernik@medzernik.dev>
2026-09-24 22:06:10 +0200

    rebase fixes (will squash later)
    Signed-off-by: medzernik <medzernik@medzernik.dev>

--- clippy_lints/src/lib.rs
+++ clippy_lints/src/lib.rs
@@ -873,7 +873,6 @@ rustc_lint::late_lint_methods!(
         BlockScrutinee: block_scrutinee::BlockScrutinee = block_scrutinee::BlockScrutinee,
         NonnullUncheckedOnBoxPtr: nonnull_unchecked_on_box_ptr::NonnullUncheckedOnBoxPtr = nonnull_unchecked_on_box_ptr::NonnullUncheckedOnBoxPtr::new(conf),
         FnParamRefCloned: fn_param_ref_cloned::FnParamRefCloned = fn_param_ref_cloned::FnParamRefCloned,
-        UnnecessaryNonzeroGet: unnecessary_nonzero_get::UnnecessaryNonzeroGet = unnecessary_nonzero_get::UnnecessaryNonzeroGet::new(conf),
         NeedlessNonzeroGet: needless_nonzero_get::NeedlessNonzeroGet = needless_nonzero_get::NeedlessNonzeroGet::new(conf),
         // add late passes here, used by `cargo dev new_lint`
     ]]
