e72653f35d01aef8878964fc3b5e6569674c50b2
voodookiidoo <voodookiidoo@yandex.ru>
2026-09-27 01:58:41 +0500

    added lint for replaceing manual splitting with a method

--- clippy_lints/src/manual_split.rs
+++ clippy_lints/src/manual_split.rs
@@ -68,7 +68,7 @@ impl<'tcx> LateLintPass<'tcx> for ManualSplitAt {
             && eq_expr_value(cx, init.span.ctxt(), target1, target2)
             && let receiver_ty = cx.typeck_results().expr_ty_adjusted(target1).peel_refs()
             && let receiver_chain = deref_chain(cx, receiver_ty).collect::<Vec<_>>()
-            && receiver_chain.iter().any(|ty| has_split_at_method(ty))
+            && receiver_chain.iter().any(|ty| has_split_at_method(*ty))
             && let Some(bind_holder) = IndexingExprHolder::try_bind(index1, index2, cx)
             && eq_expr_value(
                 cx,
@@ -172,6 +172,6 @@ fn is_str(receiver_chain: &[Ty<'_>]) -> bool {
     receiver_chain.iter().any(|ty| matches!(ty.kind(), ty::Str))
 }
 
-fn has_split_at_method(ty: &Ty<'_>) -> bool {
+fn has_split_at_method(ty: Ty<'_>) -> bool {
     matches!(ty.kind(), ty::Slice(_) | ty::Str | ty::Array(..))
 }
